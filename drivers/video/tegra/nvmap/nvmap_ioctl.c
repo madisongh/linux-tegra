f03b77171399cf3b3248a944d6947a6e9c46b886
Krishna Reddy <vdumpa@nvidia.com>
2011-07-25 11:49:54 -0700

    video: tegra: nvmap: Allow cache maint for write combine memory.
    Map uncahced memory as uncached, inner cached as inner cached and
    cached as cached.
    
    Bug 841109
    
    Original-Change-Id: Id92532828ab2e69bac80b20f2be2fc243e4db888
    Reviewed-on: http://git-master/r/43078
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>
    Tested-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-by: Kaz Fukuoka <kfukuoka@nvidia.com>
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>
    
    Rebase-Id: R75168ffeab3c91d3080b53d4d66c4e550d6f9e12

--- drivers/video/tegra/nvmap/nvmap_ioctl.c
+++ drivers/video/tegra/nvmap/nvmap_ioctl.c
@@ -220,6 +220,7 @@ int nvmap_map_into_caller_ptr(struct file *filp, void __user *arg)
 	struct nvmap_vma_priv *vpriv;
 	struct vm_area_struct *vma;
 	struct nvmap_handle *h = NULL;
+	unsigned int cache_flags;
 	int err = 0;
 
 	if (copy_from_user(&op, arg, sizeof(op)))
@@ -280,7 +281,9 @@ int nvmap_map_into_caller_ptr(struct file *filp, void __user *arg)
 	vpriv->handle = h;
 	vpriv->offs = op.offset;
 
-	if (op.flags == NVMAP_HANDLE_INNER_CACHEABLE) {
+	cache_flags = op.flags & NVMAP_HANDLE_CACHE_FLAG;
+	if (cache_flags == NVMAP_HANDLE_INNER_CACHEABLE ||
+	    cache_flags == NVMAP_HANDLE_CACHEABLE) {
 		if (h->orig_size & ~PAGE_MASK) {
 			pr_err("\n%s:attempt to convert a buffer from uc/wc to"
 				" wb, whose size is not a multiple of page size."
@@ -288,8 +291,8 @@ int nvmap_map_into_caller_ptr(struct file *filp, void __user *arg)
 		} else {
 			wmb();
 			/* override allocation time cache coherency attributes. */
-			h->flags &= (~NVMAP_HANDLE_CACHEABLE);
-			h->flags |= NVMAP_HANDLE_INNER_CACHEABLE;
+			h->flags &= ~NVMAP_HANDLE_CACHE_FLAG;
+			h->flags |= cache_flags;
 		}
 	}
 	vma->vm_page_prot = nvmap_pgprot(h, vma->vm_page_prot);
@@ -560,9 +563,12 @@ static int cache_maint(struct nvmap_client *client, struct nvmap_handle *h,
 		goto out;
 	}
 
-	if (h->flags == NVMAP_HANDLE_UNCACHEABLE ||
-	    h->flags == NVMAP_HANDLE_WRITE_COMBINE ||
-	    start == end)
+	wmb();
+#if defined(CONFIG_ARCH_TEGRA_2x_SOC)
+	if (h->flags == NVMAP_HANDLE_WRITE_COMBINE)
+		goto out;
+#endif
+	if (h->flags == NVMAP_HANDLE_UNCACHEABLE || start == end)
 		goto out;
 
 	if (fast_cache_maint(client, h, start, end, op))
@@ -616,7 +622,6 @@ out:
 	if (pte)
 		nvmap_free_pte(client->dev, pte);
 	nvmap_handle_put(h);
-	wmb();
 	return err;
 }
 
