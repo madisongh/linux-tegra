ad4b5d734e5acc1abada1de6599e7f70165b3ba8
Puneet Saxena <puneets@nvidia.com>
2012-04-19 12:32:29 +0530

    cpufreq: interface for setting governor for a cpu
    This implementation sets governor for a cpu using existing
    cpufreq interfaces.
    
    bug 871958
    
    Change-Id: Ic4e7e2a2b0babaf1829b559b5db211666d449b86
    Signed-off-by: Puneet Saxena <puneets@nvidia.com>
    Reviewed-on: http://git-master/r/97939
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    
    Rebase-Id: Rb3099a95700c83e412808b91da1c076f0873722c

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -156,6 +156,7 @@ u64 get_cpu_idle_time(unsigned int cpu, u64 *wall, int io_busy);
 int cpufreq_get_policy(struct cpufreq_policy *policy, unsigned int cpu);
 int cpufreq_update_policy(unsigned int cpu);
 bool have_governor_per_policy(void);
+int cpufreq_set_gov(char *target_gov, unsigned int cpu);
 struct kobject *get_governor_parent_kobj(struct cpufreq_policy *policy);
 #else
 static inline unsigned int cpufreq_get(unsigned int cpu)
