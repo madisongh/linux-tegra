32476c945db3a1a41c04c1ac97c8b57c0ce6b7a1
Joshua Primero <jprimero@nvidia.com>
2012-05-31 18:11:23 -0700

    drivers: misc: Thermal estimator driver
    Added driver which estimates temperature based on
    a linear formula from other temperature sensors.
    
    bug 1007726
    
    Change-Id: Ic0d3ba7f0d369d4321f55b03e6326ff4efbb512e
    Signed-off-by: Joshua Primero <jprimero@nvidia.com>
    Reviewed-on: http://git-master/r/105988
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    
    Rebase-Id: Reb6f2ab85d80012c062621d8b5782a61ce0a1ddc

--- include/linux/therm_est.h
+++ include/linux/therm_est.h
@@ -1,7 +1,7 @@
 /*
  * include/linux/therm_est.h
  *
- * Copyright (c) 2010-2016, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2010-2012, NVIDIA Corporation.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -18,97 +18,58 @@
 #define _LINUX_THERM_EST_H
 
 #include <linux/workqueue.h>
-#include <linux/thermal.h>
-#include <linux/platform_data/thermal_sensors.h>
-
 #define HIST_LEN (20)
 
-#define MAX_ACTIVE_STATES	10
-#define MAX_TIMER_TRIPS		10
-
-struct therm_est_sub_thz {
-	struct thermal_zone_device *thz;
+struct therm_est_subdevice {
+	void *dev_data;
+	int (*get_temp)(void *, long *);
+	long coeffs[HIST_LEN];
 	long hist[HIST_LEN];
 };
 
-struct therm_est_coeffs {
+struct therm_estimator {
+	long therm_est_lo_limit;
+	long therm_est_hi_limit;
+	void (*callback)(void *);
+	void *callback_data;
+	long cur_temp;
+	long polling_period;
+	struct workqueue_struct *workqueue;
+	struct delayed_work therm_est_work;
 	long toffset;
-	long (*coeffs)[HIST_LEN];
-};
-
-struct therm_est_subdevice {
-	struct therm_est_sub_thz *sub_thz;
-	struct therm_est_coeffs *coeffs_set;
-	int num_devs;
-	int num_coeffs;
-	int active_coeffs;
 	int ntemp;
-};
-
-/*
- * Timer trip provides a way to change trip temp dynamically based on timestamp
- * when the trip is enabled.
- * - Timer trip can be various numbers on a trip.
- * - If the trip is enabled, then timer will be started with time_after delay
- *   in the corresponding timer trip. After the timer expires, trip_temp and
- *   hysteresis in the corresponding timer trip will be used to trip_temp for
- *   the trip.
- * - When the timer has expired, index of timer trip will be increased a step
- *   and then start the timer with time_after delay in newly indexed timer trip.
- * - When temp is below trip temp, index of timer trip will be decreased a step
- *   and then stop the timer and start the timer with time_after delay in newly
- *   indexed timer trip.
- * - The timer will be stopped if there is no more next timer trip on the trip,
- *   or the trip is disabled.
- */
-struct therm_est_timer_trip {
-	long time_after; /* in msec */
-	long trip_temp;
-	long hysteresis;
-};
-
-struct therm_est_timer_trip_info {
-	int trip; /* trip point on thermal zone to apply timer trip */
-	int num_timers;
-	struct therm_est_timer_trip timers[MAX_TIMER_TRIPS];
-	int cur; /* index of current timer trip */
-	s64 last_tripped; /* timestamp when the trip is enabled, in msec */
-};
-
-struct therm_est_data {
-	/* trip point info */
-	int num_trips;
-	struct thermal_trip_info *trips;
-
-	/* timer trip info */
-	int num_timer_trips;
-	struct therm_est_timer_trip_info *timer_trips;
-
-	/* zone parameters */
-	struct thermal_zone_params *tzp;
-	long polling_period;
-	int passive_delay;
-	int tc1;
-	int tc2;
-	struct therm_est_subdevice subdevice;
-	int use_activator;
-};
-
-struct therm_fan_est_subdevice {
-	const char *dev_data;
-	int (*get_temp)(const char *, long *);
-	long coeffs[HIST_LEN];
-	int hist[HIST_LEN];
-};
-
-struct therm_fan_est_data {
-	long toffset;
-	long polling_period;
 	int ndevs;
-	char *cdev_type;
-	int active_trip_temps[MAX_ACTIVE_STATES];
-	int active_hysteresis[MAX_ACTIVE_STATES];
-	struct thermal_zone_params *tzp;
-	struct therm_fan_est_subdevice devs[];
+	struct therm_est_subdevice **devs;
 };
+
+#ifdef CONFIG_THERM_EST
+struct therm_estimator *therm_est_register(
+			struct therm_est_subdevice **devs,
+			int ndevs,
+			long toffset,
+			long pperiod);
+int therm_est_get_temp(struct therm_estimator *est, long *temp);
+int therm_est_set_limits(struct therm_estimator *est,
+			long lo_limit,
+			long hi_limit);
+int therm_est_set_alert(struct therm_estimator *est,
+			void (*cb)(void *),
+			void *cb_data);
+#else
+static inline struct therm_estimator *therm_est_register(
+			struct therm_est_subdevice **devs,
+			int ndevs,
+			long toffset,
+			long pperiod)
+{ return NULL; }
+static inline int therm_est_get_temp(struct therm_estimator *est, long *temp)
+{ return -EINVAL; }
+static inline int therm_est_set_limits(struct therm_estimator *est,
+			long lo_limit,
+			long hi_limit)
+{ return -EINVAL; }
+static inline int therm_est_set_alert(struct therm_estimator *est,
+			void (*cb)(void *),
+			void *cb_data)
+{ return -EINVAL; }
 #endif /* _LINUX_THERM_EST_H */
