6d321eb93a17a6e43a5c3ed793d3f252bf9d4065
Krishna Reddy <vdumpa@nvidia.com>
2012-07-18 14:59:51 -0700

    video: tegra: nvmap: Set PAGELIST_VMALLOC_MIN size correct
    page list array size greater than PAGE_SIZE should use vmalloc.
    
    Change-Id: Ic03668ba7ff716bfb3cc08aaef5f86214ee0a9df
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/116875
    Reviewed-by: Alex Waterman <alexw@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Hiroshi Doyu <hdoyu@nvidia.com>
    Tested-by: Hiroshi Doyu <hdoyu@nvidia.com>
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>
    (cherry picked from commit ddacaacd2a9fff10187e026eaa0d898694eeb95f)
    Reviewed-on: http://git-master/r/118194
    
    Rebase-Id: R3e1f86a3368af5748cf4fd83bd31643c6e3d252f
    Signed-off-by: Sri Krishna chowdary <schowdary@nvidia.com>

--- drivers/video/tegra/nvmap/nvmap_handle.c
+++ drivers/video/tegra/nvmap/nvmap_handle.c
@@ -3,7 +3,7 @@
  *
  * Handle allocation and freeing routines for nvmap
  *
- * Copyright (c) 2009-2012, NVIDIA Corporation.
+ * Copyright (c) 2009-2012, NVIDIA CORPORATION. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -54,7 +54,8 @@
  * the kernel (i.e., not a carveout handle) includes its array of pages. to
  * preserve kmalloc space, if the array of pages exceeds PAGELIST_VMALLOC_MIN,
  * the array is allocated using vmalloc. */
-#define PAGELIST_VMALLOC_MIN	(PAGE_SIZE * 2)
+#define PAGELIST_VMALLOC_MIN	(PAGE_SIZE)
+
 #ifdef CONFIG_NVMAP_PAGE_POOLS
 
 #define NVMAP_TEST_PAGE_POOL_SHRINKER 1
@@ -433,7 +434,7 @@ int nvmap_page_pool_get_free_count(struct nvmap_page_pool *pool)
 
 static inline void *altalloc(size_t len)
 {
-	if (len >= PAGELIST_VMALLOC_MIN)
+	if (len > PAGELIST_VMALLOC_MIN)
 		return vmalloc(len);
 	else
 		return kmalloc(len, GFP_KERNEL);
@@ -444,7 +445,7 @@ static inline void altfree(void *ptr, size_t len)
 	if (!ptr)
 		return;
 
-	if (len >= PAGELIST_VMALLOC_MIN)
+	if (len > PAGELIST_VMALLOC_MIN)
 		vfree(ptr);
 	else
 		kfree(ptr);
