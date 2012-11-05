2a0dade5ce7f586117742dfeeaa38976dfa228d9
Dan Willemsen <dwillemsen@nvidia.com>
2012-11-05 11:49:41 -0800

    Partial Revert "ARM: tegra11: fix compilation issues"
    This reverts commit 1b6c57a85f3db2b9382836a64f43729c53f3c17a.
    
    Rebase-Id: R1f9dd2df06a1f47e9be6142e823f0a278a545edf

--- arch/arm/mach-tegra/devices.c
+++ arch/arm/mach-tegra/devices.c
@@ -480,7 +480,7 @@ static struct resource spi_resource6[] = {
 };
 #endif
 
-#if defined(CONFIG_ARCH_TEGRA_3x_SOC)
+#ifdef CONFIG_ARCH_TEGRA_3x_SOC
 static struct resource dtv_resource[] = {
 	[0] = {
 		.start  = INT_DTV,
@@ -776,7 +776,7 @@ struct platform_device tegra_nor_device = {
 	},
 };
 
-#if defined(CONFIG_ARCH_TEGRA_3x_SOC)
+#ifdef CONFIG_ARCH_TEGRA_3x_SOC
 struct platform_device tegra_dtv_device = {
 	.name           = "tegra_dtv",
 	.id             = -1,
