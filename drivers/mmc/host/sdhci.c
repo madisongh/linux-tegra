94795dfadb98c40f9b4615e8dde6089cbbe7d56d
Naveen Kumar S <nkumars@nvidia.com>
2013-01-30 09:35:50 +0530

    mmc:sdhci:reduce level of '512 byte block' warning
    Reduced severity of the message informing usage of 512 bytes block size
    from pr_warning to pr_info.
    
    Bug 1166542
    
    Signed-off-by: Naveen Kumar S <nkumars@nvidia.com>
    Reviewed-on: http://git-master/r/218894
    (cherry picked from commit 4a59f77b2cd578dc0681cc363117e774f05acd43)
    Change-Id: I4e06da6e5551699c5acff9220a1b26151ef26ca4
    Signed-off-by: Pavan Kunapuli <pkunapuli@nvidia.com>
    Reviewed-on: http://git-master/r/739033
    Reviewed-by: Automatic_Commit_Validation_User

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -3244,7 +3244,7 @@ int sdhci_add_host(struct sdhci_host *host)
 		mmc->max_blk_size = (caps[0] & SDHCI_MAX_BLOCK_MASK) >>
 				SDHCI_MAX_BLOCK_SHIFT;
 		if (mmc->max_blk_size >= 3) {
-			pr_warn("%s: Invalid maximum block size, assuming 512 bytes\n",
+			pr_info("%s: Invalid maximum block size, assuming 512 bytes\n",
 				mmc_hostname(mmc));
 			mmc->max_blk_size = 0;
 		}
