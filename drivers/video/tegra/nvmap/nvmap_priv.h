6490e5c52e8cf7335a987a8d2583a5a0eaca5819
Krishna Reddy <vdumpa@nvidia.com>
2013-08-21 14:31:35 -0700

    video: tegra: nvmap: remove dev from nvmap_client
    prepare for dmabuf support.
    Bug 1290869
    
    Change-Id: I791f341ea20b4e90082a3199844919d176064c68
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/264592
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Alex Waterman <alexw@nvidia.com>

--- drivers/video/tegra/nvmap/nvmap_priv.h
+++ drivers/video/tegra/nvmap/nvmap_priv.h
@@ -176,7 +176,6 @@ struct nvmap_carveout_commit {
 
 struct nvmap_client {
 	const char			*name;
-	struct nvmap_device		*dev;
 	struct rb_root			handle_refs;
 	atomic_t			iovm_commit;
 	size_t				iovm_limit;
