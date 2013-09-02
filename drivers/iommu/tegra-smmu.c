19cf6cf517e2d4cc32b1134386c4cbe5f13a319a
Hiroshi Doyu <hdoyu@nvidia.com>
2013-09-02 15:50:51 +0300

    iommu/tegra: smmu: Add debug_dma_platformdata()
    For DMA_API_DEBUG to show platform specific data.
    
    Bug 1173494
    
    Change-Id: I421a0bb3675c7bc6906140f7c1ef867f15e80164
    Signed-off-by: Hiroshi Doyu <hdoyu@nvidia.com>
    Reviewed-on: http://git-master/r/269147
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>

--- drivers/iommu/tegra-smmu.c
+++ drivers/iommu/tegra-smmu.c
@@ -1342,6 +1342,24 @@ static bool smmu_iommu_capable(enum iommu_cap cap)
 	return false;
 }
 
+#if defined(CONFIG_DMA_API_DEBUG) || defined(CONFIG_FTRACE)
+char *debug_dma_platformdata(struct device *dev)
+{
+	static char buf[21];
+	struct dma_iommu_mapping *mapping = to_dma_iommu_mapping(dev);
+	struct smmu_as *as;
+	int asid = -1;
+
+	if (mapping) {
+		as = mapping->domain->priv;
+		asid = as->asid;
+	}
+
+	sprintf(buf, "%d", asid);
+	return buf;
+}
+#endif
+
 static int smmu_iommu_attach_dev(struct iommu_domain *domain,
 				 struct device *dev)
 {
