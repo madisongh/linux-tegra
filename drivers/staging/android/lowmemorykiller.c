9932f28f81aa5171581b30fe260794bb2155c730
Krishna Reddy <vdumpa@nvidia.com>
2013-10-14 11:23:19 -0700

    staging: android: nvmap page pool free memory should be in other_free
    nvmap page pool free memory should be added to other_free, which is
    used to detect low memory conditions.
    Bug 1385817
    Bug 1387131
    
    Change-Id: I60adf77c004383e43940c3fc586099f4da80a1f5
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/299032
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-on: http://git-master/r/1182914
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Alex Waterman <alexw@nvidia.com>
    Tested-by: Alex Waterman <alexw@nvidia.com>
    Reviewed-by: Sri Krishna Chowdary <schowdary@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -42,6 +42,9 @@
 #include <linux/rcupdate.h>
 #include <linux/profile.h>
 #include <linux/notifier.h>
+#ifdef CONFIG_TEGRA_NVMAP
+#include <linux/nvmap.h>
+#endif
 
 #define CREATE_TRACE_POINTS
 #include "trace/lowmemorykiller.h"
@@ -91,7 +94,11 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 	int selected_tasksize = 0;
 	short selected_oom_score_adj;
 	int array_size = ARRAY_SIZE(lowmem_adj);
-	int other_free = global_page_state(NR_FREE_PAGES) - totalreserve_pages;
+	int other_free = global_page_state(NR_FREE_PAGES) - totalreserve_pages
+#ifdef CONFIG_TEGRA_NVMAP
+			 + nvmap_page_pool_get_unused_pages()
+#endif
+			 ;
 	int other_file = global_page_state(NR_FILE_PAGES) -
 						global_page_state(NR_SHMEM) -
 						total_swapcache_pages();
