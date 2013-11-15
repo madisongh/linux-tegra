ab70b4f6f34bdf133787f0f1ae7532b8af169af0
Krishna Reddy <vdumpa@nvidia.com>
2013-11-15 11:50:43 -0800

    video: tegra: nvmap: add barrier before alloc set
    Add barrier to ensure that handle alloc data is
    visible before alloc flag is set.
    Bug 1391550
    Bug 1338435
    
    Change-Id: Ie7dcc7b1807c063b49d36ccb1caf1e74eb1ebad4
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/331897
    Reviewed-by: Alex Waterman <alexw@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit

--- drivers/video/tegra/nvmap/nvmap_handle.c
+++ drivers/video/tegra/nvmap/nvmap_handle.c
@@ -820,6 +820,11 @@ static void alloc_handle(struct nvmap_client *client,
 		if (b) {
 			h->carveout = b;
 			h->heap_pgalloc = false;
+			/* barrier to ensure all handle alloc data
+			 * is visible before alloc is seen by other
+			 * processors.
+			 */
+			mb();
 			h->alloc = true;
 			nvmap_carveout_commit_add(client,
 				nvmap_heap_to_arg(nvmap_block_to_heap(b)),
@@ -835,8 +840,8 @@ static void alloc_handle(struct nvmap_client *client,
 			atomic_sub(reserved, &client->iovm_commit);
 			return;
 		}
-
 		h->heap_pgalloc = true;
+		mb();
 		h->alloc = true;
 	}
 }
