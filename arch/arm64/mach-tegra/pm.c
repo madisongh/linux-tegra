2d0913e9bb70ac7f3efd30dc23516fd04d097ea2
Prashant Gaikwad <pgaikwad@nvidia.com>
2014-05-12 15:07:49 +0530

    arm: tegra: move smmu header to include/linux
    Change-Id: Ib63bad8b7e0c08c5e1de95d5437ab7a48888b596
    Signed-off-by: Prashant Gaikwad <pgaikwad@nvidia.com>

--- arch/arm64/mach-tegra/pm.c
+++ arch/arm64/mach-tegra/pm.c
@@ -52,6 +52,7 @@
 #include <linux/irqchip/tegra.h>
 #include <linux/tegra-pm.h>
 #include <linux/tegra_pm_domains.h>
+#include <linux/tegra_smmu.h>
 
 #include <trace/events/power.h>
 #include <trace/events/nvsecurity.h>
@@ -67,7 +68,6 @@
 #include <asm/psci.h>
 
 #include <mach/irqs.h>
-#include <mach/tegra_smmu.h>
 
 #include "board.h"
 #include "clock.h"
