e1925efcd978738d7a9b1d87d630a15c932be593
Arto Merilainen <amerilainen@nvidia.com>
2014-05-22 12:00:04 +0300

    sync: Dump fence information on timeout
    Currently we print only the fence memory address if the fence
    times out. This is not enough for debugging *why* the fence timed
    out.
    
    This patch modifies timeout routine so that we print the syncpoints
    that relate to the current fence.
    
    Bug 200006324
    
    Change-Id: I5968ef6acde29b2360cfaa9850760abb4423ca07
    Signed-off-by: Arto Merilainen <amerilainen@nvidia.com>
    Reviewed-on: http://git-master/r/1123309
    Reviewed-by: Shridhar Rasal <srasal@nvidia.com>
    Tested-by: Shridhar Rasal <srasal@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- drivers/staging/android/sync.c
+++ drivers/staging/android/sync.c
@@ -368,6 +368,21 @@ int sync_fence_cancel_async(struct sync_fence *fence,
 }
 EXPORT_SYMBOL(sync_fence_cancel_async);
 
+static void sync_fence_dump(struct sync_fence *fence)
+{
+	struct sync_pt *pt;
+	char val[32];
+
+	list_for_each_entry(pt, &fence->pt_list_head, pt_list) {
+		val[0] = '\0';
+		if (pt->parent->ops->pt_value_str)
+			pt->parent->ops->pt_value_str(pt, val, sizeof(val));
+
+		pr_info("name=%s, value=%s\n", pt->parent->name, val);
+	}
+
+}
+
 int sync_fence_wait(struct sync_fence *fence, long timeout)
 {
 	long ret;
@@ -392,6 +407,7 @@ int sync_fence_wait(struct sync_fence *fence, long timeout)
 		if (timeout) {
 			pr_info("fence timeout on [%p] after %dms\n", fence,
 				jiffies_to_msecs(timeout));
+			sync_fence_dump(fence);
 			sync_dump();
 		}
 		return -ETIME;
@@ -400,6 +416,7 @@ int sync_fence_wait(struct sync_fence *fence, long timeout)
 	ret = atomic_read(&fence->status);
 	if (ret) {
 		pr_info("fence error %ld on [%p]\n", ret, fence);
+		sync_fence_dump(fence);
 		sync_dump();
 	}
 	return ret;
