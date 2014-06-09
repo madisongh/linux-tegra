da9b0aa844f4ec5fffed83e2d9a447aa0557c4a9
Chaitanya Bandi <bandik@nvidia.com>
2014-06-09 17:49:39 +0530

    regmap: irq: Add pre_irq and post_irq support
    Added pre_irq and post_irq support to regmap_irq.
    These are callbacks that enable irq chips to perform
    any chip specific programming before and after main
    interrupt handling in regmap_irq_handler respectively.
    
    Change-Id: I136b5e5c71b0d0c89247043e99a059f5adfb7c69
    Signed-off-by: Chaitanya Bandi <bandik@nvidia.com>
    Reviewed-on: http://git-master/r/420661
    (cherry picked from commit 25dcf0dd708413e5f2bf7473cca2f291af14a0c5)
    Reviewed-on: http://git-master/r/422099
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- drivers/base/regmap/regmap-irq.c
+++ drivers/base/regmap/regmap-irq.c
@@ -276,6 +276,9 @@ static irqreturn_t regmap_irq_thread(int irq, void *d)
 		goto exit;
 	}
 
+	if (chip->pre_irq && chip->pre_post_irq_data)
+		chip->pre_irq(chip->pre_post_irq_data);
+
 	if (chip->runtime_pm) {
 		ret = pm_runtime_get_sync(map->dev);
 		if (ret < 0) {
@@ -379,6 +382,9 @@ static irqreturn_t regmap_irq_thread(int irq, void *d)
 		pm_runtime_put(map->dev);
 
 exit:
+	if (chip->post_irq && chip->pre_post_irq_data)
+		chip->post_irq(chip->pre_post_irq_data);
+
 	mutex_unlock(&data->shutdown_lock);
 	if (handled)
 		return IRQ_HANDLED;
