beba51e75d048425227c972f0aa3193427f7a660
Dan Willemsen <dwillemsen@nvidia.com>
2014-06-09 21:40:21 -0700

    net: wireless: bcmdhd: Remove __DATE__ and __TIME__
    These have been deprecated in 3.14, to help with deterministic builds.
    
    Signed-off-by: Dan Willemsen <dwillemsen@nvidia.com>

--- drivers/net/wireless/bcmdhd/dhd_common.c
+++ drivers/net/wireless/bcmdhd/dhd_common.c
@@ -106,7 +106,7 @@ bool ap_fw_loaded = FALSE;
 
 #if defined(DHD_DEBUG)
 const char dhd_version[] = "Dongle Host Driver, version " EPI_VERSION_STR
-	DHD_COMPILED " on " __DATE__ " at " __TIME__;
+	DHD_COMPILED;
 #else
 const char dhd_version[] = "\nDongle Host Driver, version " EPI_VERSION_STR "\nCompiled from ";
 #endif 
