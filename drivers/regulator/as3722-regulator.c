a3c6dd4bbffc097de152f5ed2baafba7fae102f4
Bibek Basu <bbasu@nvidia.com>
2014-06-11 16:20:00 +0530

    regulator: as3722: adjust enable_time for regulators
    According to measurement done, LDOs take a maximum of
    100us to reach it max desired value.Thus this patch adjusts
    the default enable time for as3722 regulators
    
    Bug 1481647
    
    Change-Id: Ia610e0a11f1001fdf76b2860b6e2b3d6ed0a1b1a
    Signed-off-by: Bibek Basu <bbasu@nvidia.com>
    Reviewed-on: http://git-master/r/422152
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- drivers/regulator/as3722-regulator.c
+++ drivers/regulator/as3722-regulator.c
@@ -810,7 +810,7 @@ static int as3722_regulator_probe(struct platform_device *pdev)
 			as3722_regs->desc[id].min_uV = 825000;
 			as3722_regs->desc[id].uV_step = 25000;
 			as3722_regs->desc[id].linear_min_sel = 1;
-			as3722_regs->desc[id].enable_time = 500;
+			as3722_regs->desc[id].enable_time = 150;
 			break;
 		case AS3722_REGULATOR_ID_LDO3:
 			if (reg_config->ext_control)
@@ -820,7 +820,7 @@ static int as3722_regulator_probe(struct platform_device *pdev)
 			as3722_regs->desc[id].min_uV = 620000;
 			as3722_regs->desc[id].uV_step = 20000;
 			as3722_regs->desc[id].linear_min_sel = 1;
-			as3722_regs->desc[id].enable_time = 500;
+			as3722_regs->desc[id].enable_time = 350;
 			if (reg_config->enable_tracking) {
 				ret = as3722_ldo3_set_tracking_mode(as3722_regs,
 					id, AS3722_LDO3_MODE_PMOS_TRACKING);
@@ -874,7 +874,7 @@ static int as3722_regulator_probe(struct platform_device *pdev)
 				ops = &as3722_ldo_extcntrl_ops;
 			else
 				ops = &as3722_ldo_ops;
-			as3722_regs->desc[id].enable_time = 500;
+			as3722_regs->desc[id].enable_time = 150;
 			as3722_regs->desc[id].linear_ranges = as3722_ldo_ranges;
 			as3722_regs->desc[id].n_linear_ranges =
 						ARRAY_SIZE(as3722_ldo_ranges);
