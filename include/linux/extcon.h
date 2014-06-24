b26e96a61fc2efa730eb16106cc72f8231b24473
Laxman Dewangan <ldewangan@nvidia.com>
2014-06-24 17:14:35 +0530

    extcon: extcon-class: add DT support based on cable name
    Add DT support to register the extcon notification based on
    cable names - extcon handle properties.
    The client can get the cable information through its node or extcon name
    and once cable infomration is available, it can register for notification
    when state gets changed.
    
    The typical dt nodes are:
    Driver node:
    	extcon: arizona-extcon {
    		compatible = "wlf,arizona-extcon";
    		#extcon-cells = <1>;
    	};
    Driver need to specifi the cable ID as
     Cable			ID
     ----------------------------
      Mechanical		0
      Microphone		1
      Headphone		2
      Line-out		3
    
    Here #extcon-cells is must and specifies teh size of extcon cells.
    
    Client node:
    Client node can be register it as:
    	audio-controller@b0000 {
    		reg = <0xb0000 0x2210>;
    		interrupts = <19>, <20>;
    		extcon-cables = <&extcon 1>, <&extcon 3>;
    		extcon-cable-names = "Microphone", "Line-out";
    	};
    
    and client driver can register the cable by passing the cable name
    as above along with its node.
    
    Change-Id: Ia9328d399a8188ba22fecc4bd0cc39a3749b4fbd
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/428204

--- include/linux/extcon.h
+++ include/linux/extcon.h
@@ -78,6 +78,7 @@ struct extcon_cable;
  * struct extcon_dev - An extcon device represents one external connector.
  * @name:		The name of this extcon device. Parent device name is
  *			used if NULL.
+ * @node:		Devicetree node of parent device.
  * @supported_cable:	Array of supported cable names ending with NULL.
  *			If supported_cable is NULL, cable name related APIs
  *			are disabled.
@@ -113,6 +114,7 @@ struct extcon_cable;
 struct extcon_dev {
 	/* Optional user initializing data */
 	const char *name;
+	const struct device_node *node;
 	const char **supported_cable;
 	const u32 *mutually_exclusive;
 
@@ -190,6 +192,12 @@ extern int devm_extcon_dev_register(struct device *dev,
 extern void devm_extcon_dev_unregister(struct device *dev,
 				       struct extcon_dev *edev);
 extern struct extcon_dev *extcon_get_extcon_dev(const char *extcon_name);
+extern struct extcon_cable *extcon_get_extcon_cable(struct device *dev,
+		const char *cable_name);
+extern struct extcon_cable *extcon_get_extcon_cable_by_extcon_name(
+		const char *extcon_name, const char *cable_name);
+extern struct extcon_dev *extcon_get_extcon_dev_by_cable(struct device *dev,
+			const char *cable_name);
 
 /*
  * Following APIs control the memory of extcon device.
@@ -242,6 +250,8 @@ extern int extcon_register_interest(struct extcon_specific_cable_nb *obj,
 				    struct notifier_block *nb);
 extern int extcon_unregister_interest(struct extcon_specific_cable_nb *nb);
 
+extern int  extcon_register_cable_interest(struct extcon_specific_cable_nb *obj,
+		struct extcon_cable *ext_cable, struct notifier_block *nb);
 /*
  * Following APIs are to monitor every action of a notifier.
  * Registrar gets notified for every external port of a connection device.
@@ -342,6 +352,25 @@ static inline struct extcon_dev *extcon_get_extcon_dev(const char *extcon_name)
 	return NULL;
 }
 
+static inline struct extcon_cable *extcon_get_extcon_cable(struct device *dev,
+		const char *cable_name)
+{
+	return NULL;
+}
+
+static inline struct extcon_cable *extcon_get_extcon_cable_by_extcon_name(
+		const char *extcon_name, const char *cable_name)
+{
+	return NULL;
+}
+
+static inline struct extcon_dev *extcon_get_extcon_dev_by_cable(
+		struct device *dev, const char *cable_name)
+
+{
+	return NULL;
+}
+
 static inline int extcon_register_notifier(struct extcon_dev *edev,
 					   struct notifier_block *nb)
 {
@@ -364,6 +393,13 @@ static inline int extcon_register_interest(struct extcon_specific_cable_nb *obj,
 
 static inline int extcon_unregister_interest(struct extcon_specific_cable_nb
 						    *obj)
+{
+	return 0;
+}
+
+static inline int extcon_register_cable_interest(
+	struct extcon_specific_cable_nb *obj,
+	struct extcon_cable *ext_cable, struct notifier_block *nb)
 {
 	return 0;
 }
