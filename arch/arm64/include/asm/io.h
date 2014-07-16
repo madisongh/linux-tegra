929564515b728be27c3578e7e9dab673facd6a12
David Yu <davyu@nvidia.com>
2014-07-16 12:09:14 +0900

    fs: pstore: Add support for register trace
    With this option kernel traces register read/write calls into a
    persistent ram buffer that can be decoded and dumped after reboot
    through pstore filesystem. It can be used to determine what function
    was last called before a reset or panic.
    
    Bug 200018413
    
    Change-Id: Ie7896da3661615b439b938f43c2a50ec74ebdda0
    Signed-off-by: David Yu <davyu@nvidia.com>
    Reviewed-on: http://git-master/r/438470
    Reviewed-by: Mitch Luban <mluban@nvidia.com>
    Tested-by: Mitch Luban <mluban@nvidia.com>

--- arch/arm64/include/asm/io.h
+++ arch/arm64/include/asm/io.h
@@ -3,6 +3,7 @@
  *
  * Copyright (C) 1996-2000 Russell King
  * Copyright (C) 2012 ARM Ltd.
+ * Copyright (C) 2014 NVIDIA Corporation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -23,6 +24,7 @@
 
 #include <linux/types.h>
 #include <linux/blk_types.h>
+#include <linux/pstore.h>
 
 #include <asm/byteorder.h>
 #include <asm/barrier.h>
@@ -36,27 +38,32 @@
  */
 static inline void __raw_writeb(u8 val, volatile void __iomem *addr)
 {
+	pstore_rtrace_call(RTRACE_WRITE, (void __force *)addr);
 	asm volatile("strb %w0, [%1]" : : "r" (val), "r" (addr));
 }
 
 static inline void __raw_writew(u16 val, volatile void __iomem *addr)
 {
+	pstore_rtrace_call(RTRACE_WRITE, (void __force *)addr);
 	asm volatile("strh %w0, [%1]" : : "r" (val), "r" (addr));
 }
 
 static inline void __raw_writel(u32 val, volatile void __iomem *addr)
 {
+	pstore_rtrace_call(RTRACE_WRITE, (void __force *)addr);
 	asm volatile("str %w0, [%1]" : : "r" (val), "r" (addr));
 }
 
 static inline void __raw_writeq(u64 val, volatile void __iomem *addr)
 {
+	pstore_rtrace_call(RTRACE_WRITE, (void __force *)addr);
 	asm volatile("str %0, [%1]" : : "r" (val), "r" (addr));
 }
 
 static inline u8 __raw_readb(const volatile void __iomem *addr)
 {
 	u8 val;
+	pstore_rtrace_call(RTRACE_READ, (void __force *)addr);
 	asm volatile("ldrb %w0, [%1]" : "=r" (val) : "r" (addr));
 	return val;
 }
@@ -64,6 +71,7 @@ static inline u8 __raw_readb(const volatile void __iomem *addr)
 static inline u16 __raw_readw(const volatile void __iomem *addr)
 {
 	u16 val;
+	pstore_rtrace_call(RTRACE_READ, (void __force *)addr);
 	asm volatile("ldrh %w0, [%1]" : "=r" (val) : "r" (addr));
 	return val;
 }
@@ -71,6 +79,7 @@ static inline u16 __raw_readw(const volatile void __iomem *addr)
 static inline u32 __raw_readl(const volatile void __iomem *addr)
 {
 	u32 val;
+	pstore_rtrace_call(RTRACE_READ, (void __force *)addr);
 	asm volatile("ldr %w0, [%1]" : "=r" (val) : "r" (addr));
 	return val;
 }
@@ -78,6 +87,7 @@ static inline u32 __raw_readl(const volatile void __iomem *addr)
 static inline u64 __raw_readq(const volatile void __iomem *addr)
 {
 	u64 val;
+	pstore_rtrace_call(RTRACE_READ, (void __force *)addr);
 	asm volatile("ldr %0, [%1]" : "=r" (val) : "r" (addr));
 	return val;
 }
