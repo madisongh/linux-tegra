c0c14acf3fc46a2a4b153fb24baeedbe2ad6a4cf
Bibek Basu <bbasu@nvidia.com>
2014-08-12 14:38:57 +0530

    arm: tegra12: initialize pll_a before being used
    i2s clock tries to lock pll_a during clock init without
    initializing pll_a in kernel. Patch fixes that by initializing pll_a
    
    Bug 200027051
    
    Change-Id: Ia4ba705ea1bb57e4d462854c24551ee3d3af4718
    Signed-off-by: Bibek Basu <bbasu@nvidia.com>
    Reviewed-on: http://git-master/r/455472
    Reviewed-by: Winnie Hsu <whsu@nvidia.com>
    Tested-by: Winnie Hsu <whsu@nvidia.com>

--- arch/arm/mach-tegra/board-ardbeg.c
+++ arch/arm/mach-tegra/board-ardbeg.c
@@ -170,6 +170,7 @@ static __initdata struct tegra_clk_init_table ardbeg_clk_init_table[] = {
 	{ "hda",	"pll_p",	108000000,	false},
 	{ "hda2codec_2x", "pll_p",	48000000,	false},
 	{ "pwm",	"pll_p",	48000000,	false},
+	{ "pll_a",	"pll_p_out1",	282240000,	false},
 	{ "i2s1",	"pll_a_out0",	0,		false},
 	{ "i2s3",	"pll_a_out0",	0,		false},
 	{ "i2s4",	"pll_a_out0",	0,		false},
