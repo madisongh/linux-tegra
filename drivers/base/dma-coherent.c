eb208d095d1822a9143e7c68ae11fc7eac5db8e1
Vandana Salve <vsalve@nvidia.com>
2014-09-15 20:33:22 +0530

    dma:coherent: API dma_get_coherent_stats added
    API to get base and size of dma coherent memory
    
    bug 1519517
    
    Change-Id: I72a2b92c2fcdad9a672f7b338f1ab9e7f9fe4c90
    Signed-off-by: Vandana Salve <vsalve@nvidia.com>
    Reviewed-on: http://git-master/r/498888
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-by: Hiroshi Doyu <hdoyu@nvidia.com>

--- drivers/base/dma-coherent.c
+++ drivers/base/dma-coherent.c
@@ -853,6 +853,30 @@ int dma_mmap_from_coherent(struct device *dev, struct vm_area_struct *vma,
 }
 EXPORT_SYMBOL(dma_mmap_from_coherent);
 
+int dma_get_coherent_stats(struct device *dev,
+			struct dma_coherent_stats *stats)
+{
+	struct heap_info *h = NULL;
+	struct dma_coherent_mem *mem = dev->dma_mem;
+
+	if ((!dev) || !stats)
+		return -EINVAL;
+
+	h = dev_get_drvdata(dev);
+	if (h && (h->magic == RESIZE_MAGIC)) {
+		stats->size = h->curr_len;
+		stats->base = h->curr_base;
+		goto out;
+	}
+
+	if (!mem)
+		return -EINVAL;
+	stats->size = mem->size << PAGE_SHIFT;
+	stats->base = mem->device_base;
+out:
+	return 0;
+}
+
 /*
  * Support for reserved memory regions defined in device tree
  */
