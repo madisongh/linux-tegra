4d6308a1e37010df3f3b4f4014f9cf2f8d8365d5
navneet kumar <navneetk@nvidia.com>
2014-10-27 12:36:26 -0700

    thermal: of: consolidate sensor callbacks as ops
    Consolidate all the sensor callbacks (get_temp/get_trend)
    into a 'thermal_of_sensor_ops' struct.
    
    As a part of this, introduce a 'thermal_zone_of_sensor_register2'
    sensor API that accepts sensor_ops instead of the two callbacks
    as separate arguments to the register function.
    
    Modify the older version of register function to call register2.
    
    Adjust all the references to get_temp/get_trend callbacks.
    
    Change-Id: I505d568dcfac86646cf75e3d13c4d11b863f19bd
    Signed-off-by: navneet kumar <navneetk@nvidia.com>
    Reviewed-on: http://git-master/r/590501
    Reviewed-by: Hyungwoo Yang <hyungwooy@nvidia.com>
    Reviewed-by: Diwakar Tundlam <dtundlam@nvidia.com>

--- include/linux/thermal.h
+++ include/linux/thermal.h
@@ -301,6 +301,11 @@ struct thermal_genl_event {
 	int temp;
 };
 
+struct thermal_of_sensor_ops {
+	int (*get_temp)(void *, long *);
+	int (*get_trend)(void *, long *);
+};
+
 /* Function declarations */
 #ifdef CONFIG_THERMAL_OF
 struct thermal_zone_device *
@@ -309,6 +314,9 @@ thermal_zone_of_sensor_register(struct device *dev, int id,
 				int (*get_trend)(void *, long *));
 void thermal_zone_of_sensor_unregister(struct device *dev,
 				       struct thermal_zone_device *tz);
+struct thermal_zone_device *
+thermal_zone_of_sensor_register2(struct device *dev, int sensor_id,
+				void *data, struct thermal_of_sensor_ops *sops);
 #else
 static inline struct thermal_zone_device *
 thermal_zone_of_sensor_register(struct device *dev, int id,
@@ -324,6 +332,12 @@ void thermal_zone_of_sensor_unregister(struct device *dev,
 {
 }
 
+static inline struct thermal_zone_device *
+thermal_zone_of_sensor_register2(struct device *dev, int sensor_id,
+				void *data, struct thermal_of_sensor_ops *sops)
+{
+	return NULL;
+}
 #endif
 struct thermal_zone_device *thermal_zone_device_register(const char *, int, u64,
 		void *, struct thermal_zone_device_ops *,
