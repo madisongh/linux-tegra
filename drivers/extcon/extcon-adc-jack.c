326b72dea24fd193ed07ebfe3987a65b47d926a1
Mallikarjun Kasoju <mkasoju@nvidia.com>
2014-11-05 17:11:26 +0530

    extcon: adc-jack: Add debounce support
    Add debounce support to make sure ADC values are not read
    immediately after getting interrupt.
    
    Bug 1555564
    
    Change-Id: I24ee2f5c192ce384b26151c946abb6a157695c5c
    Signed-off-by: Mallikarjun Kasoju <mkasoju@nvidia.com>

--- drivers/extcon/extcon-adc-jack.c
+++ drivers/extcon/extcon-adc-jack.c
@@ -51,7 +51,9 @@ struct adc_jack_data {
 
 	int irq;
 	unsigned long handling_delay; /* in jiffies */
+	int debounce_jiffies;
 	struct delayed_work handler;
+	struct timer_list timer;
 
 	struct iio_channel *chan;
 };
@@ -88,12 +90,23 @@ static void adc_jack_handler(struct work_struct *work)
 	dev_info(&data->edev->dev, "Cable State 0x%02X\n", state);
 }
 
+static void ecx_extcon_notifier_timer(unsigned long _data)
+{
+	struct adc_jack_data *data = (struct adc_jack_data *)_data;
+
+	schedule_delayed_work(&data->handler, data->handling_delay);
+}
+
 static irqreturn_t adc_jack_irq_thread(int irq, void *_data)
 {
 	struct adc_jack_data *data = _data;
 
-	queue_delayed_work(system_power_efficient_wq,
-			   &data->handler, data->handling_delay);
+	if (data->debounce_jiffies)
+		mod_timer(&data->timer, jiffies + data->debounce_jiffies);
+	else
+		queue_delayed_work(system_power_efficient_wq,
+				   &data->handler, data->handling_delay);
+
 	return IRQ_HANDLED;
 }
 
@@ -130,6 +143,10 @@ static struct adc_jack_pdata *of_get_platform_data(
 	ret = of_property_read_u32(np, "extcon-adc-jack,handling-delay", &pval);
 	if (!ret)
 		pdata->handling_delay_ms = pval;
+
+	ret = of_property_read_u32(np, "extcon-adc-jack,debounce-ms", &pval);
+	if (!ret)
+		pdata->debounce_ms = pval;
 
 	nstates = of_property_count_u32(np, "extcon-adc-jack,states");
 	if (nstates < 0)
@@ -234,8 +251,11 @@ static int adc_jack_probe(struct platform_device *pdev)
 		return PTR_ERR(data->chan);
 
 	data->handling_delay = msecs_to_jiffies(pdata->handling_delay_ms);
+	data->debounce_jiffies = msecs_to_jiffies(pdata->debounce_ms);
 
 	INIT_DEFERRABLE_WORK(&data->handler, adc_jack_handler);
+	setup_timer(&data->timer,
+		ecx_extcon_notifier_timer, (unsigned long)data);
 
 	platform_set_drvdata(pdev, data);
 
