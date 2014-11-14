137a91de0b9adbfb19fa70626b90b7476adf3abe
Andre Przywara <andre.przywara@arm.com>
2014-11-14 15:54:11 +0000

    arm64: add Cortex-A57 erratum 832075 workaround
    The ARM erratum 832075 applies to certain revisions of Cortex-A57,
    one of the workarounds is to change device loads into using
    load-aquire semantics.
    This is achieved using the alternatives framework.
    
    Change-Id: Ia8d0c7c07f6489fdf13e6c9877a0b3cd70813e40
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Reviewed-on: http://git-master/r/721976
    Tested-by: Bo Yan <byan@nvidia.com>
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bo Yan <byan@nvidia.com>

--- arch/arm64/include/asm/io.h
+++ arch/arm64/include/asm/io.h
@@ -30,6 +30,8 @@
 #include <asm/barrier.h>
 #include <asm/pgtable.h>
 #include <asm/early_ioremap.h>
+#include <asm/alternative.h>
+#include <asm/cpufeature.h>
 
 #include <xen/xen.h>
 
@@ -64,7 +66,10 @@ static inline u8 __raw_readb(const volatile void __iomem *addr)
 {
 	u8 val;
 	pstore_rtrace_call(RTRACE_READ, (void __force *)addr, 0);
-	asm volatile("ldrb %w0, [%1]" : "=r" (val) : "r" (addr));
+	asm volatile(ALTERNATIVE("ldrb %w0, [%1]",
+				 "ldarb %w0, [%1]",
+				 ARM64_WORKAROUND_DEVICE_LOAD_ACQUIRE)
+		     : "=r" (val) : "r" (addr));
 	pstore_rtrace_call(RTRACE_READ, (void __force *)addr, (long)val);
 	return val;
 }
@@ -72,8 +77,12 @@ static inline u8 __raw_readb(const volatile void __iomem *addr)
 static inline u16 __raw_readw(const volatile void __iomem *addr)
 {
 	u16 val;
+
 	pstore_rtrace_call(RTRACE_READ, (void __force *)addr, 0);
-	asm volatile("ldrh %w0, [%1]" : "=r" (val) : "r" (addr));
+	asm volatile(ALTERNATIVE("ldrh %w0, [%1]",
+				 "ldarh %w0, [%1]",
+				 ARM64_WORKAROUND_DEVICE_LOAD_ACQUIRE)
+		     : "=r" (val) : "r" (addr));
 	return val;
 }
 
@@ -81,7 +90,10 @@ static inline u32 __raw_readl(const volatile void __iomem *addr)
 {
 	u32 val;
 	pstore_rtrace_call(RTRACE_READ, (void __force *)addr, 0);
-	asm volatile("ldr %w0, [%1]" : "=r" (val) : "r" (addr));
+	asm volatile(ALTERNATIVE("ldr %w0, [%1]",
+				 "ldar %w0, [%1]",
+				 ARM64_WORKAROUND_DEVICE_LOAD_ACQUIRE)
+		     : "=r" (val) : "r" (addr));
 	return val;
 }
 
@@ -89,7 +101,10 @@ static inline u64 __raw_readq(const volatile void __iomem *addr)
 {
 	u64 val;
 	pstore_rtrace_call(RTRACE_READ, (void __force *)addr, 0);
-	asm volatile("ldr %0, [%1]" : "=r" (val) : "r" (addr));
+	asm volatile(ALTERNATIVE("ldr %0, [%1]",
+				 "ldar %0, [%1]",
+				 ARM64_WORKAROUND_DEVICE_LOAD_ACQUIRE)
+		     : "=r" (val) : "r" (addr));
 	return val;
 }
 
