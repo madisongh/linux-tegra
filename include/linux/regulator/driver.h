17ace258415a0d4bb33b382a13901504ad6a4634
Laxman Dewangan <ldewangan@nvidia.com>
2015-01-08 18:52:29 +0530

    regulator: core: add support to dump complete power tree, rail states/voltages
    Add support to dump complete power tree, rail status and its voltage
    from booted kernel through debugfs sysfs.
    
    Added 3 debugsfs as
    /d/power_tree
    /d/rail_states
    /d/rail_voltages
    
    This help to get the correctness of configuration and re-verification
    of platform power tree and its configuration.
    
    Change-Id: Id33e8bb3ef9738a9e8b974701699be89aee892f8
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/670684
    GVS: Gerrit_Virtual_Submit

--- include/linux/regulator/driver.h
+++ include/linux/regulator/driver.h
@@ -376,6 +376,7 @@ struct regulator_dev {
 	struct regulation_constraints *constraints;
 	struct regulator *supply;	/* for tree */
 	struct regmap *regmap;
+	int machine_constraints;
 
 	struct delayed_work disable_work;
 	int deferred_disables;
@@ -383,6 +384,7 @@ struct regulator_dev {
 	void *reg_data;		/* regulator_dev data */
 
 	struct dentry *debugfs;
+	struct dentry *pdebugfs;
 
 	struct regulator_time_profile set_volt_profile;
 
