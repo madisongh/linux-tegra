95df123acf13f204ef24f9c6482d8c9c5dce2f06
Seshagiri.Holi <sholi@nvidia.com>
2015-01-27 15:59:00 +0530

    mmc: sd: Fall back to HS mode when tuning fails
    bug 1582100
    
    Signed-off-by: Seshagiri.Holi <sholi@nvidia.com>
    Reviewed-on: http://git-master/r/677693
    (cherry picked from commit 48e5ea056c682fe242b8d372d871ea7483931753)
    Change-Id: I276a1823d6c6f300e879486b79e0f4974029cc64
    Signed-off-by: Pavan Kunapuli <pkunapuli@nvidia.com>
    Reviewed-on: http://git-master/r/740021
    Reviewed-by: Automatic_Commit_Validation_User

--- drivers/mmc/core/sdio.c
+++ drivers/mmc/core/sdio.c
@@ -641,8 +641,8 @@ try_again:
 		goto err;
 	}
 
-	if ((rocr & R4_MEMORY_PRESENT) &&
-	    mmc_sd_get_cid(host, ocr & rocr, card->raw_cid, NULL) == 0) {
+	if ((ocr & R4_MEMORY_PRESENT) &&
+		mmc_sd_get_cid(host, ocr & ocr, card->raw_cid, NULL, 1) == 0) {
 		card->type = MMC_TYPE_SD_COMBO;
 
 		if (oldcard && (oldcard->type != MMC_TYPE_SD_COMBO ||
