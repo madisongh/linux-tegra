InES-HPMM/linux-l4t

regulator: core: support for state change from sysfs for gpio regulators
Add support to enable/disable from sysfs for gpio based fixed regulators.

bug 1436543

Change-Id: Ic7d1d9fa73b40b1db8f7f824d4bb53624fae00dc
Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
Reviewed-on: http://git-master/r/358749
GVS: Gerrit_Virtual_Submit
Reviewed-by: Pankaj Dabade <pdabade@nvidia.com>
Tested-by: Pankaj Dabade <pdabade@nvidia.com>

drivers/regulator/core.c
@@ -463,18 +463,27 @@ static ssize_t regulator_state_set(struct device *dev,
 	mutex_lock(&rdev->mutex);
 	if (enabled) {
 		int delay = 0;
-		if (!rdev->desc->ops->enable) {
+		if (!rdev->desc->ops->enable && !rdev->ena_pin) {
+			rdev_warn(rdev, "Ops not supported\n");
 			ret = -EINVAL;
 			goto end;
 		}
 		ret = _regulator_get_enable_time(rdev);
 		if (ret >= 0)
 			delay = ret;
-		ret = rdev->desc->ops->enable(rdev);
-		if (ret < 0) {
-			rdev_warn(rdev, "enable() failed: %d\n", ret);
-			goto end;
+
+		if (rdev->ena_pin) {
+			gpio_set_value_cansleep(rdev->ena_pin->gpio,
+					!rdev->ena_pin->ena_gpio_invert);
+			rdev->ena_gpio_state = 1;
+		} else if (rdev->desc->ops->enable) {
+			ret = rdev->desc->ops->enable(rdev);
+			if (ret < 0) {
+				rdev_warn(rdev, "enable() failed: %d\n", ret);
+				goto end;
+			}
 		}
+
 		if (delay >= 1000) {
 			mdelay(delay / 1000);
 			udelay(delay % 1000);
@@ -483,18 +492,27 @@ static ssize_t regulator_state_set(struct device *dev,
 		}
 	} else {
 		int delay = 0;
-		if (!rdev->desc->ops->disable) {
+		if (!rdev->desc->ops->disable && !rdev->ena_pin) {
+			rdev_warn(rdev, "Ops not supported\n");
 			ret = -EINVAL;
 			goto end;
 		}
 		ret = _regulator_get_disable_time(rdev);
 		if (ret >= 0)
 			delay = ret;
-		ret = rdev->desc->ops->disable(rdev);
-		if (ret < 0) {
-			rdev_warn(rdev, "disable() failed: %d\n", ret);
-			goto end;
+
+		if (rdev->ena_pin) {
+			gpio_set_value_cansleep(rdev->ena_pin->gpio,
+					rdev->ena_pin->ena_gpio_invert);
+			rdev->ena_gpio_state = 0;
+		} else if (rdev->desc->ops->disable) {
+			ret = rdev->desc->ops->disable(rdev);
+			if (ret < 0) {
+				rdev_warn(rdev, "disable() failed: %d\n", ret);
+				goto end;
+			}
 		}
+
 		if (delay >= 1000) {
 			mdelay(delay / 1000);
 			udelay(delay % 1000);

madisongh/linux-tegra

drivers/regulator/core.c
@@ -464,18 +464,27 @@ static ssize_t regulator_state_set(struct device *dev,
 	mutex_lock(&rdev->mutex);
 	if (enabled) {
 		int delay = 0;
-		if (!rdev->desc->ops->enable) {
+		if (!rdev->desc->ops->enable && !rdev->ena_pin) {
+			rdev_warn(rdev, "Ops not supported\n");
 			ret = -EINVAL;
 			goto end;
 		}
 		ret = _regulator_get_enable_time(rdev);
 		if (ret >= 0)
 			delay = ret;
-		ret = rdev->desc->ops->enable(rdev);
-		if (ret < 0) {
-			rdev_warn(rdev, "enable() failed: %d\n", ret);
-			goto end;
+
+		if (rdev->ena_pin) {
+			gpiod_set_value_cansleep(rdev->ena_pin->gpiod,
+					!rdev->ena_pin->ena_gpio_invert);
+			rdev->ena_gpio_state = 1;
+		} else if (rdev->desc->ops->enable) {
+			ret = rdev->desc->ops->enable(rdev);
+			if (ret < 0) {
+				rdev_warn(rdev, "enable() failed: %d\n", ret);
+				goto end;
+			}
 		}
+
 		if (delay >= 1000) {
 			mdelay(delay / 1000);
 			udelay(delay % 1000);
@@ -483,14 +492,21 @@ static ssize_t regulator_state_set(struct device *dev,
 			udelay(delay);
 		}
 	} else {
-		if (!rdev->desc->ops->disable) {
+		if (!rdev->desc->ops->disable && !rdev->ena_pin) {
+			rdev_warn(rdev, "Ops not supported\n");
 			ret = -EINVAL;
 			goto end;
 		}
-		ret = rdev->desc->ops->disable(rdev);
-		if (ret < 0) {
-			rdev_warn(rdev, "disable() failed: %d\n", ret);
-			goto end;
+		if (rdev->ena_pin) {
+			gpiod_set_value_cansleep(rdev->ena_pin->gpiod,
+					rdev->ena_pin->ena_gpio_invert);
+			rdev->ena_gpio_state = 0;
+		} else if (rdev->desc->ops->disable) {
+			ret = rdev->desc->ops->disable(rdev);
+			if (ret < 0) {
+				rdev_warn(rdev, "disable() failed: %d\n", ret);
+				goto end;
+			}
 		}
 	}
 