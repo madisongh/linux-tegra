danielhk/android_kernel_asus_tegra

drivers: net: Modifications for RMC PegaPCI. * As submitted by RMC for modem support *
Bug 1054808

Change-Id: I0fc2974dd3ef4481cd4fb06871e034328423f163
Signed-off-by: Raj Jayaraman <rjayaraman@nvidia.com>
Reviewed-on: http://git-master/r/160032
Reviewed-by: Automatic_Commit_Validation_User
Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

include/linux/if_ether.h
@@ -119,6 +119,9 @@
 #define ETH_P_PHONET	0x00F5		/* Nokia Phonet frames          */
 #define ETH_P_IEEE802154 0x00F6		/* IEEE802.15.4 frame		*/
 #define ETH_P_CAIF	0x00F7		/* ST-Ericsson CAIF protocol	*/
+#define ETH_P_MHI	0x00F8		/* Renesas MHI protocol         */
+#define ETH_P_RAW	0x00F9		/* RAW access to frames         */
+#define ETH_P_MHDP	0x00FA		/* MHDP data frames             */
 
 /*
  *	This is an Ethernet frame header.

madisongh/linux-tegra

Change-Id: Ia25f0d6aaea899cb327cb2141d6896a508238c0c
(cherry picked from commit a81627cafe577dc5f640feb11596d485200c4c0d)
Reviewed-on: http://git-master/r/162292
Reviewed-by: WK Tsai <wtsai@nvidia.com>
Reviewed-by: Steve Lin <stlin@nvidia.com>

include/uapi/linux/if_ether.h
@@ -129,6 +129,9 @@
 #define ETH_P_IEEE802154 0x00F6		/* IEEE802.15.4 frame		*/
 #define ETH_P_CAIF	0x00F7		/* ST-Ericsson CAIF protocol	*/
 #define ETH_P_XDSA	0x00F8		/* Multiplexed DSA protocol	*/
+#define ETH_P_MHI	0x00F9		/* Renesas MHI protocol         */
+#define ETH_P_RAW	0x00FA		/* RAW access to frames         */
+#define ETH_P_MHDP	0x00FB		/* MHDP data frames             */
 
 /*
  *	This is an Ethernet frame header.