InES-HPMM/linux-l4t

arm64: mm: dma: support DMA32 coherent allocations
Support DMA32 coherent allocations for devices with 32-bit mask.
Bug 1454889

Change-Id: Ieaa996166761c6057eedc9d6e4e1ffc3a3f11d69
Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
Reviewed-on: http://git-master/r/441917
Reviewed-by: Sri Krishna Chowdary <schowdary@nvidia.com>
GVS: Gerrit_Virtual_Submit

arch/arm64/mm/dma-mapping.c
@@ -262,7 +262,7 @@ static u64 get_coherent_dma_mask(struct device *dev)
 
 		if ((~mask) & (u64)arm_dma_limit) {
 			dev_warn(dev, "coherent DMA mask %#llx is smaller "
-				 "than system GFP_DMA mask %#llx\n",
+				 "than system GFP_DMA32 mask %#llx\n",
 				 mask, (u64)arm_dma_limit);
 			return 0;
 		}
@@ -715,8 +715,18 @@ static void *__dma_alloc(struct device *dev, size_t size, dma_addr_t *handle,
 	if (!mask)
 		return NULL;
 
-	if (mask < 0xffffffffULL)
-		gfp |= GFP_DMA;
+#ifdef CONFIG_ZONE_DMA32
+	if (mask == (u64)arm_dma_limit) {
+		if ((gfp & GFP_ZONEMASK) != GFP_DMA32 &&
+		    (gfp & GFP_ZONEMASK) != 0) {
+			dev_warn(dev, "Invalid GFP flags(%x) passed. "
+				"GFP_DMA32 should only be set.",
+				 gfp & GFP_ZONEMASK);
+			return NULL;
+		}
+		gfp |= GFP_DMA32;
+	}
+#endif
 
 	/*
 	 * Following is a work-around (a.k.a. hack) to prevent pages

arch/arm64/mm/mm.h
@@ -11,4 +11,8 @@ static inline pmd_t *pmd_off_k(unsigned long virt)
 /* consistent regions used by dma_alloc_attrs() */
 #define VM_ARM_DMA_CONSISTENT  0x20000000
 
+#ifdef CONFIG_ZONE_DMA32
+#define arm_dma_limit (0xffffffff)
+#else
 #define arm_dma_limit ((phys_addr_t)~0)
+#endif

madisongh/linux-tegra

arch/arm64/mm/dma-mapping.c
@@ -887,8 +887,18 @@ static void *__dma_alloc(struct device *dev, size_t size, dma_addr_t *handle,
 	if (!mask)
 		return NULL;
 
-	if (mask < 0xffffffffULL)
+#ifdef CONFIG_ZONE_DMA
+	if (mask == (u64)arm_dma_limit) {
+		if ((gfp & GFP_ZONEMASK) != GFP_DMA &&
+		    (gfp & GFP_ZONEMASK) != 0) {
+			dev_warn(dev, "Invalid GFP flags(%x) passed. "
+				"GFP_DMA should only be set.",
+				 gfp & GFP_ZONEMASK);
+			return NULL;
+		}
 		gfp |= GFP_DMA;
+	}
+#endif
 
 	/*
 	 * Following is a work-around (a.k.a. hack) to prevent pages

arch/arm64/mm/mm.h
@@ -10,4 +10,8 @@ static inline pmd_t *pmd_off_k(unsigned long virt)
 /* consistent regions used by dma_alloc_attrs() */
 #define VM_ARM_DMA_CONSISTENT  0x20000000
 
+#ifdef CONFIG_ZONE_DMA
+#define arm_dma_limit (0xffffffff)
+#else
 #define arm_dma_limit ((phys_addr_t)~0)
+#endif