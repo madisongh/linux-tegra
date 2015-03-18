venkatarajasekhar/Wiko-Highway-4G-Kernel

net: rfkill: moving to clk prepare APIs
The clk_enable/clk_disable pair of APIs are replaced with
clk_prepare_enable and clk_disable_unprepare.

This is needed for the migration to common clk framework.

Bug 920915

Change-Id: I9ccd2a3390f262099ed2059a4ff7d49143a73d99
Signed-off-by: Sivaram Nair <sivaramn@nvidia.com>
Reviewed-on: http://git-master/r/172217
Reviewed-by: Automatic_Commit_Validation_User
Reviewed-by: Rhyland Klein <rklein@nvidia.com>
Reviewed-by: Juha Tukkinen <jtukkinen@nvidia.com>

net/rfkill/rfkill-gpio.c
@@ -57,10 +57,10 @@ static int rfkill_gpio_set_power(void *data, bool blocked)
 		if (gpio_is_valid(rfkill->pdata->reset_gpio))
 			gpio_direction_output(rfkill->pdata->reset_gpio, 0);
 		if (rfkill->pwr_clk && PWR_CLK_ENABLED(rfkill))
-			clk_disable(rfkill->pwr_clk);
+			clk_disable_unprepare(rfkill->pwr_clk);
 	} else {
 		if (rfkill->pwr_clk && PWR_CLK_DISABLED(rfkill))
-			clk_enable(rfkill->pwr_clk);
+			clk_prepare_enable(rfkill->pwr_clk);
 		if (gpio_is_valid(rfkill->pdata->reset_gpio))
 			gpio_direction_output(rfkill->pdata->reset_gpio, 1);
 		if (gpio_is_valid(rfkill->pdata->shutdown_gpio))
@@ -201,7 +201,7 @@ static int rfkill_gpio_remove(struct platform_device *pdev)
 	if (gpio_is_valid(rfkill->pdata->reset_gpio))
 		gpio_free(rfkill->pdata->reset_gpio);
 	if (rfkill->pwr_clk && PWR_CLK_ENABLED(rfkill))
-		clk_disable(rfkill->pwr_clk);
+		clk_disable_unprepare(rfkill->pwr_clk);
 	if (rfkill->pwr_clk)
 		clk_put(rfkill->pwr_clk);
 	kfree(rfkill->shutdown_name);

madisongh/linux-tegra

net/rfkill/rfkill-gpio.c
@@ -46,13 +46,13 @@ static int rfkill_gpio_set_power(void *data, bool blocked)
 	struct rfkill_gpio_data *rfkill = data;
 
 	if (!blocked && !IS_ERR(rfkill->clk) && !rfkill->clk_enabled)
-		clk_enable(rfkill->clk);
+		clk_prepare_enable(rfkill->clk);
 
 	gpiod_set_value_cansleep(rfkill->shutdown_gpio, !blocked);
 	gpiod_set_value_cansleep(rfkill->reset_gpio, !blocked);
 
 	if (blocked && !IS_ERR(rfkill->clk) && rfkill->clk_enabled)
-		clk_disable(rfkill->clk);
+		clk_disable_unprepare(rfkill->clk);
 
 	rfkill->clk_enabled = !blocked;
 