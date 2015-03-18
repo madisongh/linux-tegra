InES-HPMM/linux-l4t

ARM64: include: port some missing headers from arm
Change-Id: I53121d39813594884cf54daf9d3aea060a92bc1d
Signed-off-by: Peng Du <pdu@nvidia.com>
Reviewed-on: http://git-master/r/259824
Reviewed-by: Alexander Van Brunt <avanbrunt@nvidia.com>

arch/arm64/include/asm/dma.h
@@ -1,0 +1,17 @@
+#ifndef __ASM_ARM_DMA_H
+#define __ASM_ARM_DMA_H
+
+/*
+ * This is the maximum virtual address which can be DMA'd from.
+ */
+#ifdef CONFIG_ZONE_DMA32
+#define MAX_DMA_ADDRESS	0xffffffffUL
+#endif
+
+#ifdef CONFIG_PCI
+extern int isa_dma_bridge_buggy;
+#else
+#define isa_dma_bridge_buggy    (0)
+#endif
+
+#endif /* __ASM_ARM_DMA_H */

madisongh/linux-tegra

arch/arm64/include/asm/dma.h
@@ -1,0 +1,17 @@
+#ifndef __ASM_ARM_DMA_H
+#define __ASM_ARM_DMA_H
+
+/*
+ * This is the maximum virtual address which can be DMA'd from.
+ */
+#ifdef CONFIG_ZONE_DMA
+#define MAX_DMA_ADDRESS	0xffffffffUL
+#endif
+
+#ifdef CONFIG_PCI
+extern int isa_dma_bridge_buggy;
+#else
+#define isa_dma_bridge_buggy    (0)
+#endif
+
+#endif /* __ASM_ARM_DMA_H */