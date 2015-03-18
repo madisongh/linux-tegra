GalaxyTab101/samsung-kernel-galaxytab101

arm: tegra: Reserve SMMU I/O window address space
Bug 790951
Bug 791114

Original-Change-Id: I50ac8b20c1a1cd2ed5c135f940ec1791fb3dc6c0
Reviewed-on: http://git-master/r/19145
Reviewed-by: Scott Williams <scwilliams@nvidia.com>
Tested-by: Scott Williams <scwilliams@nvidia.com>
Change-Id: I3406c6cc5b0733b91965983028dc5935f2ad62f8

arch/arm/mach-tegra/common.c
@@ -54,14 +54,6 @@ unsigned long tegra_lp0_vec_size;
 unsigned long tegra_grhost_aperture = ~0ul;
 static   bool is_tegra_debug_uart_hsport;
 
-static struct board_info tegra_board_info = {
-	.board_id = -1,
-	.sku = -1,
-	.fab = -1,
-	.major_revision = -1,
-	.minor_revision = -1,
-};
-
 void (*tegra_reset)(char mode, const char *cmd);
 
 static __initdata struct tegra_clk_init_table common_clk_init_table[] = {
@@ -390,6 +382,13 @@ void __init tegra_reserve(unsigned long carveout_size, unsigned long fb_size,
 	if (tegra_carveout_size && tegra_carveout_start < tegra_grhost_aperture)
 		tegra_grhost_aperture = tegra_carveout_start;
 
+#ifdef CONFIG_TEGRA_IOVMM_SMMU
+	if (memblock_reserve(TEGRA_SMMU_BASE, TEGRA_SMMU_SIZE)) {
+		pr_err("Failed to reserve SMMU I/O window %08u@%08u\n",
+			TEGRA_SMMU_BASE, TEGRA_SMMU_SIZE);
+	}
+#endif
+
 	/*
 	 * TODO: We should copy the bootloader's framebuffer to the framebuffer
 	 * allocated above, and then free this one.
@@ -424,4 +423,9 @@ void __init tegra_reserve(unsigned long carveout_size, unsigned long fb_size,
 		tegra_carveout_start,
 		tegra_carveout_size ?
 			tegra_carveout_start + tegra_carveout_size - 1 : 0);
+
+#ifdef CONFIG_TEGRA_IOVMM_SMMU
+	pr_info("SMMU:                   %08u - %08u\n",
+		TEGRA_SMMU_BASE, TEGRA_SMMU_BASE + TEGRA_SMMU_SIZE - 1);
+#endif
 }

madisongh/linux-tegra

Original-Change-Id: I3406c6cc5b0733b91965983028dc5935f2ad62f8
Rebase-Id: R5dff98dee46a0789c3c4e90008a34161c81eb7b3

arch/arm/mach-tegra/common.c
@@ -93,14 +93,6 @@ void tegra_assert_system_reset(enum reboot_mode mode, const char *cmd)
 	writel_relaxed(reg, reset);
 }
 
-static struct board_info tegra_board_info = {
-	.board_id = -1,
-	.sku = -1,
-	.fab = -1,
-	.major_revision = -1,
-	.minor_revision = -1,
-};
-
 void tegra_init_cache(u32 tag_latency, u32 data_latency)
 {
 #ifdef CONFIG_CACHE_L2X0
@@ -362,6 +354,13 @@ void __init tegra_reserve(unsigned long carveout_size, unsigned long fb_size,
 	if (tegra_carveout_size && tegra_carveout_start < tegra_grhost_aperture)
 		tegra_grhost_aperture = tegra_carveout_start;
 
+#ifdef CONFIG_TEGRA_IOVMM_SMMU
+	if (memblock_reserve(TEGRA_SMMU_BASE, TEGRA_SMMU_SIZE)) {
+		pr_err("Failed to reserve SMMU I/O window %08x@%08x\n",
+			TEGRA_SMMU_BASE, TEGRA_SMMU_SIZE);
+	}
+#endif
+
 	/*
 	 * TODO: We should copy the bootloader's framebuffer to the framebuffer
 	 * allocated above, and then free this one.
@@ -396,6 +395,11 @@ void __init tegra_reserve(unsigned long carveout_size, unsigned long fb_size,
 		tegra_carveout_start,
 		tegra_carveout_size ?
 			tegra_carveout_start + tegra_carveout_size - 1 : 0);
+
+#ifdef CONFIG_TEGRA_IOVMM_SMMU
+	pr_info("SMMU:                   %08x - %08x\n",
+		TEGRA_SMMU_BASE, TEGRA_SMMU_BASE + TEGRA_SMMU_SIZE - 1);
+#endif
 }
 
 void __init tegra_init_late(void)