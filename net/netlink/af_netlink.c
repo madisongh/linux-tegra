InES-HPMM/linux-l4t

netlink: Print portid as signed int
CTS test "testTryToCrashVold" will decode portid from string to signed int,
so the unsigned string may exceed the max value of signed int.

Bug 1417380

Change-Id: I519d335c31a72c65387dd13b5f5229149f6ed15f
Signed-off-by: Liangcai Li <liangcail@nvidia.com>
Reviewed-on: http://git-master/r/345723
Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

net/netlink/af_netlink.c
@@ -2762,7 +2762,7 @@ static int netlink_seq_show(struct seq_file *seq, void *v)
 		struct sock *s = v;
 		struct netlink_sock *nlk = nlk_sk(s);
 
-		seq_printf(seq, "%pK %-3d %-6u %08x %-8d %-8d %pK %-8d %-8d %-8lu\n",
+		seq_printf(seq, "%pK %-3d %-6d %08x %-8d %-8d %pK %-8d %-8d %-8lu\n",
 			   s,
 			   s->sk_protocol,
 			   nlk->portid,

madisongh/linux-tegra

net/netlink/af_netlink.c
@@ -2981,7 +2981,7 @@ static int netlink_seq_show(struct seq_file *seq, void *v)
 		struct sock *s = v;
 		struct netlink_sock *nlk = nlk_sk(s);
 
-		seq_printf(seq, "%pK %-3d %-6u %08x %-8d %-8d %d %-8d %-8d %-8lu\n",
+		seq_printf(seq, "%pK %-3d %-6d %08x %-8d %-8d %d %-8d %-8d %-8lu\n",
 			   s,
 			   s->sk_protocol,
 			   nlk->portid,