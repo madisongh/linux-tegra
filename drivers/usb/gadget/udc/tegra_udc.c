a69bf8373e87fcf97b2d990eff93003b5e3b2b53
Rakesh Babu Bodla <rbodla@nvidia.com>
2015-06-30 15:16:24 +0530

    usb: gadget: tegra: update the electrical parameters
    Update hs_sync_strart_dly to 9 and use fuses for
    XCVR values instead of configuring through soc
    data for t210.
    
    Change-Id: I2e4e77e041e5b5ca10d221c366ea070735a6f30e
    Signed-off-by: Rakesh Babu Bodla <rbodla@nvidia.com>
    Reviewed-on: http://git-master/r/764099
    Reviewed-by: Venkat Moganty <vmoganty@nvidia.com>
    Tested-by: Venkat Moganty <vmoganty@nvidia.com>
    Reviewed-on: http://git-master/r/1157108
    Reviewed-by: Rohith Seelaboyina <rseelaboyina@nvidia.com>
    Tested-by: Rohith Seelaboyina <rseelaboyina@nvidia.com>

--- drivers/usb/gadget/udc/tegra_udc.c
+++ drivers/usb/gadget/udc/tegra_udc.c
@@ -2950,7 +2950,7 @@ static struct tegra_usb_platform_data *tegra_udc_dt_parse_pdata(
 
 static struct tegra_udc_soc_data tegra_soc_config = {
 	.utmi = {
-		.hssync_start_delay = 0,
+		.hssync_start_delay = 9,
 		.elastic_limit = 16,
 		.idle_wait_delay = 17,
 		.term_range_adj = 6,
@@ -2976,7 +2976,7 @@ static struct tegra_udc_soc_data tegra21x_soc_config = {
 		.xcvr_lsfslew = 2,
 		.xcvr_lsrslew = 2,
 		.xcvr_setup_offset = 0,
-		.xcvr_use_fuses = 0,
+		.xcvr_use_fuses = 1,
 	},
 	.has_hostpc = true,
 	.unaligned_dma_buf_supported = false,
