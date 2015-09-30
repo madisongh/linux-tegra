cb1bb161987d6593e078923e42d9251a3de4061b
Sai Gurrappadi <sgurrappadi@nvidia.com>
2015-09-30 13:43:12 -0700

    sched: Add per-cpu max capacity to sched_group_capacity
    struct sched_group_capacity currently represents the compute capacity
    sum of all cpus in the sched_group. Unless it is divided by the
    group_weight to get the average capacity per cpu it hides differences in
    cpu capacity for mixed capacity systems (e.g. high RT/IRQ utilization or
    ARM big.LITTLE). But even the average may not be sufficient if the group
    covers cpus of different capacities. Instead, by extending struct
    sched_group_capacity to indicate max per-cpu capacity in the group a
    suitable group for a given task utilization can easily be found such
    that cpus with reduced capacity can be avoided for tasks with high
    utilization (not implemented by this patch).
    
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>
    
    From the chromeos EAS tree.
    
    Bug 1665314
    
    Change-Id: I93288e8c0e0d5059871b6514eb8b082f06bfe65d
    Signed-off-by: Sai Gurrappadi <sgurrappadi@nvidia.com>
    (cherry picked from commit caaab4fbee2737aed7a63bfc9bca759f3c23e95c)
    Reviewed-on: http://git-master/r/829118
    Reviewed-by: Peter Boonstoppel <pboonstoppel@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Alexander Van Brunt <avanbrunt@nvidia.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6310,13 +6310,14 @@ static void update_cpu_capacity(struct sched_domain *sd, int cpu)
 
 	cpu_rq(cpu)->cpu_capacity = capacity;
 	sdg->sgc->capacity = capacity;
+	sdg->sgc->max_capacity = capacity;
 }
 
 void update_group_capacity(struct sched_domain *sd, int cpu)
 {
 	struct sched_domain *child = sd->child;
 	struct sched_group *group, *sdg = sd->groups;
-	unsigned long capacity;
+	unsigned long capacity, max_capacity;
 	unsigned long interval;
 
 	interval = msecs_to_jiffies(sd->balance_interval);
@@ -6329,6 +6330,7 @@ void update_group_capacity(struct sched_domain *sd, int cpu)
 	}
 
 	capacity = 0;
+	max_capacity = 0;
 
 	if (child->flags & SD_OVERLAP) {
 		/*
@@ -6353,11 +6355,12 @@ void update_group_capacity(struct sched_domain *sd, int cpu)
 			 */
 			if (unlikely(!rq->sd)) {
 				capacity += capacity_of(cpu);
-				continue;
+			} else {
+				sgc = rq->sd->groups->sgc;
+				capacity += sgc->capacity;
 			}
 
-			sgc = rq->sd->groups->sgc;
-			capacity += sgc->capacity;
+			max_capacity = max(capacity, max_capacity);
 		}
 	} else  {
 		/*
@@ -6367,12 +6370,16 @@ void update_group_capacity(struct sched_domain *sd, int cpu)
 
 		group = child->groups;
 		do {
-			capacity += group->sgc->capacity;
+			struct sched_group_capacity *sgc = group->sgc;
+
+			capacity += sgc->capacity;
+			max_capacity = max(sgc->max_capacity, max_capacity);
 			group = group->next;
 		} while (group != child->groups);
 	}
 
 	sdg->sgc->capacity = capacity;
+	sdg->sgc->max_capacity = max_capacity;
 }
 
 /*
