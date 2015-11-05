960cd6a93330b6b8f58ac512ba84bc2d3df668c3
Allen Yu <alleny@nvidia.com>
2015-11-05 22:08:14 +0800

    pstore: rtrace: support specifying an address space
    Add debugfs support to specify an upper limit and a lower limit of
    address so that only access to the specified address space will be
    recorded.
    
    Bug 200151604
    
    Change-Id: I3c3d8b0bebeb6ff5df1615b52accda80128bacf4
    Reviewed-on: http://git-master/r/828466
    (cherry picked from commit 86c41b925ee665065e2769a42d7e2ae08896b6a8)
    Reviewed-on: http://git-master/r/832544
    (cherry picked from commit be8885497521db35973350ad4dc03a7d1f1705b9)
    Reviewed-on: http://git-master/r/1238478
    Reviewed-by: Chun Xu <chunx@nvidia.com>
    Reviewed-by: Kerwin Wan <kerwinw@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Tested-by: Chun Xu <chunx@nvidia.com>

--- fs/pstore/rtrace.c
+++ fs/pstore/rtrace.c
@@ -26,11 +26,14 @@
 #include <linux/pstore.h>
 #include <asm/barrier.h>
 #include "internal.h"
+#include <asm/io.h>
 
 struct rtrace_state {
 	int enabled;
 	int initialized;
 	int temp_bypass;
+	unsigned long upper_limit;
+	unsigned long lower_limit;
 };
 
 static struct rtrace_state rtrace;
@@ -58,12 +61,15 @@ noinline void notrace pstore_rtrace_call(enum rtrace_event_type log_type,
 
 	local_irq_save(flags);
 
-	rec.cpu = raw_smp_processor_id();
-	rec.event = log_type;
-	rec.caller = __builtin_return_address(0);
 	rec.raddr = data;
-	psinfo->write_buf(PSTORE_TYPE_RTRACE, 0, NULL, 0, (void *)&rec,
-			  0, sizeof(rec), psinfo);
+	if (rec.raddr >= rtrace.lower_limit &&
+				rec.raddr <= rtrace.upper_limit) {
+		rec.cpu = raw_smp_processor_id();
+		rec.event = log_type;
+		rec.caller = (unsigned long)__builtin_return_address(0);
+		psinfo->write_buf(PSTORE_TYPE_RTRACE, 0, NULL, 0, (void *)&rec,
+				  0, sizeof(rec), psinfo);
+	}
 
 	local_irq_restore(flags);
 }
@@ -103,10 +109,60 @@ static ssize_t pstore_rtrace_knob_read(struct file *f, char __user *buf,
 	return simple_read_from_buffer(buf, count, ppos, val, sizeof(val));
 }
 
+static ssize_t rtrace_upper_limit_write(struct file *f, const char __user *buf,
+					size_t count, loff_t *ppos)
+{
+	ssize_t ret;
+
+	ret = kstrtoul_from_user(buf, count, 0, &rtrace.upper_limit);
+	return ret ? ret : count;
+}
+
+static ssize_t rtrace_upper_limit_read(struct file *f, char __user *buf,
+				       size_t count, loff_t *ppos)
+{
+	char val[24];
+	ssize_t size = scnprintf(val, sizeof(val), "0x%016lx\n",
+						rtrace.upper_limit);
+
+	return simple_read_from_buffer(buf, count, ppos, val, size);
+}
+
+static ssize_t rtrace_lower_limit_write(struct file *f, const char __user *buf,
+					size_t count, loff_t *ppos)
+{
+	ssize_t ret;
+
+	ret = kstrtoul_from_user(buf, count, 0, &rtrace.lower_limit);
+	return ret ? ret : count;
+}
+
+static ssize_t rtrace_lower_limit_read(struct file *f, char __user *buf,
+				       size_t count, loff_t *ppos)
+{
+	char val[24];
+	ssize_t size = scnprintf(val, sizeof(val), "0x%016lx\n",
+						rtrace.lower_limit);
+
+	return simple_read_from_buffer(buf, count, ppos, val, size);
+}
+
 static const struct file_operations pstore_knob_fops = {
 	.open	= simple_open,
 	.read	= pstore_rtrace_knob_read,
 	.write	= pstore_rtrace_knob_write,
+};
+
+static const struct file_operations rtrace_upper_limit_fops = {
+	.open	= simple_open,
+	.read	= rtrace_upper_limit_read,
+	.write	= rtrace_upper_limit_write,
+};
+
+static const struct file_operations rtrace_lower_limit_fops = {
+	.open	= simple_open,
+	.read	= rtrace_lower_limit_read,
+	.write	= rtrace_lower_limit_write,
 };
 
 void pstore_register_rtrace(void)
@@ -121,6 +177,20 @@ void pstore_register_rtrace(void)
 	if (!file)
 		pr_err("%s: unable to create record_rtrace file\n", __func__);
 
+	file = debugfs_create_file("rtrace_upper_limit", 0600,
+			 psinfo->debugfs_dir, NULL, &rtrace_upper_limit_fops);
+	if (!file)
+		pr_err("%s: unable to create rtrace_upper_limit file\n",
+								    __func__);
+
+	file = debugfs_create_file("rtrace_lower_limit", 0600,
+			 psinfo->debugfs_dir, NULL, &rtrace_lower_limit_fops);
+	if (!file)
+		pr_err("%s: unable to create rtrace_lower_limit file\n",
+								    __func__);
+
+	rtrace.upper_limit = ULONG_MAX;
+	rtrace.lower_limit = 0;
 	rtrace.initialized = 1;
 #ifdef CONFIG_PSTORE_RTRACE_ENABLE_AT_STARTUP
 	rtrace.enabled = 1;
