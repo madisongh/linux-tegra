59248ac241422973429c9fd23ad0f70b021baedd
Naveen Kumar S <nkumars@nvidia.com>
2015-12-03 15:55:37 +0530

    video: tegra: dc: remove redundant flag check
    While identifying VIC, aspect ratio flag is again being
    checked after comparing few basic mode parameters. Hence
    removing the redundant flag comparision. This avoids failure
    in VIC identification when a mode does not specify  aspect ratio.
    
    bug 200148145
    
    Change-Id: I6298480b7d7a12c05c393276e5432bcd5e7dcd38
    Signed-off-by: Naveen Kumar S <nkumars@nvidia.com>
    Reviewed-on: http://git-master/r/842422
    (cherry picked from commit 0066f4d8ed7cda8ae5d34ad70594e14d6b15f041)
    Reviewed-on: http://git-master/r/926686
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Santosh Galma <galmar@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Venu Byravarasu <vbyravarasu@nvidia.com>

--- drivers/video/tegra/dc/hdmi2.0.c
+++ drivers/video/tegra/dc/hdmi2.0.c
@@ -1298,7 +1298,6 @@ static int tegra_hdmi_find_cea_vic(struct tegra_hdmi *hdmi)
 		      m.upper_margin == curr->upper_margin &&
 		      m.lower_margin == curr->lower_margin &&
 		      m.sync         == curr->sync &&
-		      m.flag         == curr->flag &&
 		      m.vmode        == curr->vmode))
 			continue;
 
