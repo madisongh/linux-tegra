eb761d76d95af25cac030afe3c6b50b083877e25
Ben Dooks <ben.dooks@codethink.co.uk>
2015-12-08 10:19:25 +0000

    gpio: pca953x: add onsemi,pca9654 id
    Add onsemi,pca9654 which is also compatible with the nxp,pca9524 as it
    is an 8bit expander with an interrupt output.
    
    Signed-off-by: Ben Dooks <ben.dooks@codethink.co.uk>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    (cherry picked from commit 9f49f6dd0473c8fab4522e2e2cb16e93ffb8f3f0)
    Signed-off-by: Suresh Mangipudi <smangipudi@nvidia.com>

--- drivers/gpio/gpio-pca953x.c
+++ drivers/gpio/gpio-pca953x.c
@@ -825,6 +825,8 @@ static const struct of_device_id pca953x_dt_ids[] = {
 	{ .compatible = "ti,tca6424", },
 	{ .compatible = "ti,tca9539", },
 
+	{ .compatible = "onsemi,pca9654" },
+
 	{ .compatible = "exar,xra1202", },
 	{ }
 };
