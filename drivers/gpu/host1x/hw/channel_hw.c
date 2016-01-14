cc20fe30cd6e795c4318036a3c1fd44c3cfaa82f
Shridhar Rasal <srasal@nvidia.com>
2016-01-14 21:28:47 +0530

    drm: host1x: fix cmdbuf offset during trace
    Change-Id: I792ad49a9a0c78c15302a15b2db83f27a5ddbb29
    Signed-off-by: Shridhar Rasal <srasal@nvidia.com>
    Reviewed-on: http://git-master/r/932774
    Signed-off-by: Mikko Perttunen <mperttunen@nvidia.com>
    (cherry picked from linux-3.18)

--- drivers/gpu/host1x/hw/channel_hw.c
+++ drivers/gpu/host1x/hw/channel_hw.c
@@ -46,11 +46,11 @@ static void trace_write_gather(struct host1x_cdma *cdma, struct host1x_bo *bo,
 		 */
 		for (i = 0; i < words; i += TRACE_MAX_LENGTH) {
 			u32 num_words = min(words - i, TRACE_MAX_LENGTH);
-
-			offset += i * sizeof(u32);
+			u32 next_offset = offset + i * sizeof(u32);
 
 			trace_host1x_cdma_push_gather(dev_name(dev), bo,
-						      num_words, offset,
+						      num_words,
+						      next_offset,
 						      mem);
 		}
 
