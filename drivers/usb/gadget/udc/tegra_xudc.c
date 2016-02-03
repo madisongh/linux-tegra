90dfa0d369a719d6787933e6ec6bcb3873941bb6
Mark Kuo <mkuo@nvidia.com>
2016-02-03 16:44:02 +0800

    usb: gadget: xudc: Program USB2 pad PD controls
    Program USB2 pad PD controls when device mode is enabled or disabled.
    This is to reduce power consumption as suggested by HW.
    
    This change is for the new XUDC driver (tegra_xudc.c).
    
    Bug 200124691
    
    Signed-off-by: Mark Kuo <mkuo@nvidia.com>
    Reviewed-on: http://git-master/r/1002219
    
    Change-Id: I99c5c4c4dcd9e78b0fe0fbc227321fd290575e22
    Signed-off-by: JC Kuo <jckuo@nvidia.com>
    Reviewed-on: http://git-master/r/1122472
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- drivers/usb/gadget/udc/tegra_xudc.c
+++ drivers/usb/gadget/udc/tegra_xudc.c
@@ -616,6 +616,8 @@ static void tegra_xudc_device_mode_on(struct tegra_xudc *xudc)
 
 	spin_lock_irqsave(&xudc->lock, flags);
 	dev_info(xudc->dev, "device mode on\n");
+	tegra_phy_xusb_utmi_pad_power_on(xudc->utmi_phy);
+	tegra_phy_xusb_utmi_pad_chg_power_on(xudc->utmi_phy);
 	tegra_phy_xusb_set_vbus_override(xudc->utmi_phy);
 
 	xudc->device_mode = true;
@@ -640,6 +642,8 @@ static void tegra_xudc_device_mode_off(struct tegra_xudc *xudc)
 	reinit_completion(&xudc->disconnect_complete);
 
 	tegra_phy_xusb_clear_vbus_override(xudc->utmi_phy);
+	tegra_phy_xusb_utmi_pad_power_down(xudc->utmi_phy);
+	tegra_phy_xusb_utmi_pad_chg_power_down(xudc->utmi_phy);
 
 	pls = (xudc_readl(xudc, PORTSC) >> PORTSC_PLS_SHIFT) &
 		PORTSC_PLS_MASK;
