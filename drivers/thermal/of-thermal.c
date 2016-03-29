c4bb0ac65a6d0ae3bb7e1eb9498f81c9dba73ade
Wei Ni <wni@nvidia.com>
2016-03-29 18:29:17 +0800

    thermal: of-thermal: allow setting trip_temp on hardware
    In current of-thermal, the .set_trip_temp only support to
    set trip_temp for SW. But some sensors support to set
    trip_temp on hardware, so that can trigger interrupt,
    shutdown or any other events.
    This patch adds .set_trip_temp() callback in
    thermal_zone_of_device_ops{}, so that the sensor device can
    use it to set trip_temp on hardware.
    
    Signed-off-by: Wei Ni <wni@nvidia.com>
    Signed-off-by: Eduardo Valentin <edubezval@gmail.com>
    (cherry picked from commit c35095215a80b13af2f521e1ab1c727a5224e53b)
    
    Conflicts:
    	drivers/thermal/of-thermal.c
    	include/linux/thermal.h
    
    Bug 200233003
    
    Change-Id: Ic9d2f580dc43e95a996e68a4501bd81352db2e50
    Signed-off-by: Srikar Srimath Tirumala <srikars@nvidia.com>
    Reviewed-on: http://git-master/r/1234160
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>

--- drivers/thermal/of-thermal.c
+++ drivers/thermal/of-thermal.c
@@ -320,7 +320,15 @@ static int of_thermal_set_trip_temp(struct thermal_zone_device *tz, int trip,
 	if (trip >= data->ntrips || trip < 0)
 		return -EDOM;
 
-/* thermal framework should take care of data->mask & (1 << trip) */
+	if (data->ops->set_trip_temp) {
+		int ret;
+
+		ret = data->ops->set_trip_temp(data->sensor_data, trip, temp);
+		if (ret)
+			return ret;
+	}
+
+	/* thermal framework should take care of data->mask & (1 << trip) */
 	data->trips[trip].temperature = temp;
 
 	if (data->ops->trip_update)
--- include/linux/thermal.h
+++ include/linux/thermal.h
@@ -346,6 +346,7 @@ struct thermal_zone_of_device_ops {
 	int (*get_trend)(void *, int, enum thermal_trend *);
 	int (*set_emul_temp)(void *, int);
 	int (*trip_update)(void *, int);
+	int (*set_trip_temp)(void *, int, int);
 };
 
 /**
