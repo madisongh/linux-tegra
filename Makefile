Unlegacy-Android/android_kernel_nvidia_tegra3

kernel: add Kbuild option for kernel optimization level
Adds a kernel config option to lower the kernel compile optimization
level from O2 to O1 for clearer GDB debugging. Default on for simulation
builds, default off for others.

Change-Id: I17fd63b5b391984d28b275e516df89d6a223021c
Reviewed-on: http://git-master/r/121822
Reviewed-by: Chao Xu <cxu@nvidia.com>
Reviewed-by: Automatic_Commit_Validation_User
Reviewed-by: Ken Adams <kadams@nvidia.com>
Tested-by: Richard Wiley <rwiley@nvidia.com>
Reviewed-by: Mark Stadler <mastadler@nvidia.com>
Reviewed-by: Bo Yan <byan@nvidia.com>

Makefile
@@ -561,7 +561,11 @@ all: vmlinux
 ifdef CONFIG_CC_OPTIMIZE_FOR_SIZE
 KBUILD_CFLAGS	+= -Os
 else
+ifdef CONFIG_LESS_GCC_OPT
+KBUILD_CLFAGS	+= -O1
+else
 KBUILD_CFLAGS	+= -O2
+endif
 endif
 
 # conserve stack if available

madisongh/linux-tegra

Rebase-Id: Ra7c14ac91b0cfbe441dbfea33f0d5b1a6fd44d5f

Makefile
@@ -610,7 +610,11 @@ KBUILD_CFLAGS	+= $(call cc-option,-fno-delete-null-pointer-checks,)
 ifdef CONFIG_CC_OPTIMIZE_FOR_SIZE
 KBUILD_CFLAGS	+= -Os $(call cc-disable-warning,maybe-uninitialized,)
 else
+ifdef CONFIG_LESS_GCC_OPT
+KBUILD_CFLAGS	+= -O1
+else
 KBUILD_CFLAGS	+= -O2
+endif
 endif
 
 # Tell gcc to never replace conditional load with a non-conditional one