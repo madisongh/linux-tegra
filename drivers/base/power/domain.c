InES-HPMM/linux-l4t

PM / Domains: Check if device wants to wake up
If a a define is marked as "NO_POWER_OFF", and it's suspended, wake
it up.

Bug 1364240

Change-Id: Ic51e69db01a88e2deefa4c3c0884d14ccc29272b
Signed-off-by: Terje Bergstrom <tbergstrom@nvidia.com>
Reviewed-on: http://git-master/r/288811

drivers/base/power/domain.c
@@ -434,9 +434,19 @@ static int genpd_dev_pm_qos_notifier(struct notifier_block *nb,
 		spin_unlock_irq(&dev->power.lock);
 
 		if (!IS_ERR(genpd)) {
+			enum pm_qos_flags_status stat;
+
 			mutex_lock(&genpd->lock);
 			genpd->max_off_time_changed = true;
+
+			stat = dev_pm_qos_flags(pdd->dev,
+					PM_QOS_FLAG_NO_POWER_OFF
+						| PM_QOS_FLAG_REMOTE_WAKEUP);
+
 			mutex_unlock(&genpd->lock);
+
+			if (stat)
+				pm_genpd_poweron(genpd);
 		}
 
 		dev = dev->parent;

madisongh/linux-tegra

(cherry picked from commit dca2e0fd4c79192eb8b90f4b7f76cdbf565eaa3f)

drivers/base/power/domain.c
@@ -274,9 +274,19 @@ static int genpd_dev_pm_qos_notifier(struct notifier_block *nb,
 		spin_unlock_irq(&dev->power.lock);
 
 		if (!IS_ERR(genpd)) {
+			enum pm_qos_flags_status stat;
+
 			mutex_lock(&genpd->lock);
 			genpd->max_off_time_changed = true;
+
+			stat = dev_pm_qos_flags(pdd->dev,
+					PM_QOS_FLAG_NO_POWER_OFF
+						| PM_QOS_FLAG_REMOTE_WAKEUP);
+
 			mutex_unlock(&genpd->lock);
+
+			if (stat)
+				genpd_poweron(genpd);
 		}
 
 		dev = dev->parent;