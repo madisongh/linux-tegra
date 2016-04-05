InES-HPMM/linux-l4t

video: tegra: nvmap: move cache maint into new file
Move cache maintenance code into new file for better
readability and organization.

Change-Id: Id2e5232c7ee010b3044ef4256fd4c92e92e06345
Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
Reviewed-on: http://git-master/r/762085

drivers/video/tegra/nvmap/nvmap_cache.c
@@ -1,0 +1,453 @@
+/*
+ * drivers/video/tegra/nvmap/nvmap_cache.c
+ *
+ * Copyright (c) 2011-2015, NVIDIA CORPORATION. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#define pr_fmt(fmt)	"nvmap: %s() " fmt, __func__
+
+#include <linux/highmem.h>
+#include <linux/io.h>
+
+#include <trace/events/nvmap.h>
+
+#include "nvmap_priv.h"
+
+static void inner_cache_maint(unsigned int op, void *vaddr, size_t size)
+{
+	if (op == NVMAP_CACHE_OP_WB_INV)
+		dmac_flush_range(vaddr, vaddr + size);
+	else if (op == NVMAP_CACHE_OP_INV)
+		dmac_map_area(vaddr, size, DMA_FROM_DEVICE);
+	else
+		dmac_map_area(vaddr, size, DMA_TO_DEVICE);
+}
+
+static void outer_cache_maint(unsigned int op, phys_addr_t paddr, size_t size)
+{
+	if (op == NVMAP_CACHE_OP_WB_INV)
+		outer_flush_range(paddr, paddr + size);
+	else if (op == NVMAP_CACHE_OP_INV)
+		outer_inv_range(paddr, paddr + size);
+	else
+		outer_clean_range(paddr, paddr + size);
+}
+
+static void heap_page_cache_maint(
+	struct nvmap_handle *h, unsigned long start, unsigned long end,
+	unsigned int op, bool inner, bool outer, bool clean_only_dirty)
+{
+	if (h->userflags & NVMAP_HANDLE_CACHE_SYNC) {
+		/*
+		 * zap user VA->PA mappings so that any access to the pages
+		 * will result in a fault and can be marked dirty
+		 */
+		nvmap_handle_mkclean(h, start, end-start);
+		nvmap_zap_handle(h, start, end - start);
+	}
+
+#ifdef NVMAP_LAZY_VFREE
+	if (inner) {
+		void *vaddr = NULL;
+
+		if (!h->vaddr) {
+			struct page **pages;
+			/* mutex lock protection is not necessary as it is
+			 * already increased in __nvmap_do_cache_maint to
+			 * protect from migrations.
+			 */
+			nvmap_kmaps_inc_no_lock(h);
+			pages = nvmap_pages(h->pgalloc.pages,
+					    h->size >> PAGE_SHIFT);
+			if (!pages)
+				goto per_page_cache_maint;
+			vaddr = vm_map_ram(pages,
+					h->size >> PAGE_SHIFT, -1,
+					nvmap_pgprot(h, PG_PROT_KERNEL));
+			nvmap_altfree(pages,
+				(h->size >> PAGE_SHIFT) * sizeof(*pages));
+		}
+		if (vaddr && atomic_long_cmpxchg(&h->vaddr, 0, (long)vaddr)) {
+			nvmap_kmaps_dec(h);
+			vm_unmap_ram(vaddr, h->size >> PAGE_SHIFT);
+		}
+		if (h->vaddr) {
+			/* Fast inner cache maintenance using single mapping */
+			inner_cache_maint(op, h->vaddr + start, end - start);
+			if (!outer)
+				return;
+			/* Skip per-page inner maintenance in loop below */
+			inner = false;
+		}
+	}
+per_page_cache_maint:
+	if (!h->vaddr)
+		nvmap_kmaps_dec(h);
+#endif
+
+	while (start < end) {
+		struct page *page;
+		void *kaddr, *vaddr;
+		phys_addr_t paddr;
+		unsigned long next;
+		unsigned long off;
+		size_t size;
+
+		page = nvmap_to_page(h->pgalloc.pages[start >> PAGE_SHIFT]);
+		next = min(((start + PAGE_SIZE) & PAGE_MASK), end);
+		off = start & ~PAGE_MASK;
+		size = next - start;
+		paddr = page_to_phys(page) + off;
+
+		if (inner) {
+			kaddr = kmap(page);
+			vaddr = (void *)kaddr + off;
+			BUG_ON(!kaddr);
+			inner_cache_maint(op, vaddr, size);
+			kunmap(page);
+		}
+
+		if (outer)
+			outer_cache_maint(op, paddr, size);
+		start = next;
+	}
+}
+
+#if defined(CONFIG_NVMAP_OUTER_CACHE_MAINT_BY_SET_WAYS)
+static bool fast_cache_maint_outer(unsigned long start,
+		unsigned long end, unsigned int op)
+{
+	bool result = false;
+	if (end - start >= cache_maint_outer_threshold) {
+		if (op == NVMAP_CACHE_OP_WB_INV) {
+			outer_flush_all();
+			result = true;
+		}
+		if (op == NVMAP_CACHE_OP_WB) {
+			outer_clean_all();
+			result = true;
+		}
+	}
+
+	return result;
+}
+#else
+static inline bool fast_cache_maint_outer(unsigned long start,
+		unsigned long end, unsigned int op)
+{
+	return false;
+}
+#endif
+
+#if defined(CONFIG_NVMAP_CACHE_MAINT_BY_SET_WAYS)
+static inline bool can_fast_cache_maint(struct nvmap_handle *h,
+	unsigned long start,
+	unsigned long end, unsigned int op)
+{
+	if ((op == NVMAP_CACHE_OP_INV) ||
+		((end - start) < cache_maint_inner_threshold))
+		return false;
+	return true;
+}
+#else
+static inline bool can_fast_cache_maint(struct nvmap_handle *h,
+	unsigned long start,
+	unsigned long end, unsigned int op)
+{
+	return false;
+}
+#endif
+
+static bool fast_cache_maint(struct nvmap_handle *h,
+	unsigned long start,
+	unsigned long end, unsigned int op,
+	bool clean_only_dirty)
+{
+	if (!can_fast_cache_maint(h, start, end, op))
+		return false;
+
+	if (h->userflags & NVMAP_HANDLE_CACHE_SYNC) {
+		nvmap_handle_mkclean(h, 0, h->size);
+		nvmap_zap_handle(h, 0, h->size);
+	}
+
+	if (op == NVMAP_CACHE_OP_WB_INV)
+		inner_flush_cache_all();
+	else if (op == NVMAP_CACHE_OP_WB)
+		inner_clean_cache_all();
+
+	/* outer maintenance */
+	if (h->flags != NVMAP_HANDLE_INNER_CACHEABLE) {
+		if(!fast_cache_maint_outer(start, end, op))
+		{
+			if (h->heap_pgalloc) {
+				heap_page_cache_maint(h, start,
+					end, op, false, true,
+					clean_only_dirty);
+			} else  {
+				phys_addr_t pstart;
+
+				pstart = start + h->carveout->base;
+				outer_cache_maint(op, pstart, end - start);
+			}
+		}
+	}
+	return true;
+}
+
+struct cache_maint_op {
+	phys_addr_t start;
+	phys_addr_t end;
+	unsigned int op;
+	struct nvmap_handle *h;
+	bool inner;
+	bool outer;
+	bool clean_only_dirty;
+};
+
+static int do_cache_maint(struct cache_maint_op *cache_work)
+{
+	pgprot_t prot;
+	unsigned long kaddr;
+	phys_addr_t pstart = cache_work->start;
+	phys_addr_t pend = cache_work->end;
+	phys_addr_t loop;
+	int err = 0;
+	struct nvmap_handle *h = cache_work->h;
+	struct nvmap_client *client;
+	unsigned int op = cache_work->op;
+	struct vm_struct *area = NULL;
+
+	if (!h || !h->alloc)
+		return -EFAULT;
+
+	client = h->owner;
+	if (can_fast_cache_maint(h, pstart, pend, op))
+		nvmap_stats_inc(NS_CFLUSH_DONE, cache_maint_inner_threshold);
+	else
+		nvmap_stats_inc(NS_CFLUSH_DONE, pend - pstart);
+	trace_nvmap_cache_maint(client, h, pstart, pend, op, pend - pstart);
+	trace_nvmap_cache_flush(pend - pstart,
+		nvmap_stats_read(NS_ALLOC),
+		nvmap_stats_read(NS_CFLUSH_RQ),
+		nvmap_stats_read(NS_CFLUSH_DONE));
+
+	wmb();
+	if (h->flags == NVMAP_HANDLE_UNCACHEABLE ||
+	    h->flags == NVMAP_HANDLE_WRITE_COMBINE || pstart == pend)
+		goto out;
+
+	if (fast_cache_maint(h, pstart, pend, op, cache_work->clean_only_dirty))
+		goto out;
+
+	if (h->heap_pgalloc) {
+		heap_page_cache_maint(h, pstart, pend, op, true,
+			(h->flags == NVMAP_HANDLE_INNER_CACHEABLE) ?
+			false : true, cache_work->clean_only_dirty);
+		goto out;
+	}
+
+	if (pstart > h->size || pend > h->size) {
+		pr_warn("cache maintenance outside handle\n");
+		err = -EINVAL;
+		goto out;
+	}
+
+	prot = nvmap_pgprot(h, PG_PROT_KERNEL);
+	area = alloc_vm_area(PAGE_SIZE, NULL);
+	if (!area) {
+		err = -ENOMEM;
+		goto out;
+	}
+	kaddr = (ulong)area->addr;
+
+	pstart += h->carveout->base;
+	pend += h->carveout->base;
+	loop = pstart;
+
+	while (loop < pend) {
+		phys_addr_t next = (loop + PAGE_SIZE) & PAGE_MASK;
+		void *base = (void *)kaddr + (loop & ~PAGE_MASK);
+		next = min(next, pend);
+
+		ioremap_page_range(kaddr, kaddr + PAGE_SIZE,
+			loop, prot);
+		inner_cache_maint(op, base, next - loop);
+		loop = next;
+		unmap_kernel_range(kaddr, PAGE_SIZE);
+	}
+
+	if (h->flags != NVMAP_HANDLE_INNER_CACHEABLE)
+		outer_cache_maint(op, pstart, pend - pstart);
+
+out:
+	if (area)
+		free_vm_area(area);
+	return err;
+}
+
+int __nvmap_do_cache_maint(struct nvmap_client *client,
+			struct nvmap_handle *h,
+			unsigned long start, unsigned long end,
+			unsigned int op, bool clean_only_dirty)
+{
+	int err;
+	struct cache_maint_op cache_op;
+
+	h = nvmap_handle_get(h);
+	if (!h)
+		return -EFAULT;
+
+	nvmap_kmaps_inc(h);
+	if (op == NVMAP_CACHE_OP_INV)
+		op = NVMAP_CACHE_OP_WB_INV;
+
+	/* clean only dirty is applicable only for Write Back operation */
+	if (op != NVMAP_CACHE_OP_WB)
+		clean_only_dirty = false;
+
+	cache_op.h = h;
+	cache_op.start = start;
+	cache_op.end = end;
+	cache_op.op = op;
+	cache_op.inner = h->flags == NVMAP_HANDLE_CACHEABLE ||
+			 h->flags == NVMAP_HANDLE_INNER_CACHEABLE;
+	cache_op.outer = h->flags == NVMAP_HANDLE_CACHEABLE;
+	cache_op.clean_only_dirty = clean_only_dirty;
+
+	nvmap_stats_inc(NS_CFLUSH_RQ, end - start);
+	err = do_cache_maint(&cache_op);
+	nvmap_kmaps_dec(h);
+	nvmap_handle_put(h);
+	return err;
+}
+
+int __nvmap_cache_maint(struct nvmap_client *client,
+			       struct nvmap_cache_op *op)
+{
+	struct vm_area_struct *vma;
+	struct nvmap_vma_priv *priv;
+	struct nvmap_handle *handle;
+	unsigned long start;
+	unsigned long end;
+	int err = 0;
+
+	if (!op->addr || op->op < NVMAP_CACHE_OP_WB ||
+	    op->op > NVMAP_CACHE_OP_WB_INV)
+		return -EINVAL;
+
+	handle = nvmap_handle_get_from_fd(op->handle);
+	if (!handle)
+		return -EINVAL;
+
+	down_read(&current->mm->mmap_sem);
+
+	vma = find_vma(current->active_mm, (unsigned long)op->addr);
+	if (!vma || !is_nvmap_vma(vma) ||
+	    (ulong)op->addr < vma->vm_start ||
+	    (ulong)op->addr >= vma->vm_end ||
+	    op->len > vma->vm_end - (ulong)op->addr) {
+		err = -EADDRNOTAVAIL;
+		goto out;
+	}
+
+	priv = (struct nvmap_vma_priv *)vma->vm_private_data;
+
+	if (priv->handle != handle) {
+		err = -EFAULT;
+		goto out;
+	}
+
+	start = (unsigned long)op->addr - vma->vm_start +
+		(vma->vm_pgoff << PAGE_SHIFT);
+	end = start + op->len;
+
+	err = __nvmap_do_cache_maint(client, priv->handle, start, end, op->op,
+				     false);
+out:
+	up_read(&current->mm->mmap_sem);
+	nvmap_handle_put(handle);
+	return err;
+}
+
+/*
+ * Perform cache op on the list of memory regions within passed handles.
+ * A memory region within handle[i] is identified by offsets[i], sizes[i]
+ *
+ * sizes[i] == 0  is a special case which causes handle wide operation,
+ * this is done by replacing offsets[i] = 0, sizes[i] = handles[i]->size.
+ * So, the input arrays sizes, offsets  are not guaranteed to be read-only
+ *
+ * This will optimze the op if it can.
+ * In the case that all the handles together are larger than the inner cache
+ * maint threshold it is possible to just do an entire inner cache flush.
+ */
+int nvmap_do_cache_maint_list(struct nvmap_handle **handles, u32 *offsets,
+			      u32 *sizes, int op, int nr)
+{
+	int i;
+	u64 total = 0;
+	u64 thresh = ~0;
+
+#if defined(CONFIG_NVMAP_CACHE_MAINT_BY_SET_WAYS)
+	thresh = cache_maint_inner_threshold;
+#endif
+
+	for (i = 0; i < nr; i++)
+		total += sizes[i] ? sizes[i] : handles[i]->size;
+
+	/* Full flush in the case the passed list is bigger than our
+	 * threshold. */
+	if (total >= thresh) {
+		for (i = 0; i < nr; i++) {
+			if (handles[i]->userflags &
+			    NVMAP_HANDLE_CACHE_SYNC) {
+				nvmap_handle_mkclean(handles[i], 0,
+						     handles[i]->size);
+				nvmap_zap_handle(handles[i], 0,
+						 handles[i]->size);
+			}
+		}
+
+		if (op == NVMAP_CACHE_OP_WB) {
+			inner_clean_cache_all();
+			outer_clean_all();
+		} else {
+			inner_flush_cache_all();
+			outer_flush_all();
+		}
+		nvmap_stats_inc(NS_CFLUSH_RQ, total);
+		nvmap_stats_inc(NS_CFLUSH_DONE, thresh);
+		trace_nvmap_cache_flush(total,
+					nvmap_stats_read(NS_ALLOC),
+					nvmap_stats_read(NS_CFLUSH_RQ),
+					nvmap_stats_read(NS_CFLUSH_DONE));
+	} else {
+		for (i = 0; i < nr; i++) {
+			u32 size = sizes[i] ? sizes[i] : handles[i]->size;
+			u32 offset = sizes[i] ? offsets[i] : 0;
+			int err = __nvmap_do_cache_maint(handles[i]->owner,
+							 handles[i], offset,
+							 offset + size,
+							 op, false);
+			if (err)
+				return err;
+		}
+	}
+
+	return 0;
+}

madisongh/linux-tegra

(cherry picked from commit eaf9044908ccfdc900d60dea9be4d9f526ffe3f4)
Signed-off-by: Sri Krishna chowdary <schowdary@nvidia.com>
Reviewed-on: http://git-master/r/764084

drivers/video/tegra/nvmap/nvmap_cache.c
@@ -1,0 +1,453 @@
+/*
+ * drivers/video/tegra/nvmap/nvmap_cache.c
+ *
+ * Copyright (c) 2011-2015, NVIDIA CORPORATION. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#define pr_fmt(fmt)	"nvmap: %s() " fmt, __func__
+
+#include <linux/highmem.h>
+#include <linux/io.h>
+
+#include <trace/events/nvmap.h>
+
+#include "nvmap_priv.h"
+
+static void inner_cache_maint(unsigned int op, void *vaddr, size_t size)
+{
+	if (op == NVMAP_CACHE_OP_WB_INV)
+		__dma_flush_range(vaddr, vaddr + size);
+	else if (op == NVMAP_CACHE_OP_INV)
+		__dma_map_area(vaddr, size, DMA_FROM_DEVICE);
+	else
+		__dma_map_area(vaddr, size, DMA_TO_DEVICE);
+}
+
+static void outer_cache_maint(unsigned int op, phys_addr_t paddr, size_t size)
+{
+	if (op == NVMAP_CACHE_OP_WB_INV)
+		outer_flush_range(paddr, paddr + size);
+	else if (op == NVMAP_CACHE_OP_INV)
+		outer_inv_range(paddr, paddr + size);
+	else
+		outer_clean_range(paddr, paddr + size);
+}
+
+static void heap_page_cache_maint(
+	struct nvmap_handle *h, unsigned long start, unsigned long end,
+	unsigned int op, bool inner, bool outer, bool clean_only_dirty)
+{
+	if (h->userflags & NVMAP_HANDLE_CACHE_SYNC) {
+		/*
+		 * zap user VA->PA mappings so that any access to the pages
+		 * will result in a fault and can be marked dirty
+		 */
+		nvmap_handle_mkclean(h, start, end-start);
+		nvmap_zap_handle(h, start, end - start);
+	}
+
+#ifdef NVMAP_LAZY_VFREE
+	if (inner) {
+		void *vaddr = NULL;
+
+		if (!h->vaddr) {
+			struct page **pages;
+			/* mutex lock protection is not necessary as it is
+			 * already increased in __nvmap_do_cache_maint to
+			 * protect from migrations.
+			 */
+			nvmap_kmaps_inc_no_lock(h);
+			pages = nvmap_pages(h->pgalloc.pages,
+					    h->size >> PAGE_SHIFT);
+			if (!pages)
+				goto per_page_cache_maint;
+			vaddr = vm_map_ram(pages,
+					h->size >> PAGE_SHIFT, -1,
+					nvmap_pgprot(h, PG_PROT_KERNEL));
+			nvmap_altfree(pages,
+				(h->size >> PAGE_SHIFT) * sizeof(*pages));
+		}
+		if (vaddr && atomic_long_cmpxchg(&h->vaddr, 0, (long)vaddr)) {
+			nvmap_kmaps_dec(h);
+			vm_unmap_ram(vaddr, h->size >> PAGE_SHIFT);
+		}
+		if (h->vaddr) {
+			/* Fast inner cache maintenance using single mapping */
+			inner_cache_maint(op, h->vaddr + start, end - start);
+			if (!outer)
+				return;
+			/* Skip per-page inner maintenance in loop below */
+			inner = false;
+		}
+	}
+per_page_cache_maint:
+	if (!h->vaddr)
+		nvmap_kmaps_dec(h);
+#endif
+
+	while (start < end) {
+		struct page *page;
+		void *kaddr, *vaddr;
+		phys_addr_t paddr;
+		unsigned long next;
+		unsigned long off;
+		size_t size;
+
+		page = nvmap_to_page(h->pgalloc.pages[start >> PAGE_SHIFT]);
+		next = min(((start + PAGE_SIZE) & PAGE_MASK), end);
+		off = start & ~PAGE_MASK;
+		size = next - start;
+		paddr = page_to_phys(page) + off;
+
+		if (inner) {
+			kaddr = kmap(page);
+			vaddr = (void *)kaddr + off;
+			BUG_ON(!kaddr);
+			inner_cache_maint(op, vaddr, size);
+			kunmap(page);
+		}
+
+		if (outer)
+			outer_cache_maint(op, paddr, size);
+		start = next;
+	}
+}
+
+#if defined(CONFIG_NVMAP_OUTER_CACHE_MAINT_BY_SET_WAYS)
+static bool fast_cache_maint_outer(unsigned long start,
+		unsigned long end, unsigned int op)
+{
+	bool result = false;
+	if (end - start >= cache_maint_outer_threshold) {
+		if (op == NVMAP_CACHE_OP_WB_INV) {
+			outer_flush_all();
+			result = true;
+		}
+		if (op == NVMAP_CACHE_OP_WB) {
+			outer_clean_all();
+			result = true;
+		}
+	}
+
+	return result;
+}
+#else
+static inline bool fast_cache_maint_outer(unsigned long start,
+		unsigned long end, unsigned int op)
+{
+	return false;
+}
+#endif
+
+#if defined(CONFIG_NVMAP_CACHE_MAINT_BY_SET_WAYS)
+static inline bool can_fast_cache_maint(struct nvmap_handle *h,
+	unsigned long start,
+	unsigned long end, unsigned int op)
+{
+	if ((op == NVMAP_CACHE_OP_INV) ||
+		((end - start) < cache_maint_inner_threshold))
+		return false;
+	return true;
+}
+#else
+static inline bool can_fast_cache_maint(struct nvmap_handle *h,
+	unsigned long start,
+	unsigned long end, unsigned int op)
+{
+	return false;
+}
+#endif
+
+static bool fast_cache_maint(struct nvmap_handle *h,
+	unsigned long start,
+	unsigned long end, unsigned int op,
+	bool clean_only_dirty)
+{
+	if (!can_fast_cache_maint(h, start, end, op))
+		return false;
+
+	if (h->userflags & NVMAP_HANDLE_CACHE_SYNC) {
+		nvmap_handle_mkclean(h, 0, h->size);
+		nvmap_zap_handle(h, 0, h->size);
+	}
+
+	if (op == NVMAP_CACHE_OP_WB_INV)
+		inner_flush_cache_all();
+	else if (op == NVMAP_CACHE_OP_WB)
+		inner_clean_cache_all();
+
+	/* outer maintenance */
+	if (h->flags != NVMAP_HANDLE_INNER_CACHEABLE) {
+		if(!fast_cache_maint_outer(start, end, op))
+		{
+			if (h->heap_pgalloc) {
+				heap_page_cache_maint(h, start,
+					end, op, false, true,
+					clean_only_dirty);
+			} else  {
+				phys_addr_t pstart;
+
+				pstart = start + h->carveout->base;
+				outer_cache_maint(op, pstart, end - start);
+			}
+		}
+	}
+	return true;
+}
+
+struct cache_maint_op {
+	phys_addr_t start;
+	phys_addr_t end;
+	unsigned int op;
+	struct nvmap_handle *h;
+	bool inner;
+	bool outer;
+	bool clean_only_dirty;
+};
+
+static int do_cache_maint(struct cache_maint_op *cache_work)
+{
+	pgprot_t prot;
+	unsigned long kaddr;
+	phys_addr_t pstart = cache_work->start;
+	phys_addr_t pend = cache_work->end;
+	phys_addr_t loop;
+	int err = 0;
+	struct nvmap_handle *h = cache_work->h;
+	struct nvmap_client *client;
+	unsigned int op = cache_work->op;
+	struct vm_struct *area = NULL;
+
+	if (!h || !h->alloc)
+		return -EFAULT;
+
+	client = h->owner;
+	if (can_fast_cache_maint(h, pstart, pend, op))
+		nvmap_stats_inc(NS_CFLUSH_DONE, cache_maint_inner_threshold);
+	else
+		nvmap_stats_inc(NS_CFLUSH_DONE, pend - pstart);
+	trace_nvmap_cache_maint(client, h, pstart, pend, op, pend - pstart);
+	trace_nvmap_cache_flush(pend - pstart,
+		nvmap_stats_read(NS_ALLOC),
+		nvmap_stats_read(NS_CFLUSH_RQ),
+		nvmap_stats_read(NS_CFLUSH_DONE));
+
+	wmb();
+	if (h->flags == NVMAP_HANDLE_UNCACHEABLE ||
+	    h->flags == NVMAP_HANDLE_WRITE_COMBINE || pstart == pend)
+		goto out;
+
+	if (fast_cache_maint(h, pstart, pend, op, cache_work->clean_only_dirty))
+		goto out;
+
+	if (h->heap_pgalloc) {
+		heap_page_cache_maint(h, pstart, pend, op, true,
+			(h->flags == NVMAP_HANDLE_INNER_CACHEABLE) ?
+			false : true, cache_work->clean_only_dirty);
+		goto out;
+	}
+
+	if (pstart > h->size || pend > h->size) {
+		pr_warn("cache maintenance outside handle\n");
+		err = -EINVAL;
+		goto out;
+	}
+
+	prot = nvmap_pgprot(h, PG_PROT_KERNEL);
+	area = alloc_vm_area(PAGE_SIZE, NULL);
+	if (!area) {
+		err = -ENOMEM;
+		goto out;
+	}
+	kaddr = (ulong)area->addr;
+
+	pstart += h->carveout->base;
+	pend += h->carveout->base;
+	loop = pstart;
+
+	while (loop < pend) {
+		phys_addr_t next = (loop + PAGE_SIZE) & PAGE_MASK;
+		void *base = (void *)kaddr + (loop & ~PAGE_MASK);
+		next = min(next, pend);
+
+		ioremap_page_range(kaddr, kaddr + PAGE_SIZE,
+			loop, prot);
+		inner_cache_maint(op, base, next - loop);
+		loop = next;
+		unmap_kernel_range(kaddr, PAGE_SIZE);
+	}
+
+	if (h->flags != NVMAP_HANDLE_INNER_CACHEABLE)
+		outer_cache_maint(op, pstart, pend - pstart);
+
+out:
+	if (area)
+		free_vm_area(area);
+	return err;
+}
+
+int __nvmap_do_cache_maint(struct nvmap_client *client,
+			struct nvmap_handle *h,
+			unsigned long start, unsigned long end,
+			unsigned int op, bool clean_only_dirty)
+{
+	int err;
+	struct cache_maint_op cache_op;
+
+	h = nvmap_handle_get(h);
+	if (!h)
+		return -EFAULT;
+
+	nvmap_kmaps_inc(h);
+	if (op == NVMAP_CACHE_OP_INV)
+		op = NVMAP_CACHE_OP_WB_INV;
+
+	/* clean only dirty is applicable only for Write Back operation */
+	if (op != NVMAP_CACHE_OP_WB)
+		clean_only_dirty = false;
+
+	cache_op.h = h;
+	cache_op.start = start;
+	cache_op.end = end;
+	cache_op.op = op;
+	cache_op.inner = h->flags == NVMAP_HANDLE_CACHEABLE ||
+			 h->flags == NVMAP_HANDLE_INNER_CACHEABLE;
+	cache_op.outer = h->flags == NVMAP_HANDLE_CACHEABLE;
+	cache_op.clean_only_dirty = clean_only_dirty;
+
+	nvmap_stats_inc(NS_CFLUSH_RQ, end - start);
+	err = do_cache_maint(&cache_op);
+	nvmap_kmaps_dec(h);
+	nvmap_handle_put(h);
+	return err;
+}
+
+int __nvmap_cache_maint(struct nvmap_client *client,
+			       struct nvmap_cache_op *op)
+{
+	struct vm_area_struct *vma;
+	struct nvmap_vma_priv *priv;
+	struct nvmap_handle *handle;
+	unsigned long start;
+	unsigned long end;
+	int err = 0;
+
+	if (!op->addr || op->op < NVMAP_CACHE_OP_WB ||
+	    op->op > NVMAP_CACHE_OP_WB_INV)
+		return -EINVAL;
+
+	handle = nvmap_handle_get_from_fd(op->handle);
+	if (!handle)
+		return -EINVAL;
+
+	down_read(&current->mm->mmap_sem);
+
+	vma = find_vma(current->active_mm, (unsigned long)op->addr);
+	if (!vma || !is_nvmap_vma(vma) ||
+	    (ulong)op->addr < vma->vm_start ||
+	    (ulong)op->addr >= vma->vm_end ||
+	    op->len > vma->vm_end - (ulong)op->addr) {
+		err = -EADDRNOTAVAIL;
+		goto out;
+	}
+
+	priv = (struct nvmap_vma_priv *)vma->vm_private_data;
+
+	if (priv->handle != handle) {
+		err = -EFAULT;
+		goto out;
+	}
+
+	start = (unsigned long)op->addr - vma->vm_start +
+		(vma->vm_pgoff << PAGE_SHIFT);
+	end = start + op->len;
+
+	err = __nvmap_do_cache_maint(client, priv->handle, start, end, op->op,
+				     false);
+out:
+	up_read(&current->mm->mmap_sem);
+	nvmap_handle_put(handle);
+	return err;
+}
+
+/*
+ * Perform cache op on the list of memory regions within passed handles.
+ * A memory region within handle[i] is identified by offsets[i], sizes[i]
+ *
+ * sizes[i] == 0  is a special case which causes handle wide operation,
+ * this is done by replacing offsets[i] = 0, sizes[i] = handles[i]->size.
+ * So, the input arrays sizes, offsets  are not guaranteed to be read-only
+ *
+ * This will optimze the op if it can.
+ * In the case that all the handles together are larger than the inner cache
+ * maint threshold it is possible to just do an entire inner cache flush.
+ */
+int nvmap_do_cache_maint_list(struct nvmap_handle **handles, u32 *offsets,
+			      u32 *sizes, int op, int nr)
+{
+	int i;
+	u64 total = 0;
+	u64 thresh = ~0;
+
+#if defined(CONFIG_NVMAP_CACHE_MAINT_BY_SET_WAYS)
+	thresh = cache_maint_inner_threshold;
+#endif
+
+	for (i = 0; i < nr; i++)
+		total += sizes[i] ? sizes[i] : handles[i]->size;
+
+	/* Full flush in the case the passed list is bigger than our
+	 * threshold. */
+	if (total >= thresh) {
+		for (i = 0; i < nr; i++) {
+			if (handles[i]->userflags &
+			    NVMAP_HANDLE_CACHE_SYNC) {
+				nvmap_handle_mkclean(handles[i], 0,
+						     handles[i]->size);
+				nvmap_zap_handle(handles[i], 0,
+						 handles[i]->size);
+			}
+		}
+
+		if (op == NVMAP_CACHE_OP_WB) {
+			inner_clean_cache_all();
+			outer_clean_all();
+		} else {
+			inner_flush_cache_all();
+			outer_flush_all();
+		}
+		nvmap_stats_inc(NS_CFLUSH_RQ, total);
+		nvmap_stats_inc(NS_CFLUSH_DONE, thresh);
+		trace_nvmap_cache_flush(total,
+					nvmap_stats_read(NS_ALLOC),
+					nvmap_stats_read(NS_CFLUSH_RQ),
+					nvmap_stats_read(NS_CFLUSH_DONE));
+	} else {
+		for (i = 0; i < nr; i++) {
+			u32 size = sizes[i] ? sizes[i] : handles[i]->size;
+			u32 offset = sizes[i] ? offsets[i] : 0;
+			int err = __nvmap_do_cache_maint(handles[i]->owner,
+							 handles[i], offset,
+							 offset + size,
+							 op, false);
+			if (err)
+				return err;
+		}
+	}
+
+	return 0;
+}