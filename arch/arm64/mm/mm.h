InES-HPMM/linux-l4t

arm64: mm: dma: support DMA32 coherent allocations
Support DMA32 coherent allocations for devices with 32-bit mask.
Bug 1454889

Change-Id: Ieaa996166761c6057eedc9d6e4e1ffc3a3f11d69
Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
Reviewed-on: http://git-master/r/441917
Reviewed-by: Sri Krishna Chowdary <schowdary@nvidia.com>
GVS: Gerrit_Virtual_Submit

arch/arm64/mm/mm.h
@@ -11,4 +11,8 @@ static inline pmd_t *pmd_off_k(unsigned long virt)
 /* consistent regions used by dma_alloc_attrs() */
 #define VM_ARM_DMA_CONSISTENT  0x20000000
 
+#ifdef CONFIG_ZONE_DMA32
+#define arm_dma_limit (0xffffffff)
+#else
 #define arm_dma_limit ((phys_addr_t)~0)
+#endif

madisongh/linux-tegra

arch/arm64/mm/mm.h
@@ -12,4 +12,8 @@ static inline pmd_t *pmd_off_k(unsigned long virt)
 /* consistent regions used by dma_alloc_attrs() */
 #define VM_ARM_DMA_CONSISTENT  0x20000000
 
+#ifdef CONFIG_ZONE_DMA
+#define arm_dma_limit (0xffffffff)
+#else
 #define arm_dma_limit ((phys_addr_t)~0)
+#endif