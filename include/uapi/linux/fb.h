BitOBSessiOn/android_kernel_nvidia_shieldtablet

drivers: video: tegra: Add 1000/1001 for HDMI_EXT
Previous changed limited 1000/1001 modes to only CEA SVD. A problem
raised by this was with the modes that were read from the HDMI EXT
blocks. They supported 1000/1001 as well.

This change marks the HDMI EXT modes as such and modifies the 1000/1001
code to consider these modes too.

Bug 200156983

Change-Id: I0c774fb75efac6f51b51f75b1badeeb47e90b66c
Signed-off-by: Aly Hirani <ahirani@nvidia.com>
Reviewed-on: http://git-master/r/839054
GVS: Gerrit_Virtual_Submit
Reviewed-by: Vinayak Pane <vpane@nvidia.com>
Reviewed-by: Jon Mayo <jmayo@nvidia.com>

include/uapi/linux/fb.h
@@ -274,8 +274,9 @@ struct fb_bitfield {
 #define FB_VMODE_1000DIV1001	0x008000
 #define FB_VMODE_IS_DETAILED	0x080000
 #define FB_VMODE_IS_CEA		0x100000
+#define FB_VMODE_IS_HDMI_EXT	0x200000
 
-#define FB_VMODE_MASK		0x18ffff
+#define FB_VMODE_MASK		0x38ffff
 
 #define FB_VMODE_YWRAP		0x10000	/* ywrap instead of panning     */
 #define FB_VMODE_SMOOTH_XPAN	0x20000	/* smooth xpan possible (internally used) */

madisongh/linux-tegra

(cherry picked from commit 90876716522bb7f3c9b0ddcee850e557039c991f)
Reviewed-on: http://git-master/r/929466
(cherry picked from commit 1a28e8e3824e48b087cb5d2612f3dd9a5f709f2f)
Reviewed-on: http://git-master/r/1018241
Reviewed-by: Mitch Luban <mluban@nvidia.com>
Tested-by: Mitch Luban <mluban@nvidia.com>

include/uapi/linux/fb.h
@@ -303,8 +303,9 @@ struct fb_bitfield {
 #define FB_VMODE_1000DIV1001	0x020000
 #define FB_VMODE_IS_DETAILED	0x200000
 #define FB_VMODE_IS_CEA		0x400000
+#define FB_VMODE_IS_HDMI_EXT	0x800000
 
-#define FB_VMODE_MASK		0x63ffff
+#define FB_VMODE_MASK		0xe3ffff
 
 #define FB_VMODE_YWRAP		0x40000  /* ywrap instead of panning */
 #define FB_VMODE_SMOOTH_XPAN	0x80000  /* smooth xpan possible (internally used) */