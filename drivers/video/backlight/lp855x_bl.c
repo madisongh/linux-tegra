f2f28e98da10acc1eba6ab03e65fc3a526ca91d6
Santosh Reddy Galma <galmar@nvidia.com>
2016-04-22 12:40:41 +0530

    backlight: lp855x: correct include path of board-panel.h
    fix board-panel.h include path in driver as board-panel.h
    file is moved to kernel-display/drivers/video/tegra/dc
    
    Change-Id: Iafedd8dc78f069fa3acebd0b0d74f6844223b1a2
    Signed-off-by: Santosh Reddy Galma <galmar@nvidia.com>
    Reviewed-on: http://git-master/r/1131000
    Reviewed-by: Daniel Solomon <daniels@nvidia.com>
    Tested-by: Daniel Solomon <daniels@nvidia.com>
    Reviewed-by: Animesh Kishore <ankishore@nvidia.com>
    Reviewed-by: Mitch Luban <mluban@nvidia.com>

--- drivers/video/backlight/lp855x_bl.c
+++ drivers/video/backlight/lp855x_bl.c
@@ -18,7 +18,7 @@
 #include <linux/platform_data/lp855x.h>
 #include <linux/pwm.h>
 #include <linux/regulator/consumer.h>
-#include <board-panel.h>
+#include "../../../kernel-display/drivers/video/tegra/dc/board-panel.h"
 
 /* LP8550/1/2/3/6 Registers */
 #define LP855X_BRIGHTNESS_CTRL		0x00
