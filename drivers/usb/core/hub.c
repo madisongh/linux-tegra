48c4a8c5cb2dbcf57e9dea346b74d93d58e1279f
JC Kuo <jckuo@nvidia.com>
2016-04-28 14:09:11 +0800

    usb: hub: runtime resume hub before access
    usb_port_resume() occasionally access hub when hub is runtime suspended.
    
    [  326.817235] hub 1-0:1.0: hub_port_status failed (err = -108)
    [  326.822895] usb usb1-port2: cannot disable (err = -108)
    
    This commit adds usb_autopm_get_interface() in usb_port_resume() before
    access hub to ensure hub is at runtime active state.
    
    bug 200184140
    bug 200188528
    
    Change-Id: I8c0b7d0a3498f936d67f13c43934e86dbda56095
    Reviewed-on: http://git-master/r/1134009
    Signed-off-by: JC Kuo <jckuo@nvidia.com>
    Reviewed-on: http://git-master/r/1164819
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -1,6 +1,7 @@
 /*
  * USB hub driver.
  *
+ * Copyright (c) 2016, NVIDIA CORPORATION.  All rights reserved.
  * (C) Copyright 1999 Linus Torvalds
  * (C) Copyright 1999 Johannes Erdfelt
  * (C) Copyright 1999 Gregory P. Smith
@@ -3395,6 +3396,7 @@ static int wait_for_ss_port_enable(struct usb_device *udev,
 int usb_port_resume(struct usb_device *udev, pm_message_t msg)
 {
 	struct usb_hub	*hub = usb_hub_to_struct_hub(udev->parent);
+	struct usb_interface *intf = to_usb_interface(hub->intfdev);
 	struct usb_port *port_dev = hub->ports[udev->portnum  - 1];
 	int		port1 = udev->portnum;
 	int		status;
@@ -3411,6 +3413,7 @@ int usb_port_resume(struct usb_device *udev, pm_message_t msg)
 
 	usb_lock_port(port_dev);
 
+	usb_autopm_get_interface(intf);
 	/* Skip the initial Clear-Suspend step for a remote wakeup */
 	status = hub_port_status(hub, port1, &portstatus, &portchange);
 	if (status == 0 && !port_is_suspended(hub, portstatus))
@@ -3474,6 +3477,7 @@ int usb_port_resume(struct usb_device *udev, pm_message_t msg)
 		usb_enable_ltm(udev);
 		usb_unlocked_enable_lpm(udev);
 	}
+	usb_autopm_put_interface(intf);
 
 	usb_unlock_port(port_dev);
 
