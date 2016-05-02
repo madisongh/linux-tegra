compulab/trimslice-android-kernel

cpufreq: properly clamp QoS min/max
typically PM QoS clients request the maximum cpu frequency by
setting cpu_freq_min to INT_MAX. Therefore, we need to clamp INT_MAX
to whatever the maximum frequency allowed by the policy to make
the requested cpu_freq_min is same as the maximum allowed cpu
frequency. Inverse logic applies to the cpu_freq_max as well.

Bug 932477

Signed-off-by: Donghan Ryu <dryu@nvidia.com>

Change-Id: I9cfc6c9750de0c7ba15e1a189cd5496fa7a31f33
Reviewed-on: http://git-master/r/78401
Reviewed-by: Donghan Ryu <dryu@nvidia.com>
Tested-by: Donghan Ryu <dryu@nvidia.com>
Reviewed-by: Automatic_Commit_Validation_User
Reviewed-by: Aleksandr Frid <afrid@nvidia.com>
Reviewed-by: Antti Miettinen <amiettinen@nvidia.com>
Tested-by: Antti Miettinen <amiettinen@nvidia.com>
Tested-by: Gerrit_Virtual_Submit
Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

drivers/cpufreq/cpufreq.c
@@ -1721,8 +1721,8 @@ static int __cpufreq_set_policy(struct cpufreq_policy *data,
 	int ret = 0;
 	unsigned int pmin = policy->min;
 	unsigned int pmax = policy->max;
-	unsigned int qmin = pm_qos_request(PM_QOS_CPU_FREQ_MIN);
-	unsigned int qmax = pm_qos_request(PM_QOS_CPU_FREQ_MAX);
+	unsigned int qmin = min(pm_qos_request(PM_QOS_CPU_FREQ_MIN), data->max);
+	unsigned int qmax = max(pm_qos_request(PM_QOS_CPU_FREQ_MAX), data->min);
 
 	cpufreq_debug_disable_ratelimit();
 	dprintk("setting new policy for CPU %u: %u - %u (%u - %u) kHz\n",

madisongh/linux-tegra

(cherry picked from commit 9688430a621515f3afe3e0ab358ea7ad5aab6d91)
Change-Id: Ie5edcd4a873ed5cce45ec0f9b981f28db6091ff1
Signed-off-by: Pritesh Raithatha <praithatha@nvidia.com>
Reviewed-on: http://git-master/r/80002
Rebase-Id: Rcdb51f6b884ccd3d6324de5569dc0ecf4994c3f0
(cherry picked from commit 0bae6cc297eb76c109b8b827d986781395a8b971)
Reviewed-on: http://git-master/r/1132340
Tested-by: Bharat Nihalani <bnihalani@nvidia.com>

drivers/cpufreq/cpufreq.c
@@ -2116,8 +2116,8 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 
 	unsigned int pmin = new_policy->min;
 	unsigned int pmax = new_policy->max;
-	unsigned int qmin = pm_qos_request(PM_QOS_CPU_FREQ_MIN);
-	unsigned int qmax = pm_qos_request(PM_QOS_CPU_FREQ_MAX);
+	unsigned int qmin = min(pm_qos_request(PM_QOS_CPU_FREQ_MIN), policy->max);
+	unsigned int qmax = max(pm_qos_request(PM_QOS_CPU_FREQ_MAX), policy->min);
 
 	pr_debug("setting new policy for CPU %u: %u - %u (%u - %u) kHz\n",
 		 new_policy->cpu, pmin, pmax, qmin, qmax);