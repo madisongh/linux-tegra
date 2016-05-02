bba2379067bd14fc47ebf81507cd487409979311
ASWINI KANDE <akande@nvidia.com>
2016-05-02 15:54:49 +0530

    usb: host: xhci: Fix to avoid integer overflow
    Fix coverity error with assignment
    of a variable before evaluation of
    expression.
    
    Coverity Id 24263
    
    Bug 200192584
    
    Change-Id: I9c3c8bf65c630af978d30e63fe1d49fde173fa43
    Signed-off-by: ASWINI KANDE <akande@nvidia.com>
    Reviewed-on: http://git-master/r/1139632
    (cherry picked from commit 2f78f5fda0b4069b68e8891e2ae1505ad5bb1691)
    Reviewed-on: http://git-master/r/1172957
    Reviewed-by: Automatic_Commit_Validation_User
    Tested-by: Rohith Seelaboyina <rseelaboyina@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Rohith Seelaboyina <rseelaboyina@nvidia.com>
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -2,6 +2,7 @@
  * xHCI host controller driver
  *
  * Copyright (C) 2008 Intel Corp.
+ * Copyright (c) 2016, NVIDIA CORPORATION.  All rights reserved.
  *
  * Author: Sarah Sharp
  * Some code borrowed from the Linux EHCI driver.
@@ -4337,22 +4338,22 @@ static unsigned long long xhci_calculate_intel_u1_timeout(
 		struct usb_device *udev,
 		struct usb_endpoint_descriptor *desc)
 {
-	unsigned long long timeout_ns;
+	unsigned long long timeout_ns = udev->u1_params.sel;
 	int ep_type;
 	int intr_type;
 
 	ep_type = usb_endpoint_type(desc);
 	switch (ep_type) {
 	case USB_ENDPOINT_XFER_CONTROL:
-		timeout_ns = udev->u1_params.sel * 3;
+		timeout_ns *= 3;
 		break;
 	case USB_ENDPOINT_XFER_BULK:
-		timeout_ns = udev->u1_params.sel * 5;
+		timeout_ns *= 5;
 		break;
 	case USB_ENDPOINT_XFER_INT:
 		intr_type = usb_endpoint_interrupt_type(desc);
 		if (intr_type == USB_ENDPOINT_INTR_NOTIFICATION) {
-			timeout_ns = udev->u1_params.sel * 3;
+			timeout_ns *= 3;
 			break;
 		}
 		/* Otherwise the calculation is the same as isoc eps */
