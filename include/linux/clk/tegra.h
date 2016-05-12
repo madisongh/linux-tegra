fb00859bc6b9abca870e07cb7c3d4e22c6fb7595
Wenjia Zhou <wenjiaz@nvidia.com>
2016-05-11 17:49:50 -0700

    include:media: Port camera hearders
    Bug 200193751
    
    Change-Id: I1d22dff80f6439349ace2f68754efdb888d9697e
    Signed-off-by: Wenjia Zhou <wenjiaz@nvidia.com>

--- include/linux/clk/tegra.h
+++ include/linux/clk/tegra.h
@@ -130,6 +130,7 @@ enum tegra_clk_ex_param {
 	TEGRA_CLK_PLLD_DSI_OUT_ENB,
 	TEGRA_CLK_PLLD_MIPI_MUX_SEL,
 	TEGRA_CLK_SOR_CLK_SEL,
+	TEGRA_CLK_MIPI_CSI_OUT_ENB,
 };
 
 void tegra_periph_reset_deassert(struct clk *c);
