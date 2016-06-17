85bf0130cb44632f0879bb93a9ee0f231ceeb2de
Alex Van Brunt <avanbrunt@nvidia.com>
2016-06-17 16:49:23 -0700

    arm64: cpuinfo: fix blank line before MTS version
    There was a blank line between CPU revision and MTS version. This made it
    look like the MTS version was associated with the next core.
    
    Instead put the blank line after the MTS version.
    
    Change-Id: Ida839b5b5592036ba46f5b63d9b4d97aebffeb1e
    Signed-off-by: Alex Van Brunt <avanbrunt@nvidia.com>
    Reviewed-on: http://git-master/r/1167283
    Reviewed-by: Richard Wiley <rwiley@nvidia.com>
    Tested-by: Richard Wiley <rwiley@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- arch/arm64/kernel/cpuinfo.c
+++ arch/arm64/kernel/cpuinfo.c
@@ -147,9 +147,10 @@ static int c_show(struct seq_file *m, void *v)
 		seq_printf(m, "CPU architecture: 8\n");
 		seq_printf(m, "CPU variant\t: 0x%x\n", MIDR_VARIANT(midr));
 		seq_printf(m, "CPU part\t: 0x%03x\n", MIDR_PARTNUM(midr));
-		seq_printf(m, "CPU revision\t: %d\n\n", MIDR_REVISION(midr));
+		seq_printf(m, "CPU revision\t: %d\n", MIDR_REVISION(midr));
 		if (MIDR_IMPLEMENTOR(midr) == ARM_CPU_IMP_NVIDIA)
 			seq_printf(m, "MTS version\t: %u\n", cpuinfo->reg_aidr);
+		seq_printf(m, "\n");
 	}
 
 	return 0;
