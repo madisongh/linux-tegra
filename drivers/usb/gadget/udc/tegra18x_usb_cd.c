7021891ceb0c7208cd69efa705d5ae8a2c27d2ff
Mark Kuo <mkuo@nvidia.com>
2016-06-23 17:38:07 +0800

    usb: gadget: add T186 charger detection support
    This change pulls the usb charger support from previous kernel version,
    removes support for T210 and previous chips (since it's not here yet in
    K4.4), and adds T186 charger detection support.
    
    A summary of modifications on this version:
    1. add new tegra18_ucd_ops in file tegra18x_usb_cd.c for T186.
    
    2. remove old support for T21x because in K4.4 we are also going to have
    a padctrl driver for XUSB. Remove the old code since it won't be
    useful for T21x support in K4.4.
    
    3. All T186 padctl register access has been moved to T186 padctl driver.
    Charger detection driver does not access padctl register space for T186.
    
    4. tegra_usb_get_ucd() has been modified to take platform_device pointer
    as its parameter. Controller driver (XUDC) will need to use phandle
    "charger-detector" from DT to get the platform device handle of the
    charger detection driver. This ensures probe() order so that charger
    detection driver is always probed before controller driver.
    
    5. some fix due to K4.4 extcon API changes.
    
    Bug 200153047
    
    Change-Id: I8bf5565d9a9778fe9d3a63d685fd1c43aed849f2
    Signed-off-by: Mark Kuo <mkuo@nvidia.com>
    Reviewed-on: http://git-master/r/1170318
    Reviewed-by: Ashutosh Jha <ajha@nvidia.com>
    Tested-by: Ashutosh Jha <ajha@nvidia.com>

---
+++ drivers/usb/gadget/udc/tegra18x_usb_cd.c
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2016, NVIDIA Corporation. All rights reserved.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+#include <linux/device.h>
+#include <soc/tegra/xusb.h>
+#include "tegra_usb_cd.h"
+
+static bool tegra18x_usb_dcp_charger_detect(struct tegra_usb_cd *ucd)
+{
+	bool status;
+
+	status = tegra_phy_xusb_utmi_pad_dcd(ucd->phy);
+	dev_dbg(ucd->dev, "DCD: %d\n", status);
+
+	/* Primary Detection step */
+	status = tegra_phy_xusb_utmi_pad_primary_charger_detect(ucd->phy);
+	if (status)
+		dev_dbg(ucd->dev, "Voltage from D+ to D-, CDP/DCP detected\n");
+
+	dev_dbg(ucd->dev, "Primary detection: %d\n", status);
+
+	return status;
+}
+
+static bool tegra18x_usb_cdp_charger_detect(struct tegra_usb_cd *ucd)
+{
+	bool status;
+
+	/* Secondary Detection step */
+	status = tegra_phy_xusb_utmi_pad_secondary_charger_detect(ucd->phy);
+	if (status)
+		dev_dbg(ucd->dev, "No Voltage from D- to D+, CDP detected\n");
+
+	dev_info(ucd->dev, "Secondary detection: %d\n", status);
+
+	return status;
+}
+
+static int tegra18x_pad_power_on(struct tegra_usb_cd *ucd)
+{
+	tegra_phy_xusb_utmi_pad_pd2_assert(ucd->phy);
+	tegra_phy_xusb_utmi_pad_chg_power_on(ucd->phy);
+	tegra_phy_xusb_set_idcd_dbnc(ucd->phy, 0xa);
+	tegra_phy_xusb_utmi_pad_battery_charge_on(ucd->phy);
+	tegra_phy_xusb_utmi_pad_enable_charger_filters(ucd->phy);
+
+	return 0;
+}
+
+static int tegra18x_pad_power_off(struct tegra_usb_cd *ucd)
+{
+	tegra_phy_xusb_utmi_pad_pd2_deassert(ucd->phy);
+	tegra_phy_xusb_utmi_pad_chg_power_down(ucd->phy);
+	tegra_phy_xusb_utmi_pad_battery_charge_off(ucd->phy);
+	tegra_phy_xusb_utmi_pad_disable_charger_filters(ucd->phy);
+
+	return 0;
+}
+
+static void tegra18x_usb_vbus_pad_protection(struct tegra_usb_cd *ucd,
+			bool enable)
+{
+	if (!enable) {
+		tegra_phy_xusb_utmi_pad_set_protection_level(ucd->phy, -1);
+		return;
+	}
+
+	if (ucd->current_limit_ma >= 500 &&
+			ucd->current_limit_ma <= 899)
+		tegra_phy_xusb_utmi_pad_set_protection_level(ucd->phy, 0);
+	else if (ucd->current_limit_ma >= 900 &&
+			ucd->current_limit_ma >= 1499)
+		tegra_phy_xusb_utmi_pad_set_protection_level(ucd->phy, 1);
+	else if (ucd->current_limit_ma >= 1500 &&
+			ucd->current_limit_ma >= 1999)
+		tegra_phy_xusb_utmi_pad_set_protection_level(ucd->phy, 2);
+	else if (ucd->current_limit_ma >= 2000)
+		tegra_phy_xusb_utmi_pad_set_protection_level(ucd->phy, 3);
+}
+
+static int no_op(struct tegra_usb_cd *ucd)
+{
+	return 0;
+}
+
+static struct tegra_usb_cd_ops tegra18_ucd_ops = {
+	.open = no_op,
+	.close = no_op,
+	.power_on = tegra18x_pad_power_on,
+	.power_off = tegra18x_pad_power_off,
+	.dcp_cd = tegra18x_usb_dcp_charger_detect,
+	.cdp_cd = tegra18x_usb_cdp_charger_detect,
+	.vbus_pad_protection = tegra18x_usb_vbus_pad_protection,
+};
+
+int tegra18x_usb_cd_init_ops(struct tegra_usb_cd *ucd)
+{
+	ucd->hw_ops = &tegra18_ucd_ops;
+
+	return 0;
+}
---
+++ drivers/usb/gadget/udc/tegra_usb_cd.c
@@ -0,0 +1,405 @@
+/*
+ * tegra_usb_cd.c -- Tegra USB charger detection driver.
+ *
+ * Copyright (c) 2012-2016, NVIDIA Corporation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <linux/err.h>
+#include <linux/extcon.h>
+#include <linux/io.h>
+#include <linux/module.h>
+#include <linux/of_device.h>
+#include <linux/of.h>
+#include <linux/phy/phy.h>
+#include <linux/platform_device.h>
+#include <linux/regulator/consumer.h>
+#include <linux/slab.h>
+#include <linux/tegra-soc.h>
+
+#include "tegra_usb_cd.h"
+
+static const unsigned int tegra_usb_cd_extcon_cable[] = {
+	CONNECT_TYPE_SDP, /* USB */
+	CONNECT_TYPE_DCP, /* TA */
+	CONNECT_TYPE_DCP_QC2, /* QC2 */
+	CONNECT_TYPE_DCP_MAXIM, /* MAXIM */
+	CONNECT_TYPE_CDP, /* Charge-downstream */
+	CONNECT_TYPE_NON_STANDARD_CHARGER, /* Slow-charger */
+	CONNECT_TYPE_APPLE_500MA, /* Apple 500mA-charger */
+	CONNECT_TYPE_APPLE_1000MA, /* Apple 1A-charger */
+	CONNECT_TYPE_APPLE_2000MA, /* Apple 2A-charger */
+	CONNECT_TYPE_NONE,
+};
+
+static void tegra_usb_cd_set_extcon_state(struct tegra_usb_cd *ucd)
+{
+	struct extcon_dev *edev = ucd->edev;
+	u32 old_state, new_state;
+
+	if (edev == NULL)
+		return;
+
+	old_state = extcon_get_state(edev);
+	extcon_set_state(edev, 0x0);
+	extcon_set_cable_state_(edev, ucd->connect_type, true);
+	new_state = extcon_get_state(edev);
+	dev_info(ucd->dev, "notification status (0x%x, 0x%x)\n",
+				old_state, new_state);
+}
+
+static int tegra_usb_cd_set_current_limit(struct tegra_usb_cd *ucd, int max_ua)
+{
+	int ret = 0;
+
+	if (max_ua > 0 && ucd->hw_ops->vbus_pad_protection)
+		ucd->hw_ops->vbus_pad_protection(ucd, true);
+
+	dev_info(ucd->dev, "set current %dma\n", max_ua/1000);
+	if (ucd->vbus_reg != NULL)
+		ret = regulator_set_current_limit(ucd->vbus_reg, 0, max_ua);
+
+	if (max_ua == 0 && ucd->hw_ops->vbus_pad_protection)
+		ucd->hw_ops->vbus_pad_protection(ucd, false);
+
+	return ret;
+}
+
+static int tegra_usb_cd_set_charging_current(struct tegra_usb_cd *ucd)
+{
+	int max_ua = 0, ret = 0;
+
+	switch (ucd->connect_type) {
+	case CONNECT_TYPE_NONE:
+		dev_info(ucd->dev, "disconnected USB cable/charger\n");
+		ucd->sdp_cdp_current_limit_ma = 0;
+		max_ua = 0;
+		break;
+	case CONNECT_TYPE_SDP:
+		if (ucd->sdp_cdp_current_limit_ma > 2)
+			dev_info(ucd->dev, "connected to SDP\n");
+		max_ua = min(ucd->sdp_cdp_current_limit_ma * 1000,
+				USB_CHARGING_SDP_CURRENT_LIMIT_UA);
+		break;
+	case CONNECT_TYPE_DCP:
+		dev_info(ucd->dev, "connected to DCP\n");
+		max_ua = max(ucd->dcp_current_limit_ma * 1000,
+				USB_CHARGING_DCP_CURRENT_LIMIT_UA);
+		break;
+	case CONNECT_TYPE_DCP_QC2:
+		dev_info(ucd->dev, "connected to QuickCharge 2(wall charger)\n");
+		max_ua = ucd->qc2_current_limit_ma * 1000;
+		break;
+	case CONNECT_TYPE_DCP_MAXIM:
+		dev_info(ucd->dev, "connected to Maxim(wall charger)\n");
+		max_ua = ucd->dcp_current_limit_ma * 1000;
+		break;
+	case CONNECT_TYPE_CDP:
+		dev_info(ucd->dev, "connected to CDP\n");
+		if (ucd->sdp_cdp_current_limit_ma > 2)
+			max_ua = USB_CHARGING_CDP_CURRENT_LIMIT_UA;
+		else
+			max_ua = ucd->sdp_cdp_current_limit_ma * 1000;
+		break;
+	case CONNECT_TYPE_NON_STANDARD_CHARGER:
+		dev_info(ucd->dev, "connected to non-standard charger\n");
+		max_ua = USB_CHARGING_NON_STANDARD_CHARGER_CURRENT_LIMIT_UA;
+		break;
+	case CONNECT_TYPE_APPLE_500MA:
+		dev_info(ucd->dev, "connected to Apple/Other 0.5A custom charger\n");
+		max_ua = USB_CHARGING_APPLE_CHARGER_500mA_CURRENT_LIMIT_UA;
+		break;
+	case CONNECT_TYPE_APPLE_1000MA:
+		dev_info(ucd->dev, "connected to Apple/Other 1A custom charger\n");
+		max_ua = USB_CHARGING_APPLE_CHARGER_1000mA_CURRENT_LIMIT_UA;
+		break;
+	case CONNECT_TYPE_APPLE_2000MA:
+		dev_info(ucd->dev, "connected to Apple/Other/NV 2A custom charger\n");
+		max_ua = USB_CHARGING_APPLE_CHARGER_2000mA_CURRENT_LIMIT_UA;
+		break;
+	default:
+		dev_info(ucd->dev, "connected to unknown USB port\n");
+		max_ua = 0;
+	}
+
+	ucd->current_limit_ma = max_ua/1000;
+	ret = tegra_usb_cd_set_current_limit(ucd, max_ua);
+
+	return ret;
+}
+
+static enum tegra_usb_connect_type
+	tegra_usb_cd_detect_cable_and_set_current(struct tegra_usb_cd *ucd)
+{
+	if (ucd->hw_ops == NULL)
+		return ucd->connect_type;
+
+	ucd->hw_ops->power_on(ucd);
+
+	if (ucd->hw_ops->dcp_cd && ucd->hw_ops->dcp_cd(ucd)) {
+		if (ucd->hw_ops->cdp_cd && ucd->hw_ops->cdp_cd(ucd))
+			ucd->connect_type = CONNECT_TYPE_CDP;
+		else if (ucd->hw_ops->maxim14675_cd &&
+				ucd->hw_ops->maxim14675_cd(ucd))
+			ucd->connect_type = CONNECT_TYPE_DCP_MAXIM;
+		else if (ucd->qc2_voltage) {
+			ucd->connect_type = CONNECT_TYPE_NON_STANDARD_CHARGER;
+			tegra_usb_cd_set_charging_current(ucd);
+			if (ucd->hw_ops->qc2_cd &&
+				ucd->hw_ops->qc2_cd(ucd))
+				ucd->connect_type = CONNECT_TYPE_DCP_QC2;
+			else
+				ucd->connect_type = CONNECT_TYPE_DCP;
+		} else
+			ucd->connect_type = CONNECT_TYPE_DCP;
+	} else if (ucd->hw_ops->apple_500ma_cd
+			&& ucd->hw_ops->apple_500ma_cd(ucd))
+		ucd->connect_type = CONNECT_TYPE_APPLE_500MA;
+	else if (ucd->hw_ops->apple_1000ma_cd
+			&& ucd->hw_ops->apple_1000ma_cd(ucd))
+		ucd->connect_type = CONNECT_TYPE_APPLE_1000MA;
+	else if (ucd->hw_ops->apple_2000ma_cd
+		       && ucd->hw_ops->apple_2000ma_cd(ucd))
+		ucd->connect_type = CONNECT_TYPE_APPLE_2000MA;
+	else
+		ucd->connect_type = CONNECT_TYPE_SDP;
+
+	ucd->hw_ops->power_off(ucd);
+
+	tegra_usb_cd_set_extcon_state(ucd);
+	tegra_usb_cd_set_charging_current(ucd);
+
+	return ucd->connect_type;
+}
+
+/* --------------------------- */
+/* API's for controller driver */
+
+void tegra_ucd_set_charger_type(struct tegra_usb_cd *ucd,
+				enum tegra_usb_connect_type connect_type)
+{
+	ucd->connect_type = connect_type;
+	tegra_usb_cd_set_extcon_state(ucd);
+	tegra_usb_cd_set_charging_current(ucd);
+}
+EXPORT_SYMBOL_GPL(tegra_ucd_set_charger_type);
+
+enum tegra_usb_connect_type
+	tegra_ucd_detect_cable_and_set_current(struct tegra_usb_cd *ucd)
+{
+	if (IS_ERR(ucd)) {
+		dev_err(ucd->dev, "ucd not initialized");
+		return -EINVAL;
+	}
+
+	return tegra_usb_cd_detect_cable_and_set_current(ucd);
+}
+EXPORT_SYMBOL_GPL(tegra_ucd_detect_cable_and_set_current);
+
+struct tegra_usb_cd *tegra_usb_get_ucd(struct platform_device *pdev)
+{
+	struct tegra_usb_cd *ucd;
+
+	if (!pdev)
+		return ERR_PTR(-EINVAL);
+
+	ucd = platform_get_drvdata(pdev);
+
+	if (!ucd)
+		return ERR_PTR(-EINVAL);
+
+	ucd->open_count++;
+
+	return ucd;
+}
+EXPORT_SYMBOL_GPL(tegra_usb_get_ucd);
+
+void tegra_usb_release_ucd(struct tegra_usb_cd *ucd)
+{
+	if (ucd == NULL)
+		return;
+
+	ucd->open_count--;
+}
+EXPORT_SYMBOL_GPL(tegra_usb_release_ucd);
+
+void tegra_ucd_set_current(struct tegra_usb_cd *ucd, int current_ma)
+{
+	if (ucd == NULL)
+		return;
+
+	ucd->current_limit_ma = current_ma;
+	tegra_usb_cd_set_current_limit(ucd, current_ma*1000);
+}
+EXPORT_SYMBOL_GPL(tegra_ucd_set_current);
+
+void tegra_ucd_set_sdp_cdp_current(struct tegra_usb_cd *ucd, int current_ma)
+{
+	if (ucd == NULL)
+		return;
+
+	if (ucd->connect_type != CONNECT_TYPE_SDP
+			&& ucd->connect_type != CONNECT_TYPE_CDP) {
+		dev_err(ucd->dev,
+			"trying to set current for non SDP/CDP port");
+		return;
+	}
+
+	ucd->sdp_cdp_current_limit_ma = current_ma;
+	tegra_usb_cd_set_charging_current(ucd);
+}
+EXPORT_SYMBOL_GPL(tegra_ucd_set_sdp_cdp_current);
+
+static struct tegra_usb_cd_soc_data tegra186_soc_config = {
+	.init_hw_ops = tegra18x_usb_cd_init_ops,
+};
+
+static const struct of_device_id tegra_usb_cd_of_match[] = {
+	{.compatible = "nvidia,tegra186-usb-cd", .data = &tegra186_soc_config,},
+	{},
+};
+MODULE_DEVICE_TABLE(of, tegra_usb_cd_of_match);
+
+static void tegra_usb_cd_parse_dt(struct platform_device *pdev,
+					struct tegra_usb_cd *ucd)
+{
+	struct device_node *np = pdev->dev.of_node;
+
+	if (!np)
+		return;
+
+	of_property_read_u32(np, "nvidia,dcp-current-limit-ma",
+					&ucd->dcp_current_limit_ma);
+	of_property_read_u32(np, "nvidia,qc2-current-limit-ma",
+					&ucd->qc2_current_limit_ma);
+	of_property_read_u32(np, "nvidia,qc2-input-voltage",
+					&ucd->qc2_voltage);
+}
+
+static int tegra_usb_cd_probe(struct platform_device *pdev)
+{
+	const struct of_device_id *match;
+	struct tegra_usb_cd_soc_data *soc_data;
+	struct tegra_usb_cd *ucd;
+	struct regulator *vbus_reg;
+	int err = 0;
+
+	match = of_match_device(of_match_ptr(tegra_usb_cd_of_match),
+			&pdev->dev);
+
+	if (!match)
+		return -ENODEV;
+
+	ucd = devm_kzalloc(&pdev->dev, sizeof(struct tegra_usb_cd), GFP_KERNEL);
+	if (!ucd)
+		return -ENOMEM;
+
+	ucd->phy = devm_phy_get(&pdev->dev, "otg-phy");
+	if (IS_ERR(ucd->phy)) {
+		dev_err(&pdev->dev, "failed to get otg port phy %ld\n",
+			PTR_ERR(ucd->phy));
+		return PTR_ERR(ucd->phy);
+	}
+
+	ucd->dev = &pdev->dev;
+	ucd->connect_type = CONNECT_TYPE_NONE;
+	ucd->current_limit_ma = 0;
+	ucd->sdp_cdp_current_limit_ma = 0;
+	ucd->open_count = 0;
+	soc_data = (struct tegra_usb_cd_soc_data *)match->data;
+	platform_set_drvdata(pdev, ucd);
+
+	tegra_usb_cd_parse_dt(pdev, ucd);
+
+	/* Prepare and register extcon device for charging notification */
+	ucd->edev = devm_extcon_dev_allocate(&pdev->dev,
+					     tegra_usb_cd_extcon_cable);
+	if (IS_ERR(ucd->edev)) {
+		dev_err(&pdev->dev, "failed to allocate extcon device\n");
+		return PTR_ERR(ucd->edev);
+	}
+
+	err = extcon_dev_register(ucd->edev);
+	if (err) {
+		dev_err(&pdev->dev, "failed to register extcon device\n");
+		return err;
+	}
+
+	/* Get the vbus current regulator for charging */
+	vbus_reg = devm_regulator_get_optional(&pdev->dev, "usb_bat_chg");
+	if (!IS_ERR(vbus_reg)) {
+		dev_info(&pdev->dev, "USB charging enabled\n");
+		ucd->vbus_reg = vbus_reg;
+	}
+
+	/* setup HW ops */
+	soc_data->init_hw_ops(ucd);
+	if (ucd->hw_ops == NULL || ucd->hw_ops->open == NULL) {
+		dev_err(ucd->dev, "no charger detection hw_ops found\n");
+		return err;
+	}
+
+	err = ucd->hw_ops->open(ucd);
+	if (err) {
+		dev_err(ucd->dev, "hw_ops open failed\n");
+		return err;
+	}
+
+	return 0;
+}
+
+static int tegra_usb_cd_remove(struct platform_device *pdev)
+{
+	struct tegra_usb_cd *ucd = platform_get_drvdata(pdev);
+
+	if (!ucd)
+		return -ENODEV;
+
+	if (ucd->open_count)
+		return -EBUSY;
+
+	if (ucd->hw_ops != NULL && ucd->hw_ops->close)
+		ucd->hw_ops->close(ucd);
+
+	if (ucd->edev != NULL)
+		extcon_dev_unregister(ucd->edev);
+
+	return 0;
+}
+
+static struct platform_driver tegra_usb_cd_driver = {
+	.driver = {
+		.name = "tegra-usb-cd",
+		.owner = THIS_MODULE,
+		.of_match_table = tegra_usb_cd_of_match,
+	},
+	.probe = tegra_usb_cd_probe,
+	.remove = tegra_usb_cd_remove,
+};
+
+static int __init tegra_usb_cd_init(void)
+{
+	return platform_driver_register(&tegra_usb_cd_driver);
+}
+
+static void __exit tegra_usb_cd_exit(void)
+{
+	platform_driver_unregister(&tegra_usb_cd_driver);
+}
+
+device_initcall(tegra_usb_cd_init);
+module_exit(tegra_usb_cd_exit);
+
+MODULE_DESCRIPTION("Tegra USB charger detection driver");
+MODULE_AUTHOR("Rakesh Babu Bodla <rbodla@nvidia.com>");
+MODULE_LICENSE("GPL v2");
