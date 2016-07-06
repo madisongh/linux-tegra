12deca18f8e955dda9705c6094c9ac613826b313
Igor Nabirushkin <inabirushkin@nvidia.com>
2016-07-06 12:41:59 +0300

    sched, mm: add tegra profiler callbacks
    Tegra Profiler:
    
    - Add sched-in/sched-out callbacks
    - Add mmap callbacks
    
    Bug 200204805
    
    Change-Id: Ib69d19e1d8a26d8e9e616b10c9d2e79c3bb03ae6
    Signed-off-by: Igor Nabirushkin <inabirushkin@nvidia.com>
    Reviewed-on: http://git-master/r/1176146
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -42,6 +42,7 @@
 #include <linux/memory.h>
 #include <linux/printk.h>
 #include <linux/userfaultfd_k.h>
+#include <linux/tegra_profiler.h>
 
 #include <asm/uaccess.h>
 #include <asm/cacheflush.h>
@@ -1681,6 +1682,7 @@ unsigned long mmap_region(struct file *file, unsigned long addr,
 	file = vma->vm_file;
 out:
 	perf_event_mmap(vma);
+	quadd_event_mmap(vma);
 
 	vm_stat_account(mm, vm_flags, file, len >> PAGE_SHIFT);
 	if (vm_flags & VM_LOCKED) {
@@ -2233,6 +2235,7 @@ int expand_upwards(struct vm_area_struct *vma, unsigned long address)
 				spin_unlock(&mm->page_table_lock);
 
 				perf_event_mmap(vma);
+				quadd_event_mmap(vma);
 			}
 		}
 	}
@@ -2303,6 +2306,7 @@ int expand_downwards(struct vm_area_struct *vma,
 				spin_unlock(&mm->page_table_lock);
 
 				perf_event_mmap(vma);
+				quadd_event_mmap(vma);
 			}
 		}
 	}
@@ -2837,6 +2841,7 @@ static unsigned long do_brk(unsigned long addr, unsigned long len)
 	vma_link(mm, vma, prev, rb_link, rb_parent);
 out:
 	perf_event_mmap(vma);
+	quadd_event_mmap(vma);
 	mm->total_vm += len >> PAGE_SHIFT;
 	if (flags & VM_LOCKED)
 		mm->locked_vm += (len >> PAGE_SHIFT);
@@ -3123,6 +3128,7 @@ static struct vm_area_struct *__install_special_mapping(
 	mm->total_vm += len >> PAGE_SHIFT;
 
 	perf_event_mmap(vma);
+	quadd_event_mmap(vma);
 
 	return vma;
 
