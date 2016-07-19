82d23fec1e56dd44b0ef032f113b49db667144ef
Hans Yang <hansy@nvidia.com>
2016-07-19 16:45:05 +0800

    usb-audio: suppress printing to avoid lockup
    Currently in snd_usb_endpoint_start(), if it fails to
    submit usb urbs, error messages will be printed and
    upper layer will retry soon if it thinks usb is not disconnected.
    This may cause too many print messages of
    "cannot submit urb %d, error %d: %s\n".
    And with a chance, it may cause a softlockup-like behavior
    when another CPU is running in console_unlock() at prink.c.
    
    By suppressing the messages, it could avoid a softlockup
    in other CPUs and also avoid 100% cpu usage.
    
    Bug 200216634
    
    Change-Id: I785fc72dc8145fec88da7fd97056101093d48081
    Signed-off-by: Hans Yang <hansy@nvidia.com>
    Reviewed-on: http://git-master/r/1183337
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: WK Tsai <wtsai@nvidia.com>
    Reviewed-by: ChihMin Cheng <ccheng@nvidia.com>
    Reviewed-by: Ashutosh Jha <ajha@nvidia.com>

--- sound/usb/endpoint.c
+++ sound/usb/endpoint.c
@@ -1,4 +1,6 @@
 /*
+ *   Copyright (c) 2016, NVIDIA CORPORATION.  All rights reserved.
+ *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation; either version 2 of the License, or
@@ -969,7 +971,7 @@ int snd_usb_endpoint_start(struct snd_usb_endpoint *ep, bool can_sleep)
 
 		err = usb_submit_urb(urb, GFP_ATOMIC);
 		if (err < 0) {
-			usb_audio_err(ep->chip,
+			usb_audio_err_ratelimited(ep->chip,
 				"cannot submit urb %d, error %d: %s\n",
 				i, err, usb_error_string(err));
 			goto __error;
