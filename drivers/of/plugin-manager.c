251e41d8b531b062a5e6806bb0b785ebf0e5b63e
Laxman Dewangan <ldewangan@nvidia.com>
2016-08-28 22:05:08 +0530

    Revert "of: plugin-manager: Add support for connection manager"
    This reverts commit c4ba9bae0fe3eb36597e96bb96f2f2392b4ef0ab.
    
    Reverting this change due to changes in design to support multiple
    same modules.
    
    Change-Id: I1225de8fc463b46dce6184965e818447392173d7
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/1208708
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Venkat Reddy Talla <vreddytalla@nvidia.com>

--- drivers/of/plugin-manager.c
+++ drivers/of/plugin-manager.c
@@ -20,7 +20,6 @@
 #include <linux/proc_fs.h>
 #include <linux/of.h>
 #include <linux/of_fdt.h>
-#include "of_private.h"
 
 enum plugin_manager_match_type {
 	PLUGIN_MANAGER_MATCH_EXACT,
@@ -28,12 +27,6 @@ enum plugin_manager_match_type {
 	PLUGIN_MANAGER_MATCH_GE,
 	PLUGIN_MANAGER_MATCH_LT,
 };
-
-static int handle_connection_overrides_node(struct device_node *connector,
-					    struct device_node *oride_parent,
-					    struct device_node *target,
-					    phandle *new_ph, phandle *old_ph,
-					    int num_ph);
 
 static struct property *__of_copy_property(const struct property *prop,
 					   void *new_value, int val_len,
@@ -144,8 +137,8 @@ struct device_node *of_get_child_by_addressed_name(
 	return NULL;
 }
 
-static int do_property_override_from_overlay(struct device_node *target,
-					     struct device_node *overlay)
+static int __init do_property_override_from_overlay(struct device_node *target,
+						    struct device_node *overlay)
 {
 	struct property *prop;
 	struct property *tprop;
@@ -196,63 +189,6 @@ static int do_property_override_from_overlay(struct device_node *target,
 		}
 
 add_prop:
-		tprop = of_find_property(target, new_prop->name, &lenp);
-		if (!tprop) {
-			ret = of_add_property(target, new_prop);
-			if (ret < 0) {
-				pr_err("Prop %s can not be added on node %s\n",
-					new_prop->name, target->full_name);
-				goto cleanup;
-			}
-		} else {
-			ret = of_update_property(target, new_prop);
-			if (ret < 0) {
-				pr_err("Prop %s can not be updated on node %s\n",
-					new_prop->name, target->full_name);
-				goto cleanup;
-			}
-		}
-	}
-
-	return 0;
-
-cleanup:
-	free_property(new_prop);
-	return ret;
-}
-
-static int do_property_override_from_connector(struct device_node *connector,
-					       struct device_node *target,
-					       struct device_node *overlay)
-{
-	struct property *prop;
-	struct property *tprop, *cprop;
-	struct property *new_prop;
-	int lenp = 0;
-	int ret;
-
-	for_each_property_of_node(overlay, prop) {
-		/* Skip those we do not want to proceed */
-		if (!strcmp(prop->name, "name") ||
-			!strcmp(prop->name, "phandle") ||
-			!strcmp(prop->name, "linux,phandle"))
-				continue;
-
-		cprop = of_find_property(connector, prop->value, &lenp);
-		if (!cprop) {
-			pr_err("Prop %s is not found in connector %s\n",
-				prop->name, connector->full_name);
-			continue;
-		}
-
-		new_prop = __of_copy_property(prop, cprop->value, cprop->length,
-					      GFP_KERNEL);
-		if (!new_prop) {
-			pr_err("Prop %s can not be duplicated\n",
-				prop->name);
-			return -EINVAL;
-		}
-
 		tprop = of_find_property(target, new_prop->name, &lenp);
 		if (!tprop) {
 			ret = of_add_property(target, new_prop);
@@ -387,8 +323,8 @@ match_type_done:
 	return false;
 }
 
-static int do_property_overrides(struct device_node *target,
-				 struct device_node *overlay)
+static int __init do_property_overrides(struct device_node *target,
+					struct device_node *overlay)
 {
 	struct device_node *tchild, *ochild;
 	const char *address_name;
@@ -419,40 +355,6 @@ static int do_property_overrides(struct device_node *target,
 	return 0;
 }
 
-static int do_property_copying(struct device_node *connector,
-			       struct device_node *target,
-			       struct device_node *overlay)
-{
-	struct device_node *tchild, *ochild;
-	const char *address_name;
-	int ret;
-
-	ret = do_property_override_from_connector(connector, target, overlay);
-	if (ret < 0) {
-		pr_err("Target %s update with overlay %s failed: %d\n",
-			target->name, overlay->name, ret);
-		return ret;
-	}
-
-	for_each_child_of_node(overlay, ochild) {
-		address_name = strrchr(ochild->full_name, '/');
-		tchild = of_get_child_by_addressed_name(target, address_name);
-		if (!tchild) {
-			pr_err("Overlay node %s not found in target node %s\n",
-				ochild->full_name, target->full_name);
-			continue;
-		}
-		ret = do_property_copying(connector, tchild, ochild);
-		if (ret < 0) {
-			pr_err("Target %s update with overlay %s failed: %d\n",
-				tchild->name, ochild->name, ret);
-			return ret;
-		}
-	}
-
-	return 0;
-}
-
 static int handle_properties_overrides(struct device_node *np,
 				       struct device_node *target)
 {
@@ -479,403 +381,6 @@ static int handle_properties_overrides(struct device_node *np,
 		pr_err("Target %s update with overlay %s failed: %d\n",
 			target->name, overlay->name, ret);
 		return -EINVAL;
-	}
-
-	return 0;
-}
-
-static int handle_properties_copying(struct device_node *connector,
-				     struct device_node *np,
-				     struct device_node *target)
-{
-	struct device_node *overlay;
-	int ret;
-
-	if (!target) {
-		target = of_parse_phandle(np, "target", 0);
-		if (!target) {
-			pr_err("Node %s does not have targer node\n",
-			       np->full_name);
-			return -EINVAL;
-		}
-	}
-
-	overlay = of_get_child_by_name(np, "_overlay_");
-	if (!overlay) {
-		pr_err("Node %s does not have Overlay\n",
-		       np->full_name);
-		return -EINVAL;
-	}
-
-	ret = do_property_copying(connector, target, overlay);
-	if (ret < 0) {
-		 pr_err("Target %s update with overlay %s failed: %d\n",
-			target->full_name, overlay->full_name, ret);
-		return -EINVAL;
-	}
-
-	return 0;
-}
-
-static int do_resolve_client_phandle(struct device_node *np, phandle *new_ph,
-				     phandle *old_ph, int num_phandle)
-{
-	struct device_node *user;
-	struct property *prop, *pp;
-	phandle cur_phandle;
-	const char *s;
-	u32 pval;
-	int index = 0, i;
-	int lenp;
-	int ret;
-
-	of_property_for_each_string(np, "node-user-prop-names", prop, s) {
-		ret = of_property_read_u32_index(np, "node-user",
-						 index, &pval);
-		if (ret < 0) {
-			pr_err("Failed to read node-user from node %s\n",
-				np->full_name);
-			return ret;
-		}
-
-		user = of_find_node_by_phandle(pval);
-		if (!user) {
-			pr_err("Failed to get user listed in node %s\n",
-				np->full_name);
-			return -EINVAL;
-		}
-
-		pp = of_find_property(user, s, &lenp);
-		if (!pp) {
-			pr_err("Failed to get property from user node %s\n",
-				user->full_name);
-			return -EINVAL;
-		}
-
-		cur_phandle = be32_to_cpup(pp->value);
-		for (i = 0; i < num_phandle; ++i) {
-			if (old_ph[i] == cur_phandle)
-				break;
-		}
-
-		if (i == num_phandle) {
-			pr_err("User:%d %s does not linked with target node %s\n",
-				index, user->full_name, np->full_name);
-			return -EINVAL;
-		}
-
-		*(uint32_t *)pp->value = cpu_to_be32(new_ph[i]);
-
-		index++;
-	}
-
-	return 0;
-}
-
-struct device_node *duplicate_single_node(struct device_node *np,
-				          const char *path,
-					  const char *new_name)
-{
-	struct device_node *dup;
-	struct property *pp, *new_pp;
-	int ret;
-	const char *add_name;
-
-	dup = kzalloc(sizeof(*dup), GFP_KERNEL);
-	if (!dup)
-		return NULL;
-
-	if (new_name) {
-		add_name = new_name;
-	} else {
-		add_name = strrchr(np->full_name, '/');
-		add_name++;
-	}
-	dup->full_name = kasprintf(GFP_KERNEL, "%s/%s", path, add_name);
-
-	of_node_init(dup);
-
-	for_each_property_of_node(np, pp) {
-		if (!strcmp(pp->name, "name"))
-			new_pp = __of_copy_property(pp, (void *)add_name,
-						    strlen(add_name),
-						    GFP_KERNEL);
-		else
-			new_pp = __of_copy_property(pp, NULL, 0, GFP_KERNEL);
-		if (!new_pp)
-			return NULL;
-
-		ret = of_add_property(dup, new_pp);
-		if (ret < 0) {
-			pr_err("Prop %s can not be added on node %s\n",
-				new_pp->name, dup->full_name);
-			free_property(new_pp);
-			return NULL;
-		}
-	}
-
-	dup->name = __of_get_property(dup, "name", NULL) ? : "<NULL>";
-	dup->type = __of_get_property(dup, "device_type", NULL) ? : "<NULL>";
-
-	return dup;
-}
-
-struct device_node *get_copy_of_node(struct device_node *np,
-				     const char *path, const char *new_name)
-{
-	struct device_node *dup;
-	struct device_node *child, *child_dup;
-	struct device_node *prev_child = NULL;
-
-	dup = duplicate_single_node(np, path, new_name);
-	if (!dup)
-		return NULL;
-
-	for_each_child_of_node(np, child) {
-		child_dup = get_copy_of_node(child, dup->full_name, NULL);
-		if (!child_dup)
-			return NULL;
-		child_dup->parent = dup;
-		child_dup->sibling = NULL;
-		if (!prev_child)
-			dup->child = child_dup;
-		else
-			prev_child->sibling = child_dup;
-		prev_child = child_dup;
-	}
-
-	return dup;
-}
-
-static int of_get_next_phandle(void)
-{
-	static phandle curr_handle = 0;
-	static bool first_time = true;
-	struct device_node *np;
-	phandle next_handle;
-	unsigned long flags;
-
-	raw_spin_lock_irqsave(&devtree_lock, flags);
-
-	if (first_time) {
-		for_each_of_allnodes(np) {
-			if (np->phandle > curr_handle)
-				curr_handle = np->phandle;
-		}
-		first_time = false;
-	}
-
-	next_handle = curr_handle++;
-	raw_spin_unlock_irqrestore(&devtree_lock, flags);
-
-	return next_handle;
-}
-
-static int adjust_phandle_reference(struct device_node *np, phandle *new_phlist,
-				    phandle *old_phlist, int curr_index)
-{
-	struct device_node *child;
-	struct property *ph_prop, *lph_prop;
-	int lenp;
-
-	ph_prop =  of_find_property(np, "phandle", &lenp);
-	lph_prop = of_find_property(np, "linux,phandle", &lenp);
-	if (ph_prop || lph_prop) {
-		np->phandle = of_get_next_phandle();
-
-		new_phlist[curr_index] = np->phandle;
-		old_phlist[curr_index++] = (ph_prop) ? be32_to_cpup(ph_prop->value) :
-							be32_to_cpup(lph_prop->value);
-		if (ph_prop->length >= 4)
-			*(uint32_t *)ph_prop->value = cpu_to_be32(np->phandle);
-		if (lph_prop->length >= 4)
-			*(uint32_t *)lph_prop->value = cpu_to_be32(np->phandle);
-
-	}
-
-	for_each_child_of_node(np, child)
-		curr_index = adjust_phandle_reference(child, new_phlist,
-						      old_phlist, curr_index);
-
-	return curr_index;
-}
-
-static int _attach_node_and_children(struct device_node *np)
-{
-	struct device_node *child;
-
-	__of_attach_node_sysfs(np);
-	for_each_child_of_node(np, child)
-		_attach_node_and_children(child);
-
-	return 0;
-}
-
-static int handle_connection_node_copy(struct device_node *connector,
-				       struct device_node *oride)
-{
-	struct device_node *dup, *src;
-	struct device_node *bus;
-	struct device_node *last_sibling;
-	phandle num_phandle;
-	const char *node_name = NULL;
-	phandle new_ph_list[100], old_ph_list[100];
-	unsigned long flags;
-
-	bus = of_parse_phandle(connector, "bus", 0);
-	if (!bus) {
-		pr_err("Node %s does not have bus property\n", connector->full_name);
-		return -EINVAL;
-	}
-
-	src = of_parse_phandle(oride, "source", 0);
-	if (!src) {
-		pr_err("Node %s does not have source node\n", oride->full_name);
-		return -EINVAL;
-	}
-
-	if (of_property_read_string(oride, "node-name", &node_name))
-		node_name = NULL;
-
-	dup = get_copy_of_node(src, bus->full_name, node_name);
-	if (!dup) {
-		pr_err("Failed to create duplicate of node %s\n", src->full_name);
-		return -EINVAL;
-	}
-
-	num_phandle = adjust_phandle_reference(dup, new_ph_list,
-					       old_ph_list, 0);
-
-	mutex_lock(&of_mutex);
-	raw_spin_lock_irqsave(&devtree_lock, flags);
-
-	/* Add target node as the lasy child of bus node */
-	if (!bus->child) {
-		bus->child = dup;
-	} else {
-		last_sibling = bus->child;
-		while(!last_sibling->sibling)
-			last_sibling = last_sibling->sibling;
-		last_sibling->sibling = dup;
-	}
-	dup->sibling = NULL;
-	dup->parent = bus;
-
-	raw_spin_unlock_irqrestore(&devtree_lock, flags);
-	_attach_node_and_children(dup);
-
-	mutex_unlock(&of_mutex);
-
-	return handle_connection_overrides_node(connector, oride, dup,
-						new_ph_list, old_ph_list,
-						num_phandle);
-}
-
-static int handle_connection_overrides_node(struct device_node *connector,
-					    struct device_node *oride_parent,
-					    struct device_node *target,
-					    phandle *new_ph, phandle *old_ph,
-					    int num_ph)
-{
-	struct device_node *child;
-	int ret;
-
-	for_each_available_child_of_node(oride_parent, child) {
-		if (of_property_read_bool(child, "properties-override")) {
-			ret = handle_properties_overrides(child, target);
-		} else if (of_property_read_bool(child, "properties-copying")) {
-			ret = handle_properties_copying(connector, child, target);
-		} else if (of_property_read_bool(child, "resolve-client-phandle")) {
-			ret = do_resolve_client_phandle(child, new_ph,
-							old_ph, num_ph);
-		} else if (of_property_read_bool(child, "node-copying")) {
-			ret = handle_connection_node_copy(connector, child);
-		} else {
-			ret = handle_properties_overrides(child, target);
-		}
-
-		if (ret < 0) {
-			pr_err("Failed to apply overrides at node %s: %d\n",
-				child->full_name, ret);
-			return ret;
-		}
-	}
-
-	return 0;
-}
-
-static int lookup_for_connectors(struct device_node *connector_list,
-				 struct device_node *ids,
-				 struct device_node *connection,
-				 const char *module_name)
-{
-	struct device_node *child;
-	struct device_node *connector;
-	bool found;
-	int ret = 0;
-
-	found = plugin_manager_match_id(ids, module_name);
-	if (!found)
-		goto look_for_child;
-
-	pr_info("node %s match with board %s\n", ids->full_name, module_name);
-
-	connector = of_get_child_by_name(connector_list, ids->name);
-	if (!connector) {
-		pr_info("Connector node %s is not found\n", ids->name);
-		return -EINVAL;
-	}
-
-	ret = handle_connection_overrides_node(connector, connection, NULL,
-					       NULL, NULL, 0);
-	if (ret < 0) {
-		pr_info("Failed to handle connection %s with connector %s: %d\n",
-			connection->full_name, connector->name, ret);
-		return ret;
-	}
-
-look_for_child:
-	for_each_available_child_of_node(ids, child) {
-		ret = lookup_for_connectors(connector_list, child,
-					    connection, module_name);
-		if (ret < 0)
-			return ret;
-	}
-
-	return ret;
-}
-
-static int __init connection_manager(struct device_node *np_frag)
-{
-	struct device_node *ids;
-	struct device_node *connector_list;
-	struct device_node *connection;
-	struct property *prop;
-	const char *bname;
-	int ret;
-
-	connector_list = of_parse_phandle(np_frag, "connectors", 0);
-	if (!connector_list) {
-		pr_err("Failed to get the conntors node\n");
-		return -EINVAL;
-	}
-
-	ids = of_find_node_by_path("/chosen/plugin-manager/ids");
-	if (!ids) {
-		pr_err("chosen/plugin-manager does'nt have ids\n");
-		return -EINVAL;
-	}
-
-	for_each_available_child_of_node(np_frag, connection) {
-		of_property_for_each_string(connection, "ids", prop, bname) {
-			ret = lookup_for_connectors(connector_list, ids,
-						    connection, bname);
-			if (ret < 0) {
-				pr_err("Failed to handle connection %s: %d\n",
-					connection->full_name, ret);
-				return ret;
-			}
-		}
 	}
 
 	return 0;
@@ -1062,14 +567,7 @@ static int __init plugin_manager_init(void)
 	}
 
 	for_each_available_child_of_node(pm_node, child) {
-		bool connection;
-
-		connection = of_property_read_bool(child, "connection-type");
-		if (connection)
-			ret = connection_manager(child);
-		else
-			ret = plugin_manager(child);
-
+		ret = plugin_manager(child);
 		if (ret < 0)
 			pr_err("Error in parsing node %s: %d\n",
 				child->full_name, ret);
