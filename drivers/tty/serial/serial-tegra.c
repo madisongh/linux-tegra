d321a1b30bab98532cf7399baeec8d2b8c6d3816
Shardar Shariff Md <smohammed@nvidia.com>
2016-10-04 20:40:56 +0530

    serial: tegra: skip reset during probe if early console instance
    Skip the reset of controller during probe if the controller is been used
    for early printk console
    
    Bug 200239162
    
    Change-Id: Id3da80620cd3a7e982fab6e4511d4f5fa667f152
    Signed-off-by: Shardar Shariff Md <smohammed@nvidia.com>
    Reviewed-on: http://git-master/r/1231440
    (cherry picked from commit 2564cc8eef6626daf9c4e13715d8bb4c48807151)
    Reviewed-on: http://git-master/r/1241072
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- drivers/tty/serial/serial-tegra.c
+++ drivers/tty/serial/serial-tegra.c
@@ -161,6 +161,7 @@ struct tegra_uart_port {
 	int					required_rate;
 	int					configured_rate;
 	struct dentry				*debugfs;
+	bool					early_printk_console_instance;
 };
 
 static void tegra_uart_start_next_tx(struct tegra_uart_port *tup);
@@ -1620,6 +1621,9 @@ static int tegra_uart_parse_dt(struct platform_device *pdev,
 	if (tup->enable_rx_buffer_throttle)
 		dev_info(&pdev->dev, "Rx buffer throttling enabled\n");
 
+	tup->early_printk_console_instance = of_property_read_bool(np,
+			"early-print-console-channel");
+
 	n_entries = of_property_count_u32_elems(np, "nvidia,adjust-baud-rates");
 	if (n_entries > 0) {
 		tup->n_adjustable_baud_rates = n_entries/3;
@@ -1789,10 +1793,11 @@ static int tegra_uart_probe(struct platform_device *pdev)
 		return PTR_ERR(tup->rst);
 	}
 
-	/* Reset the UART controller to clear all previous status.*/
-	reset_control_assert(tup->rst);
-	udelay(10);
-	reset_control_deassert(tup->rst);
+	if (!tup->early_printk_console_instance) {
+		reset_control_assert(tup->rst);
+		udelay(10);
+		reset_control_deassert(tup->rst);
+	}
 
 	parent_clk = devm_clk_get(&pdev->dev, "parent");
 	if (IS_ERR(parent_clk))
