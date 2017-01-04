9f0d09b65bbe734d02bd444947a8b6642d7feb41
Shmuel Ungerfeld <sungerfeld@nvidia.com>
2017-01-04 11:38:43 -0800

    tegra: hv: add driver for user access to pct/log
    Add driver for user level access to pct & log via mmap()
    Removed duplicate file syscalls.h from driver folder.
    Remove include path into driver folder for the trusty_virtio.
    log & pct file nodes set with permission 0444 (all can read)
    
    Signed-off-by: Shmuel Ungerfeld <sungerfeld@nvidia.com>
    
    Jira VFND-2882
    
    Change-Id: I2be5ad22bb98afcea1f7aadb55f71129a2f63e33
    Reviewed-on: http://git-master/r/1280882
    Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Vladislav Buzov <vbuzov@nvidia.com>
    Tested-by: Vladislav Buzov <vbuzov@nvidia.com>

--- drivers/trusty/trusty-virtio.c
+++ drivers/trusty/trusty-virtio.c
@@ -1,4 +1,20 @@
 /*
+ * Copyright (c) 2017, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  * Trusty Virtio driver
  *
  * Copyright (C) 2015 Google, Inc.
@@ -32,7 +48,7 @@
 
 #include <linux/atomic.h>
 
-#include "syscalls.h"
+#include <soc/tegra/virt/syscalls.h>
 
 #include "trusty-virtio-work.h"
 
@@ -247,7 +263,7 @@ static u64 trusty_virtio_get_features(struct virtio_device *vdev)
 static int trusty_virtio_finalize_features(struct virtio_device *vdev)
 {
 	struct trusty_vdev *tvdev = vdev_to_tvdev(vdev);
-	
+
 	/* Make sure we don't have any features > 32 bits! */
 	BUG_ON((u32)vdev->features != vdev->features);
 
