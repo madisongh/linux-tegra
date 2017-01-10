48a238f8230b33fe52e449359c81f3a994cc9e18
Shardar Shariff Md <smohammed@nvidia.com>
2017-01-10 19:31:39 +0530

    tegra: fuse: print the chip ID/subrevision for debug
    Print the chip ID/subrevision for debug
    
    This support is added as part of kernel unification of fuse driver.
    
    Bug 200260692
    
    Change-Id: I5d41d212ab926eaf36c5c8f803f61360e682b23f
    Signed-off-by: Shardar Shariff Md <smohammed@nvidia.com>
    Reviewed-on: http://git-master/r/1282863
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- drivers/soc/tegra/fuse/tegra-apbmisc.c
+++ drivers/soc/tegra/fuse/tegra-apbmisc.c
@@ -157,7 +157,7 @@ static struct chip_revision tegra_chip_revisions[] = {
 
 void tegra_init_revision(void)
 {
-	u32 id, chipid, major, minor, subrev;
+	u32 id, chipid, major, minor, subrev, reg;
 	enum tegra_revision revision = TEGRA_REVISION_UNKNOWN;
 	char sub_type = 0;
 	int i;
@@ -166,6 +166,7 @@ void tegra_init_revision(void)
 	chipid = tegra_hidrev_get_chipid(id);
 	major = tegra_hidrev_get_majorrev(id);
 	minor = tegra_hidrev_get_minorrev(id);
+	pr_info("tegra-id: chipid=%x.\n", id);
 
 	/* For pre-silicon the major is 0, for silicon it is >= 1 */
 	if (major == 0) {
@@ -194,6 +195,7 @@ void tegra_init_revision(void)
 		sub_type = 'r';
 		break;
 	}
+	pr_info("tegra-id: opt_subrevision=%x.\n", subrev);
 
 	for (i = 0; i < ARRAY_SIZE(tegra_chip_revisions); i++) {
 		if ((chipid != tegra_chip_revisions[i].chipid) ||
