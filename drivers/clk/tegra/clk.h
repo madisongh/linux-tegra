5f95cfb62bc06613872e61916a7e2c74dce6d86e
Peter De Schrijver <pdeschrijver@nvidia.com>
2017-01-24 14:15:59 +0200

    clk: tegra: add pto_cycles debugfs file
    This file allows selecting the number of 32kHz cycles of the PTO counter when
    measuring a clock. This is used to validate spread spectrum clocking. Allowed
    values are between 1 and 16. Any other value will cause the default value of 16
    to be used.
    
    Bug 200255593
    
    Change-Id: I2be3ed15fead5931fd226ac2a3cb4b0fd0652dab
    Signed-off-by: Peter De Schrijver <pdeschrijver@nvidia.com>
    Reviewed-on: http://git-master/r/1293171
    Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Timo Alho <talho@nvidia.com>

--- drivers/clk/tegra/clk.h
+++ drivers/clk/tegra/clk.h
@@ -966,6 +966,7 @@ struct tegra_pto_table {
 	u32 presel_value;
 	u32 presel_reg;
 	u32 presel_mask;
+	u8 cycle_count;
 };
 void tegra_register_ptos(struct tegra_pto_table *ptodefs, int num_pto_defs);
 #endif /* TEGRA_CLK_H */
