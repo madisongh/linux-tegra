5ec0905c24a4fc432ebf03f4f92f44af7f5e6937
Asha T <atalambedu@nvidia.com>
2017-02-06 11:30:36 +0530

    ASoc: Avoids resource leaks
    Coverity ID:38365
    
    Bug 200192147
    
    Change-Id: I943d9f50d97b3fa85bb4baeae4bfcdd6753819c5
    Signed-off-by: Asha T <atalambedu@nvidia.com>
    Reviewed-on: http://git-master/r/1299408
    Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Ravindra Lokhande <rlokhande@nvidia.com>

--- sound/soc/soc-compress.c
+++ sound/soc/soc-compress.c
@@ -71,7 +71,7 @@ static int soc_compr_open_fe(struct snd_compr_stream *cstream)
 	struct snd_pcm_substream *fe_substream = fe->pcm->streams[0].substream;
 	struct snd_soc_platform *platform = fe->platform;
 	struct snd_soc_dpcm *dpcm;
-	struct snd_soc_dapm_widget_list *list;
+	struct snd_soc_dapm_widget_list *list = NULL;
 	int stream;
 	int ret = 0;
 
@@ -137,6 +137,8 @@ static int soc_compr_open_fe(struct snd_compr_stream *cstream)
 	return 0;
 
 fe_err:
+	if (list)
+		dpcm_path_put(&list);
 	if (fe->dai_link->compr_ops && fe->dai_link->compr_ops->shutdown)
 		fe->dai_link->compr_ops->shutdown(cstream);
 machine_err:
