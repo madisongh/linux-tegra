d8356f6755a9eae5598adc06bc698e09aa30963d
Ajay Nandakumar <anandakumarm@nvidia.com>
2017-03-30 13:05:30 +0530

    soc: tegra: export APIs which check soc versions
    Export APIs soc_is_tegra210_n_before and soc_is_tegra186_n_later so
    that kernel tests(which are modules) can used them to check the soc
    version.
    
    Bug 200291720
    
    Change-Id: I8fcd7b5b76fbbdd44587100b642cd1436454afde
    Signed-off-by: Ajay Nandakumar <anandakumarm@nvidia.com>
    Reviewed-on: http://git-master/r/1331260
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    (cherry picked from commit 0e10aca6f7efd7793ee1bd9c5332219db7b4515e)

--- drivers/soc/tegra/common.c
+++ drivers/soc/tegra/common.c
@@ -73,6 +73,7 @@ bool soc_is_tegra210_n_before(void)
 
 	return of_match_node(tegra210_le_machine_match, root) != NULL;
 }
+EXPORT_SYMBOL(soc_is_tegra210_n_before);
 
 bool soc_is_tegra186_n_later(void)
 {
@@ -84,6 +85,7 @@ bool soc_is_tegra186_n_later(void)
 
 	return of_match_node(tegra186_ge_machine_match, root) != NULL;
 }
+EXPORT_SYMBOL(soc_is_tegra186_n_later);
 
 static int __init tegra_bootloader_fb_arg(char *options)
 {
