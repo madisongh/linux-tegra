a52d8cfb985a61524a47a90518c0c59e97e352fe
Alex Frid <afrid@nvidia.com>
2017-04-14 15:13:23 -0700

    clk: tegra: Avoid prepare lock in PLL resume
    Modified PLL resume operations to avoid taking prepare lock in PLL
    resume path, so it can be executed in atomic environment.
    
    Bug 200269979
    
    Change-Id: I7d30f03f9140d9ef9efd72589fd06567678a1171
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Reviewed-on: http://git-master/r/1463264
    Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Sivaram Nair <sivaramn@nvidia.com>
    Reviewed-by: Jon Mayo <jmayo@nvidia.com>
    (cherry picked from commit 27ca01ec88c69d20b79bceb0fd0d73adccbc4bff)

--- drivers/clk/tegra/clk-pll.c
+++ drivers/clk/tegra/clk-pll.c
@@ -2518,9 +2518,9 @@ static void clk_pll_iddq_disable(struct clk_hw *hw)
 
 void tegra_clk_pll_resume(struct clk *c, unsigned long rate)
 {
-	struct clk *parent = clk_get_parent(c);
 	struct clk_hw *hw = __clk_get_hw(c);
 	struct tegra_clk_pll *pll = to_clk_pll(hw);
+	struct clk_hw *parent = clk_hw_get_parent(hw);
 	unsigned long parent_rate;
 
 	if (clk_pll_is_enabled(hw))
@@ -2530,7 +2530,7 @@ void tegra_clk_pll_resume(struct clk *c, unsigned long rate)
 		WARN_ON(1);
 		return;
 	}
-	parent_rate = clk_get_rate(parent);
+	parent_rate = clk_hw_get_rate(parent);
 
 	if (pll->params->set_defaults)
 		pll->params->set_defaults(pll);
