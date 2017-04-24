b74b6878d256e50c614ca0f4b234d30723f9fb38
Wenjia Zhou <wenjiaz@nvidia.com>
2017-04-23 18:36:45 -0700

    drivers: Fix register read for sensors
    Fix data type required for the core register
    read api. Passing 8 bit pointer corrupts the stack
    and pushes kernel to panic.
    
    Bug 1856401
    
    Change-Id: I5b3039e5306e8579c1d9efccfd1efb5de912bfe0
    Signed-off-by: Bhanu Murthy V <bmurthyv@nvidia.com>
    Signed-off-by: Wenjia Zhou <wenjiaz@nvidia.com>
    Reviewed-on: http://git-master/r/1467786
    (cherry picked from commit 8e6e47d72fd0cd464678566c2be2e6891583f20d)
    Reviewed-on: http://git-master/r/1468154
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/media/i2c/imx274.c
+++ drivers/media/i2c/imx274.c
@@ -187,7 +187,13 @@ static inline int imx274_read_reg(struct camera_common_data *s_data,
 				u16 addr, u8 *val)
 {
 	struct imx274 *priv = (struct imx274 *)s_data->priv;
-	return regmap_read(priv->regmap, addr, (unsigned int *) val);
+	int err = 0;
+	u32 reg_val = 0;
+
+	err = regmap_read(priv->regmap, addr, &reg_val);
+	*val = reg_val & 0xFF;
+
+	return err;
 }
 
 static int imx274_write_reg(struct camera_common_data *s_data, u16 addr, u8 val)
