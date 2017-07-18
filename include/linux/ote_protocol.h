7ea942b640cda408c51ff6a0867617fe9a8c6d16
Mihir Joshi <mihirj@nvidia.com>
2017-07-18 13:51:10 -0700

    Merge remote-tracking branch 'origin/dev/dev-tlk-k44-bringup' into HEAD
    This merge commit tracks TLK driver commits from kernel-shield-3.10
    Start commit: b30a02b2b7468e0f1f031740f0ec9fc097a019b0
    End commit: 9a71f2fb564ef4cdfbcdb24c6bc7115a5dbe4489
    
    Bug 1961076
    
    Change-Id: I4e70f2b16e4571b0cfa30b5a3a5b6b0b55a453f1
    (cherry picked from commit 360fd6f21aacbdd803c12481bc0b2649c36b78e1)
    Reviewed-on: https://git-master.nvidia.com/r/1528197
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Mihir Joshi <mihirj@nvidia.com>
    Tested-by: Mihir Joshi <mihirj@nvidia.com>
    Reviewed-by: Hayden Du <haydend@nvidia.com>

--- include/linux/ote_protocol.h
+++ include/linux/ote_protocol.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2015 NVIDIA Corporation. All rights reserved.
+ * Copyright (c) 2013-2017 NVIDIA Corporation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,6 +21,11 @@
 
 int tegra_set_vpr_params(void *vpr_base, size_t vpr_size);
 
+void te_restore_keyslots(void);
+int  te_vrr_set_buf(phys_addr_t addr);
+void te_vrr_sec(void);
+/* FIXME: Merge with tegra_set_vpr_params */
+int te_set_vpr_params(void *vpr_base, size_t vpr_size);
 void te_restore_keyslots(void);
 int  te_vrr_set_buf(phys_addr_t addr);
 void te_vrr_sec(void);
