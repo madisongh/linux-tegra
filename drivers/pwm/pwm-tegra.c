OE4T/linux-tegra-4.9

pwm: tegra: Add support to select slower parent
If requested minimum frequency from the platform is not possible
from the higher rate parent then change the parent to slow parent
if slow-parent clock information is provided from DT.

This will help to select the lower frequency of PWM signal.

bug 1927065

Change-Id: I4eada8c51d283b308a6fe58e21596ed2f54c4c58
Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
Reviewed-on: http://git-master/r/1493947
Tested-by: R Raj Kumar <rrajk@nvidia.com>
GVS: Gerrit_Virtual_Submit
Reviewed-by: Automatic_Commit_Validation_User
Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>

drivers/pwm/pwm-tegra.c
@@ -189,6 +189,7 @@ static int tegra_pwm_probe(struct platform_device *pdev)
 	struct resource *r;
 	bool no_clk_sleeping_in_ops;
 	struct clk *parent_clk;
+	struct clk *parent_slow;
 	u32 pval;
 	int ret;
 
@@ -254,7 +255,7 @@ static int tegra_pwm_probe(struct platform_device *pdev)
 	pwm->clk_rate = clk_get_rate(pwm->clk);
 
 	/* Limit the maximum clock rate */
-	if (pwm->soc->max_clk_limit &&
+	if (pwm->max_clk_limit &&
 	    (pwm->clk_rate > pwm->max_clk_limit)) {
 		ret = clk_set_rate(pwm->clk, pwm->max_clk_limit);
 		if (ret < 0) {
@@ -265,6 +266,34 @@ static int tegra_pwm_probe(struct platform_device *pdev)
 		pwm->clk_rate = clk_get_rate(pwm->clk);
 	}
 
+	if (pwm->clk_rate <= pwm->max_clk_limit)
+		goto parent_done;
+	/*
+	 * If clk_rate is still higher than the max_clk_limit then
+	 * switch to slow parent if exist
+	 */
+	parent_slow = devm_clk_get(&pdev->dev, "slow-parent");
+	if (IS_ERR(parent_slow)) {
+		dev_warn(&pdev->dev, "Source clock %lu is higher than required %lu\n",
+			 pwm->clk_rate, pwm->max_clk_limit);
+		goto parent_done;
+	}
+
+	ret = clk_set_parent(pwm->clk, parent_slow);
+	if (ret < 0) {
+		dev_err(&pdev->dev, "Failed to set slow-parent: %d\n", ret);
+		return ret;
+	}
+
+	/* Set clock to maximum clock limit */
+	ret = clk_set_rate(pwm->clk, pwm->max_clk_limit);
+	if (ret < 0) {
+		dev_err(&pdev->dev, "Failed to set max clk rate: %d\n", ret);
+		return ret;
+	}
+	pwm->clk_rate = clk_get_rate(pwm->clk);
+
+parent_done:
 	if (no_clk_sleeping_in_ops) {
 		ret = clk_prepare(pwm->clk);
 		if (ret) {

madisongh/linux-tegra

Reviewed-on: http://git-master/r/1484061
Reviewed-by: R Raj Kumar <rrajk@nvidia.com>
(cherry picked from commit aaca5fe19e655885aac71e09589b1d67189c7074)

drivers/pwm/pwm-tegra.c
@@ -268,6 +268,7 @@ static int tegra_pwm_probe(struct platform_device *pdev)
 	struct resource *r;
 	bool no_clk_sleeping_in_ops;
 	struct clk *parent_clk;
+	struct clk *parent_slow;
 	u32 pval;
 	int ret;
 
@@ -344,7 +345,7 @@ static int tegra_pwm_probe(struct platform_device *pdev)
 	pwm->parent_rate = clk_get_rate(clk_get_parent(pwm->clk));
 
 	/* Limit the maximum clock rate */
-	if (pwm->soc->max_clk_limit &&
+	if (pwm->max_clk_limit &&
 	    (pwm->src_rate > pwm->max_clk_limit)) {
 		ret = clk_set_rate(pwm->clk, pwm->max_clk_limit);
 		if (ret < 0) {
@@ -355,6 +356,35 @@ static int tegra_pwm_probe(struct platform_device *pdev)
 		pwm->src_rate = clk_get_rate(pwm->clk);
 	}
 
+	if (pwm->src_rate <= pwm->max_clk_limit)
+		goto parent_done;
+	/*
+	 * If src_rate is still higher than the max_clk_limit then
+	 * switch to slow parent if exist
+	 */
+	parent_slow = devm_clk_get(&pdev->dev, "slow-parent");
+	if (IS_ERR(parent_slow)) {
+		dev_warn(&pdev->dev, "Source clock %lu is higher than required %lu\n",
+			 pwm->src_rate, pwm->max_clk_limit);
+		goto parent_done;
+	}
+
+	ret = clk_set_parent(pwm->clk, parent_slow);
+	if (ret < 0) {
+		dev_err(&pdev->dev, "Failed to set slow-parent: %d\n", ret);
+		return ret;
+	}
+
+	/* Set clock to maximum clock limit */
+	ret = clk_set_rate(pwm->clk, pwm->max_clk_limit);
+	if (ret < 0) {
+		dev_err(&pdev->dev, "Failed to set max clk rate: %d\n", ret);
+		return ret;
+	}
+	pwm->src_rate = clk_get_rate(pwm->clk);
+	pwm->parent_rate = clk_get_rate(clk_get_parent(pwm->clk));
+
+parent_done:
 	if (no_clk_sleeping_in_ops) {
 		ret = clk_prepare(pwm->clk);
 		if (ret) {