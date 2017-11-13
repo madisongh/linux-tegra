OE4T/linux-tegra-4.9

pwm: tegra: Add support to lower pwm frequency
The PWM output signal depends on the clock source frequency
and PWM divider for period. There is possibility to have lower
PWM frequency which is not possible with higher PWM clock source
and on this case, it is required to have more fine divider of clock
source to get desired lower side frequency.

Add DT property "pwm-minimum-frequency-hz" to findout the platform
specific requirement and adjust the clock source frequency to
support this lower side frequency.

bug 1927065

Change-Id: Icb642d6886b39348b9b22310b9f0446be8087ae5
Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
Reviewed-on: http://git-master/r/1493946
Tested-by: R Raj Kumar <rrajk@nvidia.com>
GVS: Gerrit_Virtual_Submit
Reviewed-by: Automatic_Commit_Validation_User
Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>

drivers/pwm/pwm-tegra.c
@@ -58,6 +58,7 @@ struct tegra_pwm_chip {
 	void __iomem *regs;
 
 	const struct tegra_pwm_soc *soc;
+	unsigned long		max_clk_limit;
 	int (*clk_enable)(struct clk *clk);
 	void (*clk_disable)(struct clk *clk);
 };
@@ -188,6 +189,7 @@ static int tegra_pwm_probe(struct platform_device *pdev)
 	struct resource *r;
 	bool no_clk_sleeping_in_ops;
 	struct clk *parent_clk;
+	u32 pval;
 	int ret;
 
 	pwm = devm_kzalloc(&pdev->dev, sizeof(*pwm), GFP_KERNEL);
@@ -208,6 +210,13 @@ static int tegra_pwm_probe(struct platform_device *pdev)
 			"nvidia,no-clk-sleeping-in-ops");
 	dev_info(&pdev->dev, "PWM clk can%s sleep in ops\n",
 			no_clk_sleeping_in_ops ? "not" : "");
+
+	ret = of_property_read_u32(pdev->dev.of_node,
+				   "pwm-minimum-frequency-hz", &pval);
+	if (!ret)
+		pwm->max_clk_limit = pval * 256 * (1 << PWM_SCALE_WIDTH);
+	else
+		pwm->max_clk_limit = pwm->soc->max_clk_limit;
 
 	pwm->clk = devm_clk_get(&pdev->dev, "pwm");
 	if (IS_ERR(pwm->clk))
@@ -234,7 +243,7 @@ static int tegra_pwm_probe(struct platform_device *pdev)
 		}
 
 		/* Set clock to maximum clock limit */
-		ret = clk_set_rate(pwm->clk, pwm->soc->max_clk_limit);
+		ret = clk_set_rate(pwm->clk, pwm->max_clk_limit);
 		if (ret < 0) {
 			dev_err(dev, "Failed to set max clk rate: %d\n", ret);
 			return ret;
@@ -246,8 +255,8 @@ static int tegra_pwm_probe(struct platform_device *pdev)
 
 	/* Limit the maximum clock rate */
 	if (pwm->soc->max_clk_limit &&
-	    (pwm->clk_rate > pwm->soc->max_clk_limit)) {
-		ret = clk_set_rate(pwm->clk, pwm->soc->max_clk_limit);
+	    (pwm->clk_rate > pwm->max_clk_limit)) {
+		ret = clk_set_rate(pwm->clk, pwm->max_clk_limit);
 		if (ret < 0) {
 			dev_err(&pdev->dev, "Failed to set max clk rate: %d\n",
 				ret);

madisongh/linux-tegra

Reviewed-on: http://git-master/r/1483242
(cherry picked from commit 6e8b704ae3fbf7b9c0fbc291ebadd0b4e71c1c58)

drivers/pwm/pwm-tegra.c
@@ -61,6 +61,7 @@ struct tegra_pwm_chip {
 	int			num_user;
 	unsigned long		src_rate;
 	unsigned long		parent_rate;
+	unsigned long		max_clk_limit;
 	struct pinctrl		*pinctrl;
 	struct pinctrl_state	*suspend_state;
 	struct pinctrl_state	*resume_state;
@@ -121,11 +122,11 @@ static long tegra_get_optimal_rate(struct tegra_pwm_chip *pc,
 	 * Make sure that derived frequency should not exceed max clock
 	 * frequency limit.
 	 */
-	if (pc->soc->max_clk_limit && (in_rate > pc->soc->max_clk_limit)) {
-		ret = clk_set_rate(pc->clk, pc->soc->max_clk_limit);
+	if (pc->max_clk_limit && (in_rate > pc->max_clk_limit)) {
+		ret = clk_set_rate(pc->clk, pc->max_clk_limit);
 		if (ret < 0)
 			dev_warn(pc->dev, "Failed to set clock rate %lu: %d\n",
-				 pc->soc->max_clk_limit, ret);
+				 pc->max_clk_limit, ret);
 
 		pc->src_rate = clk_get_rate(pc->clk);
 		return -EAGAIN;
@@ -267,6 +268,7 @@ static int tegra_pwm_probe(struct platform_device *pdev)
 	struct resource *r;
 	bool no_clk_sleeping_in_ops;
 	struct clk *parent_clk;
+	u32 pval;
 	int ret;
 
 	pwm = devm_kzalloc(&pdev->dev, sizeof(*pwm), GFP_KERNEL);
@@ -296,6 +298,12 @@ static int tegra_pwm_probe(struct platform_device *pdev)
 	dev_info(&pdev->dev, "PWM clk can%s sleep in ops\n",
 		 no_clk_sleeping_in_ops ? "not" : "");
 
+	ret = of_property_read_u32(pdev->dev.of_node,
+				   "pwm-minimum-frequency-hz", &pval);
+	if (!ret)
+		pwm->max_clk_limit = pval * 256 * (1 << PWM_SCALE_WIDTH);
+	else
+		pwm->max_clk_limit = pwm->soc->max_clk_limit;
 
 	pwm->clk = devm_clk_get(&pdev->dev, "pwm");
 	if (IS_ERR(pwm->clk)) {
@@ -324,7 +332,7 @@ static int tegra_pwm_probe(struct platform_device *pdev)
 		}
 
 		/* Set clock to maximum clock limit */
-		ret = clk_set_rate(pwm->clk, pwm->soc->max_clk_limit);
+		ret = clk_set_rate(pwm->clk, pwm->max_clk_limit);
 		if (ret < 0) {
 			dev_err(dev, "Failed to set max clk rate: %d\n", ret);
 			return ret;
@@ -337,8 +345,8 @@ static int tegra_pwm_probe(struct platform_device *pdev)
 
 	/* Limit the maximum clock rate */
 	if (pwm->soc->max_clk_limit &&
-	    (pwm->src_rate > pwm->soc->max_clk_limit)) {
-		ret = clk_set_rate(pwm->clk, pwm->soc->max_clk_limit);
+	    (pwm->src_rate > pwm->max_clk_limit)) {
+		ret = clk_set_rate(pwm->clk, pwm->max_clk_limit);
 		if (ret < 0) {
 			dev_err(&pdev->dev, "Failed to set max clk rate: %d\n",
 				ret);