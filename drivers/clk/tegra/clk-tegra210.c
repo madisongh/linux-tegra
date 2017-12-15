9613f9f587195906b1cea954432695d13499a9bc
Nicolin Chen <nicolinc@nvidia.com>
2017-12-15 12:04:24 -0800

    clk: tegra210: Do not init clock rate of pll_d2
    The pll_d2 clock is used for tegradc -- display on T210. This
    clock is usually configured in the bootloader to support early
    display.
    
    When kernel takes over the framebuffer from the bootloader, it
    should keep the same clock rate for pll_d2 in order to remain
    the same pixel clock.
    
    So this patch removes the pll_d2 from the t210_init_table. The
    display driver should take the responsibility to configure the
    rate according to the display resolution for initialization.
    
    Bug 200369159
    Bug 200399276
    
    Change-Id: I4a3107a3a32e3a729701f8d69fa218bef37685a3
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Signed-off-by: Naveen Kumar S <nkumars@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1618902
    (cherry picked from commit 30895c87a5508f96f90f158dcf8aa607a279d599)
    Reviewed-on: https://git-master.nvidia.com/r/1692620
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Santosh Galma <galmar@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/clk/tegra/clk-tegra210.c
+++ drivers/clk/tegra/clk-tegra210.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2017 NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2012-2018 NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -3680,7 +3680,6 @@ static struct tegra_clk_init_table t210_init_table[] __initdata = {
 	{ TEGRA210_CLK_PLL_A, TEGRA210_CLK_CLK_MAX, 564480000, 1 },
 	{ TEGRA210_CLK_PLL_RE_VCO, TEGRA210_CLK_CLK_MAX, 672000000, 1 },
 	{ TEGRA210_CLK_PLL_DP, TEGRA210_CLK_CLK_MAX, 270000000, 0 },
-	{ TEGRA210_CLK_PLL_D2, TEGRA210_CLK_CLK_MAX, 594000000, 0 },
 	/* This MUST be the last entry. */
 	{ TEGRA210_CLK_CLK_MAX, TEGRA210_CLK_CLK_MAX, 0, 0 },
 };
