abcc598c0268b203369dd80e18fe0440c5127d09
Pekka Pessi <ppessi@nvidia.com>
2018-07-23 06:51:47 -0700

    Revert "drivers: camera: Optimize power on/off requests"
    There are ap_camera test failure on Quill.
    
    Bug 2245697
    
    This reverts commit 101cbbee8f2198c9a3433abd0facad49c893979b.
    
    Change-Id: Ida11655418a985d337f88be3b62d008543320bd0
    Reverted-Reviewed-on: https://git-master.nvidia.com/r/1757809
    Reviewed-on: https://git-master.nvidia.com/r/1783966
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/media/platform/tegra/camera/vi/vi4_fops.c
+++ drivers/media/platform/tegra/camera/vi/vi4_fops.c
@@ -1289,15 +1289,12 @@ int vi4_power_on(struct tegra_channel *chan)
 	ret = nvhost_module_add_client(vi->ndev, &chan->video);
 	if (ret)
 		return ret;
+	tegra_vi4_power_on(vi);
 
-	ret = tegra_vi4_power_on(vi);
-	if (ret < 0)
-		return ret;
-
-	ret = tegra_channel_set_power(chan, true);
-	if (ret < 0) {
-		dev_err(vi->dev, "Failed to power on subdevices\n");
-		return ret;
+	if (atomic_add_return(1, &chan->power_on_refcnt) == 1) {
+		ret = tegra_channel_set_power(chan, 1);
+		if (ret < 0)
+			dev_err(vi->dev, "Failed to power on subdevices\n");
 	}
 
 	return ret;
@@ -1312,9 +1309,11 @@ void vi4_power_off(struct tegra_channel *chan)
 	vi = chan->vi;
 	csi = vi->csi;
 
-	ret = tegra_channel_set_power(chan, 0);
-	if (ret < 0)
-		dev_err(vi->dev, "Failed to power off subdevices\n");
+	if (atomic_dec_and_test(&chan->power_on_refcnt)) {
+		ret = tegra_channel_set_power(chan, 0);
+		if (ret < 0)
+			dev_err(vi->dev, "Failed to power off subdevices\n");
+	}
 
 	tegra_vi4_power_off(vi);
 	nvhost_module_remove_client(vi->ndev, &chan->video);
